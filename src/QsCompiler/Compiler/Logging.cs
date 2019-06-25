01640afb80428b73b810e65304bfada452f6bc59
Bettina Heim <beheim@microsoft.com>
2019-06-24 22:58:21 -0700

    xml comments for compiler

--- src/QsCompiler/Compiler/Logging.cs
+++ src/QsCompiler/Compiler/Logging.cs
@@ -47,11 +47,15 @@ namespace Microsoft.Quantum.QsCompiler.Diagnostics
 
         // methods that need to or can be specified by a deriving class
 
+        /// <summary>
         /// Called whenever a diagnostic is logged after the diagnostic has been properly processed. 
+        /// </summary>
         abstract protected internal void Print(Diagnostic msg);
 
+        /// <summary>
         /// Called whenever an exception is logged after the exception has been properly tracked. 
         /// Prints the given exception as Hint if the logger verbosity is sufficiently high. 
+        /// </summary>
         protected internal virtual void OnException(Exception ex) =>
             this.Output(ex == null ? null : new Diagnostic
             {
@@ -62,8 +66,10 @@ namespace Microsoft.Quantum.QsCompiler.Diagnostics
 
         // routines for convenience
 
+        /// <summary>
         /// Logs a diagnostic message based on the given error code,
         /// with the given source as the file for which the error occurred.
+        /// </summary>
         public void Log(ErrorCode code, IEnumerable<string> args, string source = null, Range range = null) =>
             this.Log(new Diagnostic
             {
@@ -74,8 +80,10 @@ namespace Microsoft.Quantum.QsCompiler.Diagnostics
                 Range = range
             });
 
+        /// <summary>
         /// Logs a a diagnostic message based on the given warning code,
         /// with the given source as the file for which the error occurred.
+        /// </summary>
         public void Log(WarningCode code, IEnumerable<string> args, string source = null, Range range = null) =>
             this.Log(new Diagnostic
             {
@@ -86,9 +94,11 @@ namespace Microsoft.Quantum.QsCompiler.Diagnostics
                 Range = range
             });
 
+        /// <summary>
         /// Generates a Diagnostic message based on the given information code,
         /// with any message parameters appended on a new line to the message defined by the information code.
         /// The given source is listed as the file for which the error occurred.
+        /// </summary>
         public void Log(InformationCode code, IEnumerable<string> args, string source = null, Range range = null, params string[] messageParam) =>
             this.Log(new Diagnostic
             {
@@ -99,8 +109,10 @@ namespace Microsoft.Quantum.QsCompiler.Diagnostics
                 Range = range
             });
 
+        /// <summary>
         /// Logs the given diagnostic messages.
         /// Ignores any parameter that is null. 
+        /// </summary>
         public void Log(params Diagnostic[] messages)
         {
             if (messages == null) return;
@@ -114,16 +126,20 @@ namespace Microsoft.Quantum.QsCompiler.Diagnostics
 
         // core routines which track logged diagnostics
 
+        /// <summary>
         /// Calls Print on the given diagnostic if the verbosity is sufficiently high. 
         /// Does nothing if the given diagnostic is null. 
+        /// </summary>
         private void Output(Diagnostic msg)
         { if (msg?.Severity <= this.Verbosity) this.Print(msg); }
 
+        /// <summary>
         /// Increases the error or warning counter if appropriate, and
         /// prints the given diagnostic ff the logger verbosity is sufficiently high.
         /// Before printing, the line numbers are shifted by the offset specified upon initialization. 
         /// Returns without doing anything if the given diagnostic is a warning that is to be ignored. 
         /// Throws an ArgumentNullException if the given diagnostic is null. 
+        /// </summary>
         public void Log(Diagnostic m)
         {
             if (m == null) throw new ArgumentNullException(nameof(m));
@@ -138,8 +154,10 @@ namespace Microsoft.Quantum.QsCompiler.Diagnostics
             this.Output(msg);
         }
 
+        /// <summary>
         /// Increases the exception counter and calls OnException with the given exception. 
         /// Throws an ArgumentNullException if the given exception is null. 
+        /// </summary>
         public void Log(Exception ex)
         {
             if (ex == null) throw new ArgumentNullException(nameof(ex));
@@ -154,10 +172,12 @@ namespace Microsoft.Quantum.QsCompiler.Diagnostics
         public static IEnumerable<string> Indent(params string[] items) =>
             items?.Select(msg => $"    {msg}");
 
+        /// <summary>
         /// Returns a string that contains all information about the given diagnostic in human readable format. 
         /// The string contains one-based position information if the range information is not null, 
         /// assuming the given position information is zero-based.
         /// Throws an ArgumentNullException if the given message is null. 
+        /// </summary>
         public static string HumanReadableFormat(Diagnostic msg)
         {
             if (msg == null) throw new ArgumentNullException(nameof(msg));
@@ -174,11 +194,13 @@ namespace Microsoft.Quantum.QsCompiler.Diagnostics
                 String.IsNullOrWhiteSpace(codeStr) ? $"\n{message}" : $"\n[{codeStr.Trim()}] {message}";
         }
 
+        /// <summary>
         /// Returns a string that contains all information about the given diagnostic 
         /// in a format that is detected and processed as diagnostic by VS and VS Code. 
         /// The string contains one-based position information if the range information is not null, 
         /// assuming the given position information is zero-based.
         /// Throws an ArgumentNullException if the given message is null. 
+        /// </summary>
         public static string MsBuildFormat(Diagnostic msg)
         {
             if (msg == null) throw new ArgumentNullException(nameof(msg));
--- src/QsCompiler/Compiler/Process.cs
+++ src/QsCompiler/Compiler/Process.cs
@@ -12,9 +12,11 @@ namespace Microsoft.Quantum.QsCompiler
 {
     public static class ProcessRunner
     {
+        /// <summary>
         /// Starts the given process and accumulates the received output and error data in the given StringBuilders. 
         /// Returns true if the process completed within the specified time without throwing an exception, and false otherwise.
         /// Any thrown exception is returns as out parameter. 
+        /// </summary>
         public static bool Run(Process process, StringBuilder output, StringBuilder error, out Exception ex, int timeout)
         {
             using (var outputWaitHandle = new AutoResetEvent(false))
@@ -54,12 +56,14 @@ namespace Microsoft.Quantum.QsCompiler
             }
         }
 
+        /// <summary>
         /// Starts and runs a process invoking the given command with the given arguments.
         /// If a dictionary of environment variables and their desired values is specified, 
         /// sets these environment variables prior to execution and resets them afterwards.
         /// Accumulates the received output and error data in the respective StringBuilder and returns them as out parameters. 
         /// Returns true if the process completed within the specified time without throwing an exception, and false otherwise.
         /// Returns the exit code of the process as well as any thrown exception as out parameter. 
+        /// </summary>
         public static bool Run(string command, string args,
             out StringBuilder outstream, out StringBuilder errstream, out int exitCode, out Exception ex,
             IDictionary<string, string> envVariables = null, int timeout = 10000)
