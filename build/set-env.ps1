f9141494632bedce3e582ef2f67e038d7f852802
Andres Paz <anpaz@microsoft.com>
2019-07-01 21:55:15 -0700

    Clean-up

--- build/set-env.ps1
+++ build/set-env.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = 'Stop'
 
 Write-Host "Setting up build environment variables"
 
-If ($Env:BUILD_BUILDNUMBER -eq $null) { $Env:BUILD_BUILDNUMBER ="0.0.0.1" }
+If ($Env:BUILD_BUILDNUMBER -eq $null) { $Env:BUILD_BUILDNUMBER ="0.0.1.0" }
 If ($Env:BUILD_CONFIGURATION -eq $null) { $Env:BUILD_CONFIGURATION ="Debug" }
 If ($Env:BUILD_VERBOSITY -eq $null) { $Env:BUILD_VERBOSITY ="m" }
 If ($Env:ASSEMBLY_VERSION -eq $null) { $Env:ASSEMBLY_VERSION ="$Env:BUILD_BUILDNUMBER" }
