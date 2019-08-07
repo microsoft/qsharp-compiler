223e145a68d9e74d27c477ab2f466312c1539f2b
Bettina Heim <beheim@microsoft.com>
2019-08-07 11:33:41 -0700

    wip

--- src/QsProjectSystem/QsProjectSystem/QsProjectSystemPackage.cs
+++ src/QsProjectSystem/QsProjectSystem/QsProjectSystemPackage.cs
@@ -2,6 +2,7 @@
 using System.Runtime.InteropServices;
 using System.Threading;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 using Task = System.Threading.Tasks.Task;
 
 
@@ -22,8 +23,6 @@ namespace QsProjectSystem
         public const string PackageGuidString = "3d28495d-e5df-4f6d-b974-0f4349f34a39";
         public const string QsProjectFactoryGuidString = "AE300E5E-0476-4A1C-89BC-2721B8251387";
 
-        #region Package Members
-
         /// <summary>
         /// Initialization of the package; this method is called right after the package is sited, so this is the place
         /// where you can put all the initialization code that rely on services provided by VisualStudio.
@@ -36,14 +35,28 @@ namespace QsProjectSystem
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            this.RegisterProjectFactory(new QsProjectFactory(this));
         }
-
-        #endregion
     }
 
     [Guid(QsProjectSystemPackage.QsProjectFactoryGuidString)]
-    public sealed class QsProjectFactory
+    public sealed class QsProjectFactory : IVsProjectFactory
     {
+        private QsProjectSystemPackage Package;
+        public QsProjectFactory(QsProjectSystemPackage package) =>
+            this.Package = package;
 
+
+        public int CanCreateProject(string pszFilename, uint grfCreateFlags, out int pfCanCreate) =>
+            throw new NotImplementedException();
+
+        public int CreateProject(string pszFilename, string pszLocation, string pszName, uint grfCreateFlags, ref Guid iidProject, out IntPtr ppvProject, out int pfCanceled) =>
+            throw new NotImplementedException();
+
+        public int SetSite(Microsoft.VisualStudio.OLE.Interop.IServiceProvider psp) =>
+            throw new NotImplementedException();
+
+        public int Close() =>
+            throw new NotImplementedException();
     }
 }
