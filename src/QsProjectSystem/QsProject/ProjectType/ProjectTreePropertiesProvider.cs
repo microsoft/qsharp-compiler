ff146608706f26595449c9ccd4483168232c6317
Bettina Heim <beheim@microsoft.com>
2019-08-08 12:44:27 -0700

    adding the handles for our own project icon (haven't looked for an actual icon yet)

---
+++ src/QsProjectSystem/QsProject/ProjectType/ProjectTreePropertiesProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.ProjectSystem;
+
+
+namespace Microsoft.Quantum.QsProjectSystem
+{
+    [Export(typeof(IProjectTreePropertiesProvider))]
+    [AppliesTo(QsUnconfiguredProject.UniqueCapability)]
+    internal class ProjectTreePropertiesProvider : IProjectTreePropertiesProvider
+    {
+        /// <summary>
+        /// Updates the property values for each node in the project tree, 
+        /// given the context and values calculated by lower priority tree properties providers (if any exist).
+        /// </summary>
+        public void CalculatePropertyValues(
+            IProjectTreeCustomizablePropertyContext propertyContext,
+            IProjectTreeCustomizablePropertyValues propertyValues)
+        {
+            // Only set the icon for the root project node. 
+            if (propertyValues.Flags.Contains(ProjectTreeFlags.Common.ProjectRoot))
+            { propertyValues.Icon = QsIconsMonikers.ProjectIconImageMoniker.ToProjectSystemType(); }
+        }
+    }
+}
---
+++ src/QsProjectSystem/QsProject/ProjectType/QsIconsMonikers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.Imaging.Interop;
+
+namespace Microsoft.Quantum.QsProjectSystem
+{
+    public static class QsIconsMonikers
+    {
+        private const int IconId = 0;
+
+        /// <summary>
+        /// Needs to match the QsIconsGuid defined in the imagemanifest. 
+        /// </summary>
+        private static readonly Guid ManifestGuid = 
+            new Guid("99495504-4c00-4fa2-8132-dbdef447a705");
+
+        public static ImageMoniker ProjectIconImageMoniker =>
+            new ImageMoniker { Guid = ManifestGuid, Id = IconId };
+    }
+}
