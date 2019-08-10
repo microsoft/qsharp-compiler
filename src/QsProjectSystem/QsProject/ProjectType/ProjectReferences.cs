6f84caf06a910e219a98da2b252b86fb6fb1bc23
Bettina Heim <beheim@microsoft.com>
2019-08-10 08:57:43 -0700

    some properties for project references (not much progress)

---
+++ src/QsProjectSystem/QsProject/ProjectType/ProjectReferences.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.References;
+
+
+namespace Microsoft.Quantum.QsProjectSystem
+{
+    [Export(typeof(IValidProjectReferenceChecker))]
+    [AppliesTo(QsUnconfiguredProject.UniqueCapability)]
+    class QsProjectReferenceChecker : IValidProjectReferenceChecker
+    {
+        public Task<SupportedCheckResult> CanAddProjectReferenceAsync(object referencedProject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CanAddProjectReferencesResult> CanAddProjectReferencesAsync(IImmutableSet<object> referencedProjects)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SupportedCheckResult> CanBeReferencedAsync(object referencingProject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
