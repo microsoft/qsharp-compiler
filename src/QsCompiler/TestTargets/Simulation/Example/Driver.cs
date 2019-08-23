a9704eca64342ba1f3de6f9cef9c1ba44502cbec
Bettina Heim <beheim@microsoft.com>
2019-08-23 16:14:52 -0700

    almost there

---
+++ src/QsCompiler/TestTargets/Simulation/Example/Driver.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Quantum.Simulation.Simulators;
+
+
+namespace Quantum.Example
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using (var qsim = new QuantumSimulator())
+            {
+                HelloQ.Run(qsim).Wait();
+            }
+        }
+    }
+}
---
+++ src/QsCompiler/TestTargets/Simulation/Example/Operations.qs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Quantum.Example
+{
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Canon;
+    
+    operation HelloQ () : Unit {
+        Message("Hello quantum world!");
+    }
+}
