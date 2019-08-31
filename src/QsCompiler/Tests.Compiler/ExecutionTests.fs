c989eadd3577f107ba608e9f32cddba39f542f3a
Bettina Heim <beheim@microsoft.com>
2019-08-30 18:30:38 -0700

    starting to set up some execution tests

---
+++ src/QsCompiler/Tests.Compiler/ExecutionTests.fs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Quantum.QsCompiler.Testing
+
+open System.Collections.Generic
+open Microsoft.Quantum.QsCompiler.DataTypes
+open Microsoft.Quantum.QsCompiler.Diagnostics
+open Microsoft.Quantum.QsCompiler.SyntaxExtensions
+open Microsoft.Quantum.QsCompiler.SyntaxTree
+open Xunit
+open Xunit.Abstractions
+
+
+type FunctorAutoGenTests (output:ITestOutputHelper) =
+
+    //inherit CompilerTests("TestCases", ["General.qs"; "FunctorGeneration.qs"], output)
+    //
+    //member private this.Expect name (diag : IEnumerable<DiagnosticItem>) = 
+    //    let ns = "Microsoft.Quantum.Testing.FunctorGeneration" |> NonNullable<_>.New
+    //    let name = name |> NonNullable<_>.New
+    //    this.Verify (QsQualifiedName.New (ns, name), diag)
+
+
+    [<Fact>]
+    member this.``Specialization Generation for Conjugations`` () = 
+        let expectedBody = "
+            U1
+            V1
+            U3
+            V3
+            Core3
+            Adjoint V3
+            Adjoint U3
+            Core1
+            U2
+            V2
+            Core2
+            Adjoint V2
+            Adjoint U2
+            U3
+            V3
+            Adjoint Core3
+            Adjoint V3
+            Adjoint U3
+            Adjoint V1
+            Adjoint U1        
+        "
+        let expectedAdjoint = "
+            U1
+            V1
+            U3
+            V3
+            Core3
+            Adjoint V3
+            Adjoint U3
+            U2
+            V2
+            Adjoint Core2
+            Adjoint V2
+            Adjoint U2
+            Adjoint Core1
+            U3
+            V3
+            Adjoint Core3
+            Adjoint V3
+            Adjoint U3
+            Adjoint V1
+            Adjoint U1        
+        "
+        let expectedControlled = "
+            U1
+            V1
+            U3
+            V3
+            Core3
+            Adjoint V3
+            Adjoint U3
+            Controlled Core1
+            U2
+            V2
+            Controlled Core2
+            Adjoint V2
+            Adjoint U2
+            U3
+            V3
+            Adjoint Core3
+            Adjoint V3
+            Adjoint U3
+            Adjoint V1
+            Adjoint U1        
+        "
+        let expectedControlledAdjoint = "
+            U1
+            V1
+            U3
+            V3
+            Core3
+            Adjoint V3
+            Adjoint U3
+            U2
+            V2
+            Controlled Adjoint Core2
+            Adjoint V2
+            Adjoint U2
+            Controlled Adjoint Core1
+            U3
+            V3
+            Adjoint Core3
+            Adjoint V3
+            Adjoint U3
+            Adjoint V1
+            Adjoint U1        
+        "
+        ()
---
+++ src/QsCompiler/Tests.Compiler/TestCases/ExecutionLogging.qs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/// This namespace contains test cases for tests based on logging during execution
+namespace Microsoft.Quantum.Testing.ExecutionLogging {
+
+	operation ULog<'T> (i : 'T) : Unit {
+		body (...) {
+			Message($"{i}");
+		}
+		adjoint (...) {
+			Message($"Adjoint {i}");
+		}
+		controlled (cs, ...) {
+			Message($"Controlled {i}");
+		}
+		controlled adjoint (cs, ...) {
+			Message($"Controlled Adjoint {i}");
+		}
+	}
+
+	operation CallBody (operation : (Unit => Unit)) : Unit {
+		operation();
+	}
+
+	operation CallAdjoint (adjointable : (Unit => Unit is Adj)) {
+		Adjoint adjointable();
+	}
+
+	operation CallControlled (controllable : (Unit => Unit is Ctl)) {
+		Controlled adjointable(new Qubit[0], ());
+	}
+
+	operation CallControlledAdjoint (unitary : (Unit => Unit is Ctl + Adj)) {
+		Controlled Adjoint unitary(new Qubit[0], ());
+	}
+
+
+	// tests related to auto-generation of functor specializations for operations involving conjugations
+
+	operation SpecGenForConjugations () : Unit 
+	is Adj + Ctl {
+
+		within {
+			ULog("U1");
+			ULog("V1");
+
+			within {
+				ULog("U3");
+				ULog("V3");
+			}
+			apply {
+				ULog("Core3");
+			}
+		}
+		apply {
+			ULog("Core1");
+
+			within {
+				ULog("U2");
+				ULog("V2");
+			}
+			apply {
+				ULog("Core2");
+			}
+		}
+	}
+}
