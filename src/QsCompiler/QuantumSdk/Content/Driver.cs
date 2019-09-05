e0bec02d8134f82525a24f5f57797133fe89eea0
Bettina Heim <beheim@microsoft.com>
2019-09-05 13:38:22 -0700

    almost the first draft recovered - minus the project file

---
+++ src/QsCompiler/QuantumSdk/Content/Driver.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.App1
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using (var sim = new QuantumSimulator())
+            {
+                QuantumMain.Run(sim).Wait();
+            }
+        }
+    }
+}
