8cde83369ab8078cab8c088d94ce41bffabd747b
Chris Granade <chgranad@microsoft.com>
2019-09-10 13:23:11 -0700

    Initial draft for packaging Q# data structures as dlls. (#107)

---
+++ src/QsCompiler/DllPackaging/MetadataGeneration.cs
@@ -0,0 +1,133 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Linq;
+using System;
+
+namespace Microsoft.Quantum.QsCompiler
+{
+    public static class MetadataGeneration
+    {
+        public static ArrayTypeSyntax WithOmittedRankSpecifiers(this ArrayTypeSyntax syntax) =>
+            syntax
+            .WithRankSpecifiers(
+                SingletonList<ArrayRankSpecifierSyntax>(
+                    ArrayRankSpecifier(
+                        SingletonSeparatedList<ExpressionSyntax>(
+                            OmittedArraySizeExpression()
+                        )
+                    )
+                )
+            );
+
+        internal static CodeAnalysis.SyntaxTree GenerateAssemblyMetadata(
+            Compilation compilation,
+            IEnumerable<MetadataReference> references,
+            Action<string> log = null
+        )
+        {
+            var aliases = references.Select(reference => reference.Properties.Aliases.First());
+            var typeName =
+                QualifiedName(
+                    AliasQualifiedName(
+                        IdentifierName(Token(SyntaxKind.GlobalKeyword)),
+                        IdentifierName("System")
+                    ),
+                    IdentifierName("Type")
+                );
+            var metadataTypeNodes =
+                aliases
+                .Select(
+                    alias =>
+                        TypeOfExpression(
+                            QualifiedName(
+                                AliasQualifiedName(
+                                    IdentifierName(alias),
+                                    IdentifierName(WellKnown.METADATA_NAMESPACE)
+                                ),
+                                IdentifierName(WellKnown.METADATA_TYPE)
+                            )
+                        )
+                );
+            foreach (var node in metadataTypeNodes)
+            {
+                if (log != null) {log(node.NormalizeWhitespace().ToFullString());}
+            }
+            var dependenciesInitializer =
+                ArrayCreationExpression(
+                    ArrayType(typeName)
+                    .WithOmittedRankSpecifiers()
+                )
+                .WithInitializer(
+                    InitializerExpression(
+                        SyntaxKind.ArrayInitializerExpression,
+                        SeparatedList<ExpressionSyntax>(
+                            metadataTypeNodes
+                        )
+                    )
+                );
+            var metadataField =
+                FieldDeclaration(
+                    VariableDeclaration(
+                        ArrayType(typeName)
+                            .WithOmittedRankSpecifiers()
+                    )
+                    .WithVariables(
+                        SingletonSeparatedList<VariableDeclaratorSyntax>(
+                            VariableDeclarator(Identifier(WellKnown.DEPENDENCIES_FIELD))
+                            .WithInitializer(
+                                EqualsValueClause(dependenciesInitializer)
+                            )
+                        )
+                    )
+                )
+                .WithModifiers(
+                    TokenList(
+                        Token(SyntaxKind.PublicKeyword),
+                        Token(SyntaxKind.StaticKeyword),
+                        Token(SyntaxKind.ReadOnlyKeyword)
+                    )
+                );
+            var classDef =
+                ClassDeclaration(WellKnown.METADATA_TYPE)
+                    .WithModifiers(
+                        TokenList(Token(SyntaxKind.PublicKeyword), Token(SyntaxKind.StaticKeyword))
+                    )
+                    .WithMembers(
+                        SingletonList<MemberDeclarationSyntax>(metadataField)
+                    );
+            var namespaceDef =
+                NamespaceDeclaration(IdentifierName(WellKnown.METADATA_NAMESPACE))
+                    .WithMembers(
+                        SingletonList<MemberDeclarationSyntax>(classDef)
+                    );
+
+            var tree = CSharpSyntaxTree.Create(
+                CompilationUnit()
+                    .WithExterns(
+                        List<ExternAliasDirectiveSyntax>(
+                            aliases
+                                .Select(
+                                    alias =>
+                                        ExternAliasDirective(
+                                            Identifier(alias)
+                                        )
+                                )
+                        )
+                    )
+                    .WithMembers(
+                        SingletonList<MemberDeclarationSyntax>(
+                            namespaceDef
+                        )
+                    )
+            );
+
+            return tree;
+        }
+    }
+}
---
+++ src/QsCompiler/DllPackaging/WellKnownNames.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Quantum.QsCompiler
+{
+    public static class WellKnown
+    {
+        public const string AST_RESOURCE_NAME = "__qsharp_data__.bson";
+        public const string METADATA_NAMESPACE = "__qsharp__";
+        public const string METADATA_TYPE = "Metadata";
+        public const string DEPENDENCIES_FIELD = "Dependencies";
+    }
+}
