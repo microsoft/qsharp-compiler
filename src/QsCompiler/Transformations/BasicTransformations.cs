381066da96c1d033bb91d07cea0d3d622990c08b
Scott Carda <sccarda@microsoft.com>
2019-09-26 10:53:55 -0700

    temporary commit

--- src/QsCompiler/Transformations/BasicTransformations.cs
+++ src/QsCompiler/Transformations/BasicTransformations.cs
@@ -13,6 +13,34 @@ namespace Microsoft.Quantum.QsCompiler.Transformations.BasicTransformations
 {
     // syntax tree transformations
 
+    public class ResolveGenerics :
+            SyntaxTreeTransformation<NoScopeTransformations>
+    {
+        public static QsNamespace Apply(QsNamespace ns)
+        {
+            if (ns == null) throw new ArgumentNullException(nameof(ns));
+            var filter = new ResolveGenerics();
+            return filter.Transform(ns);
+        }
+
+        //private readonly Dictionary<thing1, thing2> Callables;
+        private ResolveGenerics() : base(new NoScopeTransformations()) {}
+
+        public override QsSpecialization onSpecializationImplementation(QsSpecialization spec) // short cut to avoid further evaluation
+        {
+            this.onSourceFile(spec.SourceFile);
+            return spec;
+        }
+
+        public override QsNamespace Transform(QsNamespace ns)
+        {
+            // Get global callables
+            // this.Callables = GetCallables(ns);
+            var newNs = base.Transform(ns);
+            return newNs;
+        }
+    }
+
     public class GetSourceFiles :
         SyntaxTreeTransformation<NoScopeTransformations>
     {
