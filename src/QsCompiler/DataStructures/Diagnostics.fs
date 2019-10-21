ab4e00dcc9f2e08906ba240bdc61e74f38ed7a8e
bettinaheim <34236215+bettinaheim@users.noreply.github.com>
2019-10-21 13:59:59 -0700

    Update src/QsCompiler/DataStructures/Diagnostics.fs
    Co-Authored-By: Scott Carda <55811729+ScottCarda-MS@users.noreply.github.com>

--- src/QsCompiler/DataStructures/Diagnostics.fs
+++ src/QsCompiler/DataStructures/Diagnostics.fs
@@ -642,7 +642,7 @@ type DiagnosticItem =
             | WarningCode.DiscardingItemInAssignment            -> "The expression on the right hand side is discarded on assignment and can be ommitted."
             | WarningCode.ConditionalEvaluationOfOperationCall  -> "This expression may be short-circuited, and operation calls may not be executed."
             | WarningCode.DeprecationWithRedirect               -> "{0} has been deprecated. Please use {1} instead."
-            | WarningCode.DeprecationWithoutRedirect            -> "(0) has been deprecated."
+            | WarningCode.DeprecationWithoutRedirect            -> "{0} has been deprecated."
             | WarningCode.TypeParameterNotResolvedByArgument    -> "The value of the type parameter is not determined by the argument type. It will always have to be explicitly specified by passing type arguments." 
             | WarningCode.ReturnTypeNotResolvedByArgument       -> "The return type is not fully determined by the argument type. It will always have to be explicitly specified by passing type arguments."
             | WarningCode.NamespaceAleadyOpen                   -> "The namespace is already open."
