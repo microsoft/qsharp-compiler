7a91c510068e72e73a02abf66986b695385a7494
Bettina Heim <beheim@microsoft.com>
2019-10-31 19:45:46 -0700

    now we are running into the problem that the linked file will cause the interface not to be recognized as the same...

--- src/QsCompiler/Compiler/ExternalRewriteSteps.cs
+++ src/QsCompiler/Compiler/ExternalRewriteSteps.cs
@@ -98,7 +98,7 @@ namespace Microsoft.Quantum.QsCompiler
             {
                 var relevantTypes = new List<Type>();
                 Diagnostic LoadError(ErrorCode code, params string[] args) => Errors.LoadError(code, args, ProjectManager.MessageSource(target));
-                try { relevantTypes.AddRange(Assembly.LoadFrom(target.LocalPath).GetTypes().Where(t => t.IsAssignableFrom(typeof(IRewriteStep)))); }
+                try { relevantTypes.AddRange(Assembly.LoadFrom(target.LocalPath).GetTypes().Where(t => typeof(IRewriteStep).IsAssignableFrom(t))); }
                 catch (BadImageFormatException ex)
                 {
                     onDiagnostic?.Invoke(LoadError(ErrorCode.FileIsNotAnAssembly, target.LocalPath));
@@ -115,7 +115,7 @@ namespace Microsoft.Quantum.QsCompiler
                     try { loadedSteps.Add(new LoadedStep((IRewriteStep)Activator.CreateInstance(type), target)); }
                     catch (Exception ex)
                     {
-                        onDiagnostic?.Invoke(LoadError(ErrorCode.CouldNotInstantiateRewriteStep, type.ToString(), target.LocalPath)); // todo: check message
+                        onDiagnostic?.Invoke(LoadError(ErrorCode.CouldNotInstantiateRewriteStep, type.ToString(), target.LocalPath));
                         onException?.Invoke(ex);
                     }
                 }
