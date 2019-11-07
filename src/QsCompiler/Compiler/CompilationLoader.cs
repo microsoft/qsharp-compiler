bbca2d9303aaa791e47512c32fcbb07958bb76d2
Bettina Heim <beheim@microsoft.com>
2019-11-06 18:56:55 -0800

    adaption to account for merge

--- src/QsCompiler/Compiler/CompilationLoader.cs
+++ src/QsCompiler/Compiler/CompilationLoader.cs
@@ -332,7 +332,7 @@ namespace Microsoft.Quantum.QsCompiler
             {
                 if (!Uri.TryCreate(Assembly.GetExecutingAssembly().CodeBase, UriKind.Absolute, out Uri thisDllUri))
                 { thisDllUri = new Uri(Path.GetFullPath(".", "CompilationLoader.cs")); }
-                var rewriteStep = new RewriteSteps.LoadedStep(new Monomorphization(), thisDllUri);
+                var rewriteStep = new RewriteSteps.LoadedStep(new Monomorphization(), typeof(IRewriteStep), thisDllUri);
                 this.CompilationStatus.Monomorphization = this.ExecuteRewriteStep(rewriteStep, this.CompilationOutput, out this.CompilationOutput); 
             }
 
