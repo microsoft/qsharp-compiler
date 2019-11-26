74494aa839f674e27584c4756b4c2f708998d408
Bettina Heim <beheim@microsoft.com>
2019-11-25 20:20:52 -0800

    moving self-contained packaging of the language server into pack

--- build/pack.ps1
+++ build/pack.ps1
@@ -10,6 +10,7 @@ Write-Output("Assembly version: $Env:ASSEMBLY_VERSION");
 ##
 # Q# compiler
 ##
+
 function Pack-One() {
     param(
         [string]$project, 
@@ -36,8 +37,109 @@ Pack-One '../src/ProjectTemplates/Microsoft.Quantum.ProjectTemplates.nuspec'
 Pack-One '../src/QuantumSdk/QuantumSdk.nuspec'
 
 ##
+# Q# Language Server (self-contained)
+##
+
+$Runtimes = @{
+    "win10-x64" = "win32";
+    "linux-x64" = "linux";
+    "osx-x64" = "darwin";
+};
+
+function New-TemporaryDirectory {
+    $parent = [System.IO.Path]::GetTempPath()
+    $name = [System.IO.Path]::GetRandomFileName()
+    New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
+
+function Write-Hash() {
+    param(
+        [string]
+        $Path,
+
+        [string]
+        $BlobPlatform,
+
+        [string]
+        $TargetPath
+    );
+
+    "Writing hash of $Path into $TargetPath..." | Write-Host;
+    $packageData = Get-Content $TargetPath | ConvertFrom-Json;
+    $packageData.blobs.$BlobPlatform.sha256 = Get-FileHash $Path | Select-Object -ExpandProperty Hash;
+    # See https://stackoverflow.com/a/23738236 for why this works.
+    $packageData.blobs.$BlobPlatform `
+        | Add-Member `
+            -Force `
+            -MemberType NoteProperty `
+            -Name "size" `
+            -Value (Get-Item $Path | Select-Object -ExpandProperty Length);
+    Write-Host "New blob data: $($packageData."blobs".$BlobPlatform | ConvertTo-Json)";
+    $packageData `
+        | ConvertTo-Json -Depth 32 `
+        | Set-Content `
+            -Path $TargetPath `
+            -Encoding UTF8NoBom;
+}
+
+function Pack-SelfContained() {
+    param(
+        [string] $Project,
+
+        [string] $PackageData = $null
+    );
+
+    Write-Host "##[info]Packing $Project as a self-contained deployment...";
+    $Runtimes.GetEnumerator() | ForEach-Object {
+        $DotNetRuntimeID = $_.Key;
+        $NodePlatformID = $_.Value;
+        $TargetDir = New-TemporaryDirectory;
+        $BaseName = [System.IO.Path]::GetFileNameWithoutExtension((Join-Path $PSScriptRoot $Project));
+        $ArchiveDir = Join-Path $Env:BLOBS_OUTDIR $BaseName;
+        New-Item -ItemType Directory -Path $ArchiveDir -Force -ErrorAction SilentlyContinue;
+
+        try {
+            $ArchivePath = Join-Path $ArchiveDir "$BaseName-$DotNetRuntimeID-$Env:ASSEMBLY_VERSION.zip";
+            dotnet publish (Join-Path $PSScriptRoot $Project) `
+                -c $Env:BUILD_CONFIGURATION `
+                -v $Env:BUILD_VERBOSITY `
+                --self-contained `
+                --runtime $DotNetRuntimeID `
+                --output $TargetDir `
+                /property:DefineConstants=$Env:ASSEMBLY_CONSTANTS `
+                /property:Version=$Env:ASSEMBLY_VERSION
+            Write-Host "##[info]Writing self-contained deployment to $ArchivePath..."
+            Compress-Archive `
+                -Force `
+                -Path (Join-Path $TargetDir *) `
+                -DestinationPath $ArchivePath `
+                -ErrorAction Continue;
+            if ($null -ne $PackageData) {
+                Write-Hash `
+                    -Path $ArchivePath `
+                    -BlobPlatform $NodePlatformID `
+                    -TargetPath (Join-Path $PSScriptRoot $PackageData)
+            }
+        } catch {
+            Write-Host "##vso[task.logissue type=error;]Failed to pack self-contained deployment: $_";
+            $Script:all_ok = $false;
+        } finally {
+            Remove-Item -Recurse $TargetDir -ErrorAction Continue;
+        }
+    };
+}
+
+Pack-SelfContained `
+    -Project "../src/QsCompiler/LanguageServer/LanguageServer.csproj" `
+    -PackageData "../src/VSCodeExtension/package.json"
+
+Write-Host "Final package.json:"
+Get-Content "../src/VSCodeExtension/package.json" | Write-Host
+
+##
 # VS Code Extension
 ##
+
 Write-Host "##[info]Packing VS Code extension..."
 Push-Location (Join-Path $PSScriptRoot '../src/VSCodeExtension')
 if (Get-Command vsce -ErrorAction SilentlyContinue) {
@@ -59,6 +161,7 @@ Pop-Location
 ##
 # VisualStudioExtension
 ##
+
 Write-Host "##[info]Packing VisualStudio extension..."
 Push-Location (Join-Path $PSScriptRoot '..\src\VisualStudioExtension\QsharpVSIX')
 if (Get-Command msbuild -ErrorAction SilentlyContinue) {
