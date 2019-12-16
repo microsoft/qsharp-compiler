1047aa91c0ed25631b31ea382e39f31dd3734cb9
Bettina Heim <beheim@microsoft.com>
2019-12-15 18:55:19 -0800

    Revert "draft for msbuild task"
    This reverts commit e1b7ff67f1ef7f5009999d60b1544108f6bd20e1.

--- build/build.ps1
+++ build/build.ps1
@@ -33,7 +33,7 @@ function Build-One {
 Build-One 'build' '../QsCompiler.sln'
 Build-One 'publish' '../src/QsCompiler/CommandLineTool/CommandLineTool.csproj'
 Build-One 'build' '../src/QuantumSdk/Tools/Tools.sln'
-Build-One 'publish' '../src/QuantumSdk/Tools/BuildTasks/BuildTasks.csproj'
+Build-One 'publish' '../src/QuantumSdk/Tools/Tools.csproj'
 
 ##
 # VS Code Extension
---
+++ src/QuantumSdk/Tools/Program.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using CommandLine;
+
+
+namespace Microsoft.Quantum.QsCompiler.CommandLineCompiler
+{
+    public class Options {
+
+        [Option('i', "input", Required = true, 
+        HelpText = "Q# code or name of the Q# file to compile.")]
+        public IEnumerable<string> Input { get; set; }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
