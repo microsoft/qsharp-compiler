4867c814e42491c870e46b12d225dd684d2ca78c
Bettina Heim <beheim@microsoft.com>
2020-01-11 09:26:12 -0800

    ignoring command line and setting verbose to false during qsc program execution

--- src/QsCompiler/CommandLineTool/Program.cs
+++ src/QsCompiler/CommandLineTool/Program.cs
@@ -79,7 +79,7 @@ namespace Microsoft.Quantum.QsCompiler.CommandLineCompiler
     {
         private static int Run<T>(Func<T, ConsoleLogger, int> compile, T options) where T : Options
         {
-            options.Verbose = true;
+            options.Verbose = false;
             var logger = options.GetLogger();
             try
             {
