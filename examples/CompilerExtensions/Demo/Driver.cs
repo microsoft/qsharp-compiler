396a0a55e0cb51e0f761390c66471e2e39bb68b2
Bettina Heim <beheim@microsoft.com>
2020-02-01 16:46:32 -0800

    that should be ok as an example for defining custom compiler extensions in a referenced .NET Core project

---
+++ examples/CompilerExtensions/Demo/Driver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Microsoft.Quantum.Demo
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using var qsim = new QuantumSimulator();
+            SampleProgram.Run(qsim).Wait();
+            Console.WriteLine("SampleProgram executed sucessfully!");
+        }
+    }
+}
