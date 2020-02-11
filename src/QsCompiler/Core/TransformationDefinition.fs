becdd9f7efb7a0a9de661977a95cb3d4241fafdc
Bettina Heim <beheim@microsoft.com>
2020-02-10 17:58:57 -0800

    ... to be simplified

--- src/QsCompiler/Core/TransformationDefinition.fs
+++ src/QsCompiler/Core/TransformationDefinition.fs
@@ -61,18 +61,56 @@ and NamespaceTransformation<'T when 'T :> QsSyntaxTreeTransformation>() =
 
 and QsSyntaxTreeTransformation private (dummy) = 
 
-    member val ExpressionTypeTransformation = new ExpressionTypeTransformation<_> ()
-    member val ExpressionKindTransformation = new ExpressionKindTransformation<_> ()
-    member val ExpressionTransformation = new ExpressionTransformation<_> ()
-    member val StatementKindTransformation  = new StatementKindTransformation<_> ()
-    member val StatementTransformation = new StatementTransformation<_> ()
-    member val NamespaceTransformation = new NamespaceTransformation<_> ()
+    let mutable _ExpressionTypeTransformation = new ExpressionTypeTransformation<_> ()
+    let mutable _ExpressionKindTransformation = new ExpressionKindTransformation<_> ()
+    let mutable _ExpressionTransformation = new ExpressionTransformation<_> ()
+    let mutable _StatementKindTransformation  = new StatementKindTransformation<_> ()
+    let mutable _StatementTransformation = new StatementTransformation<_> ()
+    let mutable _NamespaceTransformation = new NamespaceTransformation<_> ()
 
-    new() as this = QsSyntaxTreeTransformation(0) then 
-        this.ExpressionTypeTransformation.Parent <- this
-        this.ExpressionKindTransformation.Parent <- this
-        this.ExpressionTransformation.Parent <- this
-        this.StatementKindTransformation.Parent <- this
-        this.StatementTransformation.Parent <- this
-        this.NamespaceTransformation.Parent <- this
+    member this.ExpressionTypeTransformation
+        with get () = _ExpressionTypeTransformation
+        and private set t = 
+            _ExpressionTypeTransformation <- t
+            t.Parent <- this
+
+    member this.ExpressionKindTransformation 
+        with get () = _ExpressionKindTransformation
+        and private set t = 
+            _ExpressionKindTransformation <- t
+            t.Parent <- this
+
+    member this.ExpressionTransformation 
+        with get () = _ExpressionTransformation
+        and private set t = 
+            _ExpressionTransformation <- t
+            t.Parent <- this
+
+    member this.StatementKindTransformation 
+        with get () = _StatementKindTransformation
+        and private set t = 
+            _StatementKindTransformation <- t
+            t.Parent <- this
+
+    member this.StatementTransformation
+        with get () = _StatementTransformation
+        and private set t = 
+            _StatementTransformation <- t
+            t.Parent <- this
+
+    member this.NamespaceTransformation 
+        with get () = _NamespaceTransformation
+        and private set t = 
+            _NamespaceTransformation <- t
+            t.Parent <- this
+
+    new() as this = 
+        let foo = ()
+        QsSyntaxTreeTransformation(0) then 
+            this.ExpressionTypeTransformation.Parent <- this
+            this.ExpressionKindTransformation.Parent <- this
+            this.ExpressionTransformation.Parent <- this
+            this.StatementKindTransformation.Parent <- this
+            this.StatementTransformation.Parent <- this
+            this.NamespaceTransformation.Parent <- this
 
