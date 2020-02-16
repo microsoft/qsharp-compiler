61ca58bc444b8e04a04f6b514cae95e0390f7ae4
Bettina Heim <beheim@microsoft.com>
2020-02-16 09:36:59 -0800

    migrated LoopUnrolling

--- src/QsCompiler/Optimizations/OptimizingTransformations/LoopUnrolling.fs
+++ src/QsCompiler/Optimizations/OptimizingTransformations/LoopUnrolling.fs
@@ -11,40 +11,45 @@ open Microsoft.Quantum.QsCompiler.Transformations.Core
 
 
 /// The SyntaxTreeTransformation used to unroll loops
-type LoopUnrolling(callables, maxSize) =
-    inherit OptimizingTransformation()
+type LoopUnrolling private (unsafe : string) =
+    inherit OptimizingTransformation<unit>()
+
+    new (callables, maxSize) as this = 
+        new LoopUnrolling("unsafe") then
+            this.Namespaces <- new LoopUnrollingNamespaces(this)
+            this.StatementKinds <- new LoopUnrollingStatementKinds(this, callables, maxSize)
+
+/// private helper class for LoopUnrolling
+and private LoopUnrollingNamespaces (parent : LoopUnrolling) = 
+    inherit OptimizingTransformationNamespaces<unit>(parent)
 
     override __.Transform x =
         let x = base.Transform x
-        VariableRenaming().Transform x
+        VariableRenaming().Namespaces.Transform x
 
-    override __.Scope = { new ScopeTransformation() with
-        override scope.StatementKind = { new StatementKindTransformation() with
-            override __.ExpressionTransformation x = x
-            override __.LocationTransformation x = x
-            override __.ScopeTransformation x = scope.Transform x
-            override __.TypeTransformation x = x
+/// private helper class for LoopUnrolling
+and private LoopUnrollingStatementKinds (parent : LoopUnrolling, callables, maxSize) = 
+    inherit StatementKindTransformation<unit>(parent)
 
-            override this.onForStatement stm =
-                let loopVar = fst stm.LoopItem |> this.onSymbolTuple
-                let iterVals = this.ExpressionTransformation stm.IterationValues
-                let loopVarType = this.TypeTransformation (snd stm.LoopItem)
-                let body = this.ScopeTransformation stm.Body
-                maybe {
-                    let! iterValsList =
-                        match iterVals.Expression with
-                        | RangeLiteral _ when isLiteral callables iterVals ->
-                            rangeLiteralToSeq iterVals.Expression |> Seq.map (IntLiteral >> wrapExpr Int) |> List.ofSeq |> Some
-                        | ValueArray va -> va |> List.ofSeq |> Some
-                        | _ -> None
-                    do! check (iterValsList.Length <= maxSize)
-                    let iterRange = iterValsList |> List.map (fun x ->
-                        let variableDecl = QsBinding.New ImmutableBinding (loopVar, x) |> QsVariableDeclaration |> wrapStmt
-                        let innerScope = { stm.Body with Statements = stm.Body.Statements.Insert(0, variableDecl) }
-                        innerScope |> newScopeStatement |> wrapStmt)
-                    let outerScope = QsScope.New (iterRange, stm.Body.KnownSymbols)
-                    return outerScope |> newScopeStatement |> this.Transform
-                }
-                |? (QsForStatement.New ((loopVar, loopVarType), iterVals, body) |> QsForStatement)
+    override this.onForStatement stm =
+        let loopVar = fst stm.LoopItem |> this.onSymbolTuple
+        let iterVals = this.Expressions.Transform stm.IterationValues
+        let loopVarType = this.Expressions.Types.Transform (snd stm.LoopItem)
+        let body = this.Statements.Transform stm.Body
+        maybe {
+            let! iterValsList =
+                match iterVals.Expression with
+                | RangeLiteral _ when isLiteral callables iterVals ->
+                    rangeLiteralToSeq iterVals.Expression |> Seq.map (IntLiteral >> wrapExpr Int) |> List.ofSeq |> Some
+                | ValueArray va -> va |> List.ofSeq |> Some
+                | _ -> None
+            do! check (iterValsList.Length <= maxSize)
+            let iterRange = iterValsList |> List.map (fun x ->
+                let variableDecl = QsBinding.New ImmutableBinding (loopVar, x) |> QsVariableDeclaration |> wrapStmt
+                let innerScope = { stm.Body with Statements = stm.Body.Statements.Insert(0, variableDecl) }
+                innerScope |> newScopeStatement |> wrapStmt)
+            let outerScope = QsScope.New (iterRange, stm.Body.KnownSymbols)
+            return outerScope |> newScopeStatement |> this.Transform
         }
-    }
+        |? (QsForStatement.New ((loopVar, loopVarType), iterVals, body) |> QsForStatement)
+
