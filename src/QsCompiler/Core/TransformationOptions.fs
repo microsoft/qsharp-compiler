8a7a5d5295a79fa1485553bc72b10bd3f950edd0
Bettina Heim <beheim@microsoft.com>
2020-02-19 09:27:20 -0800

    that should do for the type transformations

--- src/QsCompiler/Core/TransformationOptions.fs
+++ src/QsCompiler/Core/TransformationOptions.fs
@@ -6,12 +6,29 @@ namespace Microsoft.Quantum.QsCompiler.Transformations.Core
 
 /// Used to configure the behavior of the default implementations for transformations.
 type TransformationOptions = {
+    /// Disables the transformation at the transformation root, 
+    /// meaning the transformation won't recur into leaf nodes or subnodes. 
     Disable : bool
+    /// Indicates that the transformation is used to walk the syntax tree, 
+    /// but does not modify any of the nodes. 
+    /// If set to true, the nodes will hence not be rebuilt during the transformation. 
+    /// Setting this to true constitutes a promise that the return value of all methods will be ignored. 
     DisableRebuild : bool
 }
     with 
+    
+    /// Default transformation setting. 
+    /// The transformation will recur into leaf and subnodes, 
+    /// and all nodes will be rebuilt upon transformation.
     static member Default = {
         Disable = false 
+        DisableRebuild = false
+    }
+
+    /// Disables the transformation at the transformation root, 
+    /// meaning the transformation won't recur into leaf nodes or subnodes. 
+    static member Disabled = {
+        Disable = true
         DisableRebuild = false
     }
 
@@ -20,8 +37,8 @@ type TransformationOptions = {
 /// based on the specified options. 
 module internal Utils = 
     type internal INode =
-        abstract member Build<'a, 'b> : ('a -> 'b) -> 'a -> 'b -> 'b
+        abstract member BuildOr<'a, 'b> : 'b -> 'a -> ('a -> 'b) -> 'b
 
-    let Fold = { new INode with member __.Build builder arg _ = builder arg}
-    let Walk = { new INode with member __.Build _ _ original = original}
+    let Fold = { new INode with member __.BuildOr _ arg builder = builder arg}
+    let Walk = { new INode with member __.BuildOr original _ _ = original}
 
--- src/QsCompiler/Core/TypeTransformation.fs
+++ src/QsCompiler/Core/TypeTransformation.fs
@@ -8,12 +8,19 @@ open Microsoft.Quantum.QsCompiler.DataTypes
 open Microsoft.Quantum.QsCompiler.SyntaxExtensions
 open Microsoft.Quantum.QsCompiler.SyntaxTokens
 open Microsoft.Quantum.QsCompiler.SyntaxTree
+open Microsoft.Quantum.QsCompiler.Transformations.Core.Utils
 
 type private ExpressionType = 
     QsTypeKind<ResolvedType, UserDefinedType, QsTypeParameter, CallableInformation>
 
 
-type TypeTransformationBase() = 
+type TypeTransformationBase(options : TransformationOptions) = 
+
+    let Node = if options.DisableRebuild then Walk else Fold
+    new () = new TypeTransformationBase(TransformationOptions.Default)
+
+
+    // supplementary type information 
 
     abstract member onRangeInformation : QsRangeInfo -> QsRangeInfo
     default this.onRangeInformation r = r
@@ -25,35 +32,45 @@ type TypeTransformationBase() =
     default this.onCallableInformation opInfo = 
         let characteristics = this.onCharacteristicsExpression opInfo.Characteristics
         let inferred = opInfo.InferredInformation
-        CallableInformation.New (characteristics, inferred)
+        CallableInformation.New |> Node.BuildOr opInfo (characteristics, inferred)
+
+
+    // nodes containing subtypes
 
     abstract member onUserDefinedType : UserDefinedType -> ExpressionType
     default this.onUserDefinedType udt = 
         let ns, name = udt.Namespace, udt.Name
         let range = this.onRangeInformation udt.Range
-        UserDefinedType.New (ns, name, range) |> ExpressionType.UserDefinedType
+        ExpressionType.UserDefinedType << UserDefinedType.New |> Node.BuildOr InvalidType (ns, name, range)
 
     abstract member onTypeParameter : QsTypeParameter -> ExpressionType
     default this.onTypeParameter tp = 
         let origin = tp.Origin
         let name = tp.TypeName
         let range = this.onRangeInformation tp.Range
-        QsTypeParameter.New (origin, name, range) |> ExpressionType.TypeParameter
-
-    abstract member onUnitType : unit -> ExpressionType
-    default this.onUnitType () = ExpressionType.UnitType
+        ExpressionType.TypeParameter << QsTypeParameter.New |> Node.BuildOr InvalidType (origin, name, range)
 
     abstract member onOperation : (ResolvedType * ResolvedType) * CallableInformation -> ExpressionType
-    default this.onOperation ((it, ot), info) = ExpressionType.Operation ((this.Transform it, this.Transform ot), this.onCallableInformation info)
+    default this.onOperation ((it, ot), info) = 
+        ExpressionType.Operation |> Node.BuildOr InvalidType ((this.Transform it, this.Transform ot), this.onCallableInformation info)
 
     abstract member onFunction : ResolvedType * ResolvedType -> ExpressionType
-    default this.onFunction (it, ot) = ExpressionType.Function (this.Transform it, this.Transform ot)
+    default this.onFunction (it, ot) = 
+        ExpressionType.Function |> Node.BuildOr InvalidType (this.Transform it, this.Transform ot)
 
     abstract member onTupleType : ImmutableArray<ResolvedType> -> ExpressionType
-    default this.onTupleType ts = ExpressionType.TupleType ((ts |> Seq.map this.Transform).ToImmutableArray())
+    default this.onTupleType ts = 
+        ExpressionType.TupleType |> Node.BuildOr InvalidType ((ts |> Seq.map this.Transform).ToImmutableArray())
 
     abstract member onArrayType : ResolvedType -> ExpressionType
-    default this.onArrayType b = ExpressionType.ArrayType (this.Transform b)
+    default this.onArrayType b = 
+        ExpressionType.ArrayType |> Node.BuildOr InvalidType (this.Transform b)
+
+
+    // leaf nodes
+    
+    abstract member onUnitType : unit -> ExpressionType
+    default this.onUnitType () = ExpressionType.UnitType
 
     abstract member onQubit : unit -> ExpressionType
     default this.onQubit () = ExpressionType.Qubit
@@ -88,24 +105,28 @@ type TypeTransformationBase() =
     abstract member onRange : unit -> ExpressionType
     default this.onRange () = ExpressionType.Range
 
+
+    // transformation root called on each node
+
     member this.Transform (t : ResolvedType) =
-        match t.Resolution with
-        | ExpressionType.UnitType                    -> this.onUnitType ()
-        | ExpressionType.Operation ((it, ot), fs)    -> this.onOperation ((it, ot), fs)
-        | ExpressionType.Function (it, ot)           -> this.onFunction (it, ot)
-        | ExpressionType.TupleType ts                -> this.onTupleType ts
-        | ExpressionType.ArrayType b                 -> this.onArrayType b
-        | ExpressionType.UserDefinedType udt         -> this.onUserDefinedType udt
-        | ExpressionType.TypeParameter tp            -> this.onTypeParameter tp
-        | ExpressionType.Qubit                       -> this.onQubit ()
-        | ExpressionType.MissingType                 -> this.onMissingType ()
-        | ExpressionType.InvalidType                 -> this.onInvalidType ()
-        | ExpressionType.Int                         -> this.onInt ()
-        | ExpressionType.BigInt                      -> this.onBigInt ()
-        | ExpressionType.Double                      -> this.onDouble ()
-        | ExpressionType.Bool                        -> this.onBool ()
-        | ExpressionType.String                      -> this.onString ()
-        | ExpressionType.Result                      -> this.onResult ()
-        | ExpressionType.Pauli                       -> this.onPauli ()
-        | ExpressionType.Range                       -> this.onRange ()
-        |> ResolvedType.New
+        if options.Disable then t else
+        let typeKind = t.Resolution |> function
+            | ExpressionType.UnitType                    -> this.onUnitType ()
+            | ExpressionType.Operation ((it, ot), fs)    -> this.onOperation ((it, ot), fs)
+            | ExpressionType.Function (it, ot)           -> this.onFunction (it, ot)
+            | ExpressionType.TupleType ts                -> this.onTupleType ts
+            | ExpressionType.ArrayType b                 -> this.onArrayType b
+            | ExpressionType.UserDefinedType udt         -> this.onUserDefinedType udt
+            | ExpressionType.TypeParameter tp            -> this.onTypeParameter tp
+            | ExpressionType.Qubit                       -> this.onQubit ()
+            | ExpressionType.MissingType                 -> this.onMissingType ()
+            | ExpressionType.InvalidType                 -> this.onInvalidType ()
+            | ExpressionType.Int                         -> this.onInt ()
+            | ExpressionType.BigInt                      -> this.onBigInt ()
+            | ExpressionType.Double                      -> this.onDouble ()
+            | ExpressionType.Bool                        -> this.onBool ()
+            | ExpressionType.String                      -> this.onString ()
+            | ExpressionType.Result                      -> this.onResult ()
+            | ExpressionType.Pauli                       -> this.onPauli ()
+            | ExpressionType.Range                       -> this.onRange ()
+        ResolvedType.New |> Node.BuildOr t typeKind
