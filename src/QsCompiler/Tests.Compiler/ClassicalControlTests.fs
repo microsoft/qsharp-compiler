b77bc78a14acc0e210e0b1eaf08dfa6df576eb77
Bettina Heim <beheim@microsoft.com>
2020-02-18 16:00:02 -0800

    Revert "getting a proper diff"
    This reverts commit 59b9596323890bd4e335bd84ae1435a24c5d075e.

--- src/QsCompiler/Tests.Compiler/ClassicalControlTests.fs
+++ src/QsCompiler/Tests.Compiler/ClassicalControlTests.fs
@@ -69,8 +69,9 @@ type ClassicalControlTests () =
         |> writer.Statements.Transform
         |> ignore
 
-        (writer.Statements :?> ScopeToQs).Output.Split(Environment.NewLine)
-        |> Array.filter (fun str -> str <> String.Empty)
+        writer.InternalState.StatementOutputHandle 
+        |> Seq.filter (not << String.IsNullOrWhiteSpace)
+        |> Seq.toArray
 
     let CheckIfLineIsCall ``namespace`` name input =
         let call = sprintf @"(%s\.)?%s" <| Regex.Escape ``namespace`` <| Regex.Escape name
