93a95d82d628c9218c55400eeecaaf1ba13d1722
Scott Carda <sccarda@microsoft.com>
2020-03-18 11:51:04 -0700

    Brought the rewrite step files closer to a standard. Used Hash set logic instead of LINQ All/Any for preconditions.

--- src/QsCompiler/Compiler/RewriteSteps/FunctorGeneration.cs
+++ src/QsCompiler/Compiler/RewriteSteps/FunctorGeneration.cs
@@ -10,28 +10,18 @@ namespace Microsoft.Quantum.QsCompiler.BuiltInRewriteSteps
 {
     internal class FunctorGeneration : IRewriteStep
     {
-        public string Name { get; }
-        public int Priority { get; }
+        public string Name => "Functor Generation";
+        public int Priority => 10; // Not used for built-in transformations like this
         public IDictionary<string, string> AssemblyConstants { get; }
-        public IEnumerable<IRewriteStep.Diagnostic> GeneratedDiagnostics { get; }
+        public IEnumerable<IRewriteStep.Diagnostic> GeneratedDiagnostics => null;
 
-        public bool ImplementsTransformation { get; }
-        public bool ImplementsPreconditionVerification { get; }
-        public bool ImplementsPostconditionVerification { get; }
+        public bool ImplementsPreconditionVerification => true;
+        public bool ImplementsTransformation => true;
+        public bool ImplementsPostconditionVerification => false;
 
         public FunctorGeneration()
         {
-            Name = "Functor Generation";
-            Priority = 10; // Not used for built-in transformations like this
             AssemblyConstants = new Dictionary<string, string>();
-            ImplementsTransformation = true;
-            ImplementsPreconditionVerification = true;
-            ImplementsPostconditionVerification = false;
-        }
-
-        public bool Transformation(QsCompilation compilation, out QsCompilation transformed)
-        {
-            return CodeGeneration.GenerateFunctorSpecializations(compilation, out transformed);
         }
 
         public bool PreconditionVerification(QsCompilation compilation)
@@ -44,14 +34,22 @@ namespace Microsoft.Quantum.QsCompiler.BuiltInRewriteSteps
                 return false;
             }
 
-            var providedOperations = new QsNamespace[] { requiredNamespace }.Callables().Select(c => c.FullName);
-            var requiredBuiltIns = new List<QsQualifiedName>()
+            var providedOperations = new QsNamespace[] { requiredNamespace }
+                .Callables()
+                .Select(c => c.FullName)
+                .ToHashSet();
+            var requiredBuiltIns = new HashSet<QsQualifiedName>()
             {
                 BuiltIn.Length.FullName,
                 BuiltIn.RangeReverse.FullName
             };
 
-            return requiredBuiltIns.All(builtIn => providedOperations.Any(provided => provided.Equals(builtIn)));
+            return requiredBuiltIns.IsSubsetOf(providedOperations);
+        }
+
+        public bool Transformation(QsCompilation compilation, out QsCompilation transformed)
+        {
+            return CodeGeneration.GenerateFunctorSpecializations(compilation, out transformed);
         }
 
         public bool PostconditionVerification(QsCompilation compilation)
--- src/QsCompiler/Compiler/RewriteSteps/IntrinsicResolution.cs
+++ src/QsCompiler/Compiler/RewriteSteps/IntrinsicResolution.cs
@@ -10,38 +10,32 @@ namespace Microsoft.Quantum.QsCompiler.BuiltInRewriteSteps
 {
     internal class IntrinsicResolution : IRewriteStep
     {
-        public string Name { get; }
-        public int Priority { get; }
+        public string Name => "Intrinsic Resolution";
+        public int Priority => 10; // Not used for built-in transformations like this
         public IDictionary<string, string> AssemblyConstants { get; }
-        public IEnumerable<IRewriteStep.Diagnostic> GeneratedDiagnostics { get; }
+        public IEnumerable<IRewriteStep.Diagnostic> GeneratedDiagnostics => null;
 
-        public bool ImplementsTransformation { get; }
-        public bool ImplementsPreconditionVerification { get; }
-        public bool ImplementsPostconditionVerification { get; }
+        public bool ImplementsPreconditionVerification => false;
+        public bool ImplementsTransformation => true;
+        public bool ImplementsPostconditionVerification => false;
 
         private QsCompilation Environment { get; }
 
         public IntrinsicResolution(QsCompilation environment)
         {
-            Name = "IntrinsicResolution";
-            Priority = 10; // Not used for built-in transformations like this
             AssemblyConstants = new Dictionary<string, string>();
-            ImplementsTransformation = true;
-            ImplementsPreconditionVerification = false;
-            ImplementsPostconditionVerification = false;
-
             Environment = environment;
+        }
+
+        public bool PreconditionVerification(QsCompilation compilation)
+        {
+            throw new System.NotImplementedException();
         }
 
         public bool Transformation(QsCompilation compilation, out QsCompilation transformed)
         {
             transformed = ReplaceWithTargetIntrinsics.Apply(this.Environment, compilation);
             return true;
-        }
-
-        public bool PreconditionVerification(QsCompilation compilation)
-        {
-            throw new System.NotImplementedException();
         }
 
         public bool PostconditionVerification(QsCompilation compilation)
