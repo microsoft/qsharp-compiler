65de15483c4f7245bfc4791186f6ab3ac0024a59
Ricardo Espinoza <ricardoe@microsoft.com>
2020-06-23 01:16:37 -0700

    Fixing Yeoman typescript dependencies.

--- src/VSCodeExtension/src/generator-qsharp/app/index.ts
+++ src/VSCodeExtension/src/generator-qsharp/app/index.ts
@@ -1,40 +1,40 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+import yo = require("yeoman-generator");
+import yosay = require("yosay");
 
-export default class extends Generator {
+export default class QSharpGenerator extends yo {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the spectacular ${chalk.red('generator-qsharp')} generator!`)
+      yosay("Welcome to the spectacular Q# generator!")
     );
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
+        type: "confirm",
+        name: "someAnswer",
+        message: "Would you like to enable this option?",
         default: true
       }
     ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
-      this.props = props;
+      // TODO: Store the properties in QSharpGenerator object
     });
   }
 
   writing() {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath("dummyfile.txt"),
+      this.destinationPath("dummyfile.txt")
     );
   }
 
   install() {
     this.installDependencies();
   }
-};
+}
+
