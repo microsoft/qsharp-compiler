44960a4a1aa8b5873c4fe33099431e169e95c627
Ricardo Espinoza <43434922+ricardo-espinoza@users.noreply.github.com>
2020-06-22 17:46:30 -0700

    Update src/VSCodeExtension/src/generator-qsharp/app/index.ts
    Co-authored-by: Chris Granade <chgranad@microsoft.com>

--- src/VSCodeExtension/src/generator-qsharp/app/index.ts
+++ src/VSCodeExtension/src/generator-qsharp/app/index.ts
@@ -6,7 +6,7 @@ const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 
-module.exports = class extends Generator {
+export default class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
