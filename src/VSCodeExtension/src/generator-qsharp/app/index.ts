4116f53c74c05ffa9f8932295471d2ab2e4e96ea
Ricardo Espinoza <43434922+ricardo-espinoza@users.noreply.github.com>
2020-06-22 17:47:36 -0700

    Update src/VSCodeExtension/src/generator-qsharp/app/index.ts
    Co-authored-by: Chris Granade <chgranad@microsoft.com>

--- src/VSCodeExtension/src/generator-qsharp/app/index.ts
+++ src/VSCodeExtension/src/generator-qsharp/app/index.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
