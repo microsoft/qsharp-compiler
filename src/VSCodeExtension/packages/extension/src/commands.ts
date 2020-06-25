9d0bcfbddb41dbb5903740048e7cec7359168a87
Ricardo Espinoza <ricardoe@microsoft.com>
2020-06-25 14:52:23 -0700

    Temporarily disabling Yeoman extension dependencies to avoid npm security issues.

--- src/VSCodeExtension/packages/extension/src/commands.ts
+++ src/VSCodeExtension/packages/extension/src/commands.ts
@@ -10,7 +10,7 @@ import { IPackageInfo } from './packageInfo';
 import * as semver from 'semver';
 import { promisify } from 'util';
 
-import * as yeoman from 'yeoman-environment';
+//import * as yeoman from 'yeoman-environment';
 
 export function registerCommand(context: vscode.ExtensionContext, name: string, action: () => void) {
     context.subscriptions.push(
@@ -89,8 +89,8 @@ function createNewProjectAtUri(dotNetSdk: DotnetInfo, projectType: string, uri: 
 
 export function createNewProject(dotNetSdk: DotnetInfo) {
     // TODO: finish switching over to Yeoman.
-    let env = yeoman.createEnv();
-    env.register(require.resolve('generator-qsharp'), 'qsharp:app');
+    // let env = yeoman.createEnv();
+    // env.register(require.resolve('generator-qsharp'), 'qsharp:app');
 
     const projectTypes: {[key: string]: string} = {
         "Standalone console application": "console",
