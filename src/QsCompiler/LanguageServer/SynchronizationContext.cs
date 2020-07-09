debd2bbb097130f226abbfe73a69eb7420d663b5
Sarah Marshall <sarah.marshall@microsoft.com>
2020-07-09 03:22:26 -0700

    Add StyleCop to LanguageServer

--- src/QsCompiler/LanguageServer/SynchronizationContext.cs
+++ src/QsCompiler/LanguageServer/SynchronizationContext.cs
@@ -6,29 +6,35 @@ using System.Threading;
 using Microsoft.Quantum.QsCompiler;
 using Microsoft.VisualStudio.Threading;
 
-
 namespace Microsoft.Quantum.QsLanguageServer
 {
     /// <summary>
-    /// Used to enforce in-order processing of the communication with the Q# language server. 
-    /// Such a synchronization context is needed since the Q# language server 
+    /// Used to enforce in-order processing of the communication with the Q# language server.
+    /// Such a synchronization context is needed since the Q# language server
     /// processes changes incrementally rather than reprocessing entire files each time.
     /// </summary>
     public class QsSynchronizationContext : SynchronizationContext
     {
         private readonly AsyncQueue<(SendOrPostCallback, object)> queued = new AsyncQueue<(SendOrPostCallback, object)>();
+
         private void ProcessNext()
         {
             var gotNext = this.queued.TryDequeue(out (SendOrPostCallback, object) next);
             QsCompilerError.Verify(gotNext, "nothing to process in the SynchronizationContext");
-            if (gotNext) next.Item1(next.Item2);
+            if (gotNext)
+            {
+                next.Item1(next.Item2);
+            }
         }
 
         public override void Post(SendOrPostCallback fct, object arg)
         {
-            if (fct == null) throw new ArgumentNullException(nameof(fct));
-            this.queued.Enqueue((fct,arg));
-            base.Send(_ => ProcessNext(), null);
+            if (fct == null)
+            {
+                throw new ArgumentNullException(nameof(fct));
+            }
+            this.queued.Enqueue((fct, arg));
+            this.Send(_ => this.ProcessNext(), null);
         }
     }
 }
