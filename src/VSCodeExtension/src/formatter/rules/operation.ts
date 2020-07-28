d71b4ae268665db7804add59ed36b65e6cee3deb
Raphael Koh <t-rkoh@microsoft.com>
2020-07-28 11:26:56 -0400

    Add tests for operation args rule

--- src/VSCodeExtension/src/formatter/rules/operation.ts
+++ src/VSCodeExtension/src/formatter/rules/operation.ts
@@ -6,21 +6,27 @@
  *
  * @param code incoming code
  *
- * @example `operation   Foo(q:Qubit, n   :Int)   :  Bool` ->
+ * @example `operation   Foo(q:Qubit,  n   :Int)   :  Bool` ->
  * `operation Foo (q : Qubit, n : Int) : Bool`
  */
 export const argsRule = (code: string): string => {
-    const operationMatcher: RegExp = /operation\s*(\w+)\s*\((.*)\)\s*:\s*(\w+)/g;
-    const argumentMatcher: RegExp = /((\w*)\s*:\s*([a-zA-Z]+))/g;
+    const operationMatcher: RegExp = /operation\s*(\w+)\s*(\(.*\))\s*:\s*(\w+)/g;
+    const firstArgMatcher: RegExp = /\(((\w*)\s*:\s*([a-zA-Z]+))/g
+    const restArgMatcher: RegExp = /,\s*((\w*)\s*:\s*([a-zA-Z]+))/g;
 
     return code.replace(operationMatcher, (match, opName, args, retType) => {
-        args = args.replace(
-            argumentMatcher,
-            (match: string, group1: string, variable: string, type: string) => {
-                return `${variable} : ${type}`;
-            }
-        );
-        return `operation ${opName} (${args}) : ${retType}`;
+        args = args
+            .replace(
+                firstArgMatcher,
+                (match: string, group: string, variable: string, type: string) =>
+                    `(${variable} : ${type}`
+            )
+            .replace(
+                restArgMatcher,
+                (match: string, group: string, variable: string, type: string) =>
+                    `, ${variable} : ${type}`
+            );
+        return `operation ${opName} ${args} : ${retType}`;
     });
 };
 
---
+++ src/VSCodeExtension/src/tests/unit/operation.test.ts
@@ -0,0 +1,55 @@
+import "mocha";
+import * as assert from "assert";
+import { formatter } from "../../formatter/formatter";
+import { argsRule } from "../../formatter/rules/operation";
+
+describe("arguments rule", () => {
+    it("no error", () => {
+        const code = "operation Foo (q : Qubit, n : Int) : Bool";
+        const expectedCode = "operation Foo (q : Qubit, n : Int) : Bool";
+
+        assert.equal(formatter(code, [argsRule]), expectedCode);
+    });
+
+    it("keep leading whitespace", () => {
+        const code = "  operation Foo (q : Qubit, n : Int) : Bool";
+        const expectedCode = "  operation Foo (q : Qubit, n : Int) : Bool";
+
+        assert.equal(formatter(code, [argsRule]), expectedCode);
+    });
+
+    it("remove unnecessary spaces (non-arguments)", () => {
+        const expectedCode = "operation Foo (q : Qubit, n : Int) : Bool";
+
+        let code = "operation   Foo (q : Qubit, n : Int) : Bool";
+        assert.equal(formatter(code, [argsRule]), expectedCode);
+
+        code = "operation Foo   (q : Qubit, n : Int) : Bool";
+        assert.equal(formatter(code, [argsRule]), expectedCode);
+
+        code = "operation Foo (q : Qubit, n : Int)   : Bool";
+        assert.equal(formatter(code, [argsRule]), expectedCode);
+
+        code = "operation Foo (q : Qubit, n : Int) :   Bool";
+        assert.equal(formatter(code, [argsRule]), expectedCode);
+    });
+
+    it("remove unnecessary spaces (arguments)", () => {
+        const expectedCode = "operation Foo (q : Qubit, n : Int) : Bool";
+
+        let code = "operation Foo (q   : Qubit, n : Int) : Bool";
+        assert.equal(formatter(code, [argsRule]), expectedCode);
+
+        code = "operation Foo (q :    Qubit, n : Int) : Bool";
+        assert.equal(formatter(code, [argsRule]), expectedCode);
+
+        code = "operation Foo (q : Qubit, n   : Int) : Bool";
+        assert.equal(formatter(code, [argsRule]), expectedCode);
+
+        code = "operation Foo (q : Qubit, n :   Int) : Bool";
+        assert.equal(formatter(code, [argsRule]), expectedCode);
+
+        code = "operation Foo (q : Qubit,   n : Int) : Bool";
+        assert.equal(formatter(code, [argsRule]), expectedCode);
+    });
+});
