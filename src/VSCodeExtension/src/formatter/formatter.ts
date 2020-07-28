8d25bf06ebc2c36a4c27b22f70f001282b018adf
Andres Rondon <amrondonp@gmail.com>
2020-07-28 14:11:23 -0700

    initial tests

--- src/VSCodeExtension/src/formatter/formatter.ts
+++ src/VSCodeExtension/src/formatter/formatter.ts
@@ -5,3 +5,14 @@ export type FormatRule = (code: string) => string;
 
 export const formatter = (code: string, rules: FormatRule[]): string =>
     rules.reduce((formattedCode, rule) => rule(formattedCode), code);
+
+export const withCommentsIgnored = (rule: FormatRule): FormatRule => {
+    return (code: string) => {
+        const tokenizedCode = code.split(/(\/\/.*)/g);
+        return tokenizedCode.reduce((previousCode, token) => {
+            const tokenIsComment = token.startsWith("//")
+            const formattedCode = tokenIsComment ? token : rule(token);
+            return previousCode + formattedCode;
+        }, "");
+    }
+};  
--- src/VSCodeExtension/src/tests/unit/formatter.test.ts
+++ src/VSCodeExtension/src/tests/unit/formatter.test.ts
@@ -1,6 +1,6 @@
 import "mocha";
 import * as assert from "assert";
-import { formatter } from "../../formatter/formatter";
+import { formatter, withCommentsIgnored } from "../../formatter/formatter";
 
 describe("formatter tests", () => {
   it("has no rules", () => {
@@ -8,5 +8,28 @@ describe("formatter tests", () => {
     const expectedCode = "if(1 == 1){H(qs[0]);}";
 
     assert.equal(formatter(code, []), expectedCode);
+  });
+
+  it("ignoring comments high level function", () => {
+    const code =
+      `part1 = 1;
+// comment here
+part2 = 2;`;
+
+    const ruleCalledWith: string[] = [];
+
+    const mockRule = (ruleCode: string) => {
+      ruleCalledWith.push(ruleCode);
+      return ruleCode;
+    }
+
+    const decoratedRule = withCommentsIgnored(mockRule);
+    const formattedCode = formatter(code, [decoratedRule]);
+
+    assert.equal(ruleCalledWith.length, 2);
+    assert.equal(ruleCalledWith[0], "part1 = 1;\n");
+    assert.equal(ruleCalledWith[1], "\npart2 = 2;");
+
+    assert.equal(formattedCode, code);
   });
 });
