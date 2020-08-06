ede09b348412d9b6e683813c447a2fbda3d0ab06
Sarah Marshall <sarah.marshall@microsoft.com>
2020-08-06 15:53:36 -0700

    Use lambda in CheckDefinedTypesForCycles

--- src/QsCompiler/SyntaxProcessor/TreeVerification.fs
+++ src/QsCompiler/SyntaxProcessor/TreeVerification.fs
@@ -191,5 +191,5 @@ let CheckDefinedTypesForCycles (definitions : ImmutableArray<TypeDeclarationHead
             let udt = definitions.[udtIndex]
             let loc = getLocation udt
             ((loc.Offset, udt.SourceFile), loc.Range |> QsCompilerDiagnostic.Error (ErrorCode.TypeIsPartOfCyclicDeclaration, [])) |> diagnostics.Add        
-    let updateDiagnosticRange ((pos, _), msg) = { msg with QsCompilerDiagnostic.Range = pos + msg.Range } 
-    diagnostics.ToLookup(fst >> snd, updateDiagnosticRange)
+    diagnostics.ToLookup (fst >> snd, fun ((position, _), diagnostic) ->
+        { diagnostic with QsCompilerDiagnostic.Range = position + diagnostic.Range })
