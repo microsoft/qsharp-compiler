24854277674a4be58b53112c68474c7263e19434
Ryan Shaffer <rmshaffer@gmail.com>
2020-08-22 16:24:18 -0700

    Don't test with .qs files

--- src/QsCompiler/Tests.LanguageServer/Tests.cs
+++ src/QsCompiler/Tests.LanguageServer/Tests.cs
@@ -240,7 +240,6 @@ namespace Microsoft.Quantum.QsLanguageServer.Testing
             }
             await RunTest(emptyLastLine: true, useQsExtension: false);
             await RunTest(emptyLastLine: false, useQsExtension: false);
-            await RunTest(emptyLastLine: true, useQsExtension: true);
         }
 
         [TestMethod]
