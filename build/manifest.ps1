4f30e9faa9a9fe9901c9687c4495b6d263ed3367
Christopher Granade <cgranade@cgranade.com>
2020-08-27 08:39:10 -0700

    Fix signing step.

--- build/manifest.ps1
+++ build/manifest.ps1
@@ -8,7 +8,7 @@
 
 if ("$Env:ENABLE_VSIX" -ne "false") {
     $VsixAssemblies = @(
-        "./src/DocumentationGenerator/bin/$Env:BUILD_CONFIGURATION/netcoreapp3.1/Microsoft.Quantum.DocumentationGenerator.dll",
+        "./src/QsCompiler/DocumentationGenerator/bin/$Env:BUILD_CONFIGURATION/netcoreapp3.1/Microsoft.Quantum.DocumentationGenerator.dll",
         "./src/QsCompiler/LanguageServer/bin/$Env:BUILD_CONFIGURATION/netcoreapp3.1/Microsoft.Quantum.QsCompilationManager.dll",
         "./src/QsCompiler/LanguageServer/bin/$Env:BUILD_CONFIGURATION/netcoreapp3.1/Microsoft.Quantum.QsCore.dll",
         "./src/QsCompiler/LanguageServer/bin/$Env:BUILD_CONFIGURATION/netcoreapp3.1/Microsoft.Quantum.QsDataStructures.dll",
