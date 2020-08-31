dd537ed44380786a668d59e0996669f4a9259309
Sarah Marshall <sarah.marshall@microsoft.com>
2020-08-31 11:39:16 -0700

    Clarify meaning of priority number

--- src/QsCompiler/Compiler/PluginInterface.cs
+++ src/QsCompiler/Compiler/PluginInterface.cs
@@ -11,7 +11,8 @@ using VS = Microsoft.VisualStudio.LanguageServer.Protocol;
 namespace Microsoft.Quantum.QsCompiler
 {
     /// <summary>
-    /// Lists the priorities for built-in rewrite steps.
+    /// Lists the priorities for built-in rewrite steps. Steps with a larger priority number have higher priority and
+    /// will be executed first.
     /// </summary>
     public static class RewriteStepPriorities
     {
@@ -121,7 +122,7 @@ namespace Microsoft.Quantum.QsCompiler
 
         /// <summary>
         /// The priority of the transformation relative to other transformations within the same dll or package.
-        /// Steps with higher priority will be executed first.
+        /// Steps with a larger priority number have higher priority and will be executed first.
         /// </summary>
         public int Priority { get; }
 
