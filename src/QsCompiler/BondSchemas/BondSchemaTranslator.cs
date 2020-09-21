cc54fbeba4957cc27655c53f564fcea3ef1ad969
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2020-09-20 20:15:35 -0700

    Created base structure for the Bond schema translator.

---
+++ src/QsCompiler/BondSchemas/BondSchemaTranslator.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.Quantum.QsCompiler.DataTypes;
+
+namespace Microsoft.Quantum.QsCompiler.BondSchemas
+{
+    public static class BondSchemaTranslator
+    {
+        public static QsCompilation CreateBondCompilation(SyntaxTree.QsCompilation qsCompilation) =>
+            new QsCompilation
+            {
+                // TODO: Implement.
+            };
+    }
+}
