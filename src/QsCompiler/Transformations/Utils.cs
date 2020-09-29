1e8c8b4d85e70c8f3d1a2e9e49fe319567397ccb
Sarah Marshall <sarah.marshall@microsoft.com>
2020-09-29 11:18:46 -0700

    Use InternalsVisibleTo

--- src/QsCompiler/Transformations/Utils.cs
+++ src/QsCompiler/Transformations/Utils.cs
@@ -7,23 +7,23 @@ namespace Microsoft.Quantum.QsCompiler.Transformations
     /// <summary>
     /// Helpful utility functions.
     /// </summary>
-    public static class Utils
+    internal static class Utils
     {
         /// <summary>
         /// Applies the action <paramref name="f"/> to the value <paramref name="x"/>.
         /// </summary>
-        public static void Apply<T>(this T x, Action<T> f) => f(x);
+        internal static void Apply<T>(this T x, Action<T> f) => f(x);
 
         /// <summary>
         /// Applies the function <paramref name="f"/> to the value <paramref name="x"/> and returns the result.
         /// </summary>
-        public static TOut Apply<TIn, TOut>(this TIn x, Func<TIn, TOut> f) => f(x);
+        internal static TOut Apply<TIn, TOut>(this TIn x, Func<TIn, TOut> f) => f(x);
 
         /// <summary>
         /// Projects each element of a sequence into a new form and discards null values.
         /// </summary>
         /// <remarks>Overload for reference types.</remarks>
-        public static IEnumerable<TResult> SelectNotNull<TSource, TResult>(
+        internal static IEnumerable<TResult> SelectNotNull<TSource, TResult>(
             this IEnumerable<TSource> source, Func<TSource, TResult?> selector)
             where TResult : class =>
             source.SelectMany(item =>
@@ -34,7 +34,7 @@ namespace Microsoft.Quantum.QsCompiler.Transformations
         /// Projects each element of a sequence into a new form and discards null values.
         /// </summary>
         /// <remarks>Overload for value types.</remarks>
-        public static IEnumerable<TResult> SelectNotNull<TSource, TResult>(
+        internal static IEnumerable<TResult> SelectNotNull<TSource, TResult>(
             this IEnumerable<TSource> source, Func<TSource, TResult?> selector)
             where TResult : struct =>
             source.SelectMany(item =>
