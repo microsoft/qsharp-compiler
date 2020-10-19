c2e5b22630eeb0426b9c9a3c701329f6ef909b98
Sarah Marshall <sarah.marshall@microsoft.com>
2020-10-19 15:50:53 -0700

    Add new runtime capability names

--- src/QsCompiler/DataStructures/ReservedKeywords.fs
+++ src/QsCompiler/DataStructures/ReservedKeywords.fs
@@ -3,6 +3,7 @@
 
 namespace Microsoft.Quantum.QsCompiler.ReservedKeywords
 
+open System
 open System.Collections.Immutable
 
 
@@ -248,20 +249,24 @@ module AssemblyConstants =
     let ResourcesEstimator = "ResourcesEstimator"
     let ExposeReferencesViaTestNames = "ExposeReferencesViaTestNames"
 
-    /// The runtime capabilities supported by an execution target.
-    type RuntimeCapabilities = 
-        // Note: The names of the capabilities here need to match the ones defined by the Sdk.
+    /// The runtime capabilities supported by an execution target. The names of the capabilities here match the ones
+    /// defined by the SDK.
+    type RuntimeCapabilities =
+        // RELEASE 2021-04: Remove Unknown, QPRGen0, and QPRGen1.
 
-        /// No known runtime restrictions. Any Q# program can be executed.
-        | Unknown = 0
+        /// No runtime restrictions. Any Q# program can be executed.
+        | FullComputation = 0
+        | [<Obsolete "Renamed to FullComputation.">] Unknown = 0
 
         /// Measurement results cannot be compared for equality.
-        | QPRGen0 = 1
+        | BasicQuantumFunctionality = 1
+        | [<Obsolete "Renamed to BasicQuantumFunctionality.">] QPRGen0 = 1
 
         /// Measurement results can be compared for equality only in if-statement conditional expressions in operations.
         /// The block of an if-statement that depends on a result cannot contain set statements for mutable variables
         /// declared outside the block, or return statements.
-        | QPRGen1 = 2
+        | BasicMeasurementFeedback = 2
+        | [<Obsolete "Renamed to BasicMeasurementFeedback.">] QPRGen1 = 2
 
 
 /// contains reserved names for command line arguments of Q# projects
