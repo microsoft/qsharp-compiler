c3087bc964f0568fc10770d70276111756c63610
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2020-10-31 13:17:05 -0700

    Added test file.

---
+++ src/QsCompiler/Tests.Compiler/CompilationLoaderTests.fs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Quantum.QsCompiler.Testing
+
+open Xunit
+open Xunit.Abstractions
+
+type CompilationLoaderTests (output:ITestOutputHelper) =
+    [<Fact>]
+    member this.``Basic Write Read Binary`` () =
+        Assert.True(true)
