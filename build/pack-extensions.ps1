4dd7b0af9414777192f21ffd2fdbd8ce94b8973a
Andres Paz <anpaz@microsoft.com>
2020-11-23 11:48:27 -0800

    Missing ASSEMBLY_CONSTANTS
    (cherry picked from commit 88debafc7b7bcedc974c1c115998df74a954a1f9)

--- build/pack-extensions.ps1
+++ build/pack-extensions.ps1
@@ -57,6 +57,13 @@ function Pack-SelfContained() {
         [string] $PackageData = $null
     );
 
+    Write-Host "##[info]Building $project ($action)..."
+    if ("" -ne "$Env:ASSEMBLY_CONSTANTS") {
+        $args = @("/property:DefineConstants=$Env:ASSEMBLY_CONSTANTS");
+    }  else {
+        $args = @();
+    }
+
     # Make sure the LanguageServer is built on its own:
     dotnet build (Join-Path $PSScriptRoot $project) `
         -c $Env:BUILD_CONFIGURATION `
@@ -75,11 +82,6 @@ function Pack-SelfContained() {
         New-Item -ItemType Directory -Path $ArchiveDir -Force -ErrorAction SilentlyContinue;
 
         try {
-            if ("" -ne "$Env:ASSEMBLY_CONSTANTS") {
-                $args = @("/property:DefineConstants=$Env:ASSEMBLY_CONSTANTS");
-            }  else {
-                $args = @();
-            }
             $ArchivePath = Join-Path $ArchiveDir "$BaseName-$DotNetRuntimeID-$Env:SEMVER_VERSION.zip";
             dotnet publish (Join-Path $PSScriptRoot $Project) `
                 -c $Env:BUILD_CONFIGURATION `
