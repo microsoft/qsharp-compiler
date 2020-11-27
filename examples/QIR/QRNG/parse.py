a55817c2090b37c8f9af63e5d8e646c54046bf31
Dave Wecker <wecker@microsoft.com>
2020-11-26 16:23:21 -0800

    Working on parser

--- examples/QIR/QRNG/parse.py
+++ examples/QIR/QRNG/parse.py
@@ -1,20 +1,74 @@
 from  pprint import pprint
 import llvmlite.binding as bnd
+import re
+
+reCall1  = re.compile(r'\s*(\S+) = +call (\S+) @([^(]+)\(([^)]*)\)')
+reCall2  = re.compile(r'\s*call void @([^(]+)\(([^)]*)\)')
+reRet1   = re.compile(r'\s*ret (\S+) (\S+)')
+reStore1 = re.compile(r'\s*store (\S+) (\S+), (\S+) (\S+)')
+reBr1    = re.compile(r'\s*br label (\S+)')
 
 with open("QRNG.ll","r") as file:
     text = file.read()
-print(text)
+#print(text)
 
 mod = bnd.parse_assembly(text)
 
+def parseIns(ins):
+    line = str(ins).replace("%","v_")
+    if ins.opcode == 'call':
+        m1 = reCall1.match(line)
+        m2 = reCall2.match(line)
+        if m1 is not None:
+            if m1.group(3) == '__quantum__qis__measure':
+                print(f'        int {m1.group(1)} = 1;')
+        elif m2 is not None:
+            print(f'            ### CALL_2 1={m2.group(1)} 2={m2.group(2)}')
+        else:
+            print(f'            ### call {line}')
+    elif ins.opcode == 'ret':
+        m1 = reRet1.match(line)
+        if m1 is not None:
+            print(f'        return {m1.group(2)};')
+        else:
+            print(f'            ### ret {line}')
+
+    elif ins.opcode == "alloca":
+        print(f'        int v_{ins.name};')
+    elif ins.opcode == "store":
+        m1 = reStore1.match(line)
+        if m1 is not None:
+            print(f'        {m1.group(4)} = {m1.group(2)};')
+        else:
+            print(f'            ### ret {line}')
+
+    elif ins.opcode == "br":
+        m1 = reBr1.match(line)
+        if m1 is not None:
+            print(f'        blkNam = "{m1.group(1)}";')
+        else:
+            print(f'            ### ret {line}')
+
+    else:
+        print(f'            ### INSTR n={ins.name} o={ins.opcode} r={line}')
+
+def parseBlock(blk):
+    print(f'  case "{blk.name}"":')
+    for ins in blk.instructions:
+        parseIns(ins)
+    print('    break;')
+
+def parseFunc(func):
+    print(f'void {func.name}() {{')
+    print('  char* blkNam = "entry";')
+    print('  while (1) switch blkNam {')
+    for blk in func.blocks:
+        parseBlock(blk)
+    print('  }')
+    print('}')
+
 for func in mod.functions:
     if func.name.startswith('Qrng'):
-        print(f'### NAME={func.name}')
-        for blk in func.blocks:
-            print(f'    ### BLOCK {blk.name}')
-            for ins in blk.instructions:
-                print(f'        ### INSTR n={ins.name} o={ins.opcode} r={ins}')
-                #for op in ins.operands:
-                #    print(f'          ### OP n={op.name} r={op} ')
+        parseFunc(func)
                 
 print('DONE')
