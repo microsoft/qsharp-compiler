fc058dc035492b48dffd7135a5a4a39b24212fea
Sarah Marshall <sarah.marshall@microsoft.com>
2021-01-26 15:19:46 -0800

    Re-enable execution tests

--- src/QsCompiler/Tests.Compiler/ExecutionTests.fs
+++ src/QsCompiler/Tests.Compiler/ExecutionTests.fs
@@ -39,12 +39,12 @@ type ExecutionTests(output: ITestOutputHelper) =
         AssertEqual expectedOutput out
 
 
-    [<Fact(Skip = "TODO")>]
+    [<Fact>]
     member this.``Loading via test names``() =
         ExecuteAndCompareOutput "LogViaTestName" "not implemented"
 
 
-    [<Fact(Skip = "TODO")>]
+    [<Fact>]
     member this.``Specialization Generation for Conjugations``() =
         ExecuteAndCompareOutput "ConjugationsInBody" "
                 U1
@@ -139,7 +139,7 @@ type ExecutionTests(output: ITestOutputHelper) =
             "
 
 
-    [<Fact(Skip = "TODO")>]
+    [<Fact>]
     member this.``Referencing Projects and Packages``() =
         ExecuteAndCompareOutput "PackageAndProjectReference" "
                 Welcome to Q#!
@@ -150,7 +150,7 @@ type ExecutionTests(output: ITestOutputHelper) =
                 [Complex((1, 0))]
             "
 
-    [<Fact(Skip = "TODO")>]
+    [<Fact>]
     member this.``Adjoint generation from expressions should be reversed``() =
         ExecuteAndCompareOutput "AdjointExpressions" "
                 1
