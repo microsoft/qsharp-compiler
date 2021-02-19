679bf91a046c486d52517688ec67c98e80dce40a
Sarah Marshall <sarah.marshall@microsoft.com>
2021-02-18 18:32:00 -0800

    Use correct subtyping relation in substitutions

--- src/QsCompiler/SyntaxProcessor/ExpressionVerification.fs
+++ src/QsCompiler/SyntaxProcessor/ExpressionVerification.fs
@@ -35,14 +35,6 @@ type private StripInferredInfoFromType() =
 
 let private StripInferredInfoFromType = (new StripInferredInfoFromType()).OnType
 
-/// used for type matching arguments in call-like expressions
-type private Variance =
-    | Covariant
-    | Contravariant
-    | Invariant
-
-let private invalid = InvalidType |> ResolvedType.New
-
 let private ExprWithoutTypeArgs isMutable (ex, t, dep, range) =
     let inferred = InferredExpressionInformation.New(isMutable = isMutable, quantumDep = dep)
     TypedExpression.New(ex, ImmutableDictionary.Empty, t, inferred, range)
@@ -58,103 +50,6 @@ let private missingFunctors (target: ImmutableHashSet<_>, given) =
     match given with
     | Some fList -> target.Except(fList) |> mapFunctors
     | None -> if target.Any() then target |> mapFunctors else [ "(None)" ]
-
-/// Return the string representation for a ResolveType.
-/// User defined types are represented by their full name.
-let internal toString (t: ResolvedType) = SyntaxTreeToQsharp.Default.ToCode t
-
-/// Given two resolve types, determines and returns a common base type if such a type exists,
-/// or pushes adds a suitable error using addError and returns invalid type if a common base type does not exist.
-/// Adds an ExpressionOfUnknownType error if either of the types contains a missing type.
-/// Adds an InvalidUseOfTypeParameterizedObject error if the types contain external type parameters,
-/// i.e. type parameters that do not belong to the given parent (callable specialization).
-/// Adds a ConstrainsTypeParameter error if an internal type parameter (i.e. one that belongs to the given parent) in one type
-/// does not correspond to the same type parameter in the other type (or an invalid type).
-/// Note: the only subtyping that occurs is due to operation supporting only a proper subset of the functors supported by their derived type.
-/// This subtyping carries over to tuple types containing operations, and callable types containing operations as within their in- and/or output type.
-/// However, arrays in particular are treated as invariant;
-/// i.e. an array of operations of type t1 are *not* a subtype of arrays of operations of type t2 even if t1 is a subtype of t2.
-let private CommonBaseType addError
-                           mismatchErr
-                           parent
-                           (lhsType: ResolvedType, lhsRange)
-                           (rhsType: ResolvedType, rhsRange)
-                           : ResolvedType =
-    let raiseError errCode (lhsCond, rhsCond) =
-        if lhsCond then lhsRange |> addError errCode
-        if rhsCond then rhsRange |> addError errCode
-        invalid
-
-    let rec matchInAndOutputType variance (i1, o1) (i2, o2) =
-        let inputVariance =
-            match variance with
-            | Covariant -> Contravariant
-            | Contravariant -> Covariant
-            | Invariant -> Invariant
-
-        let argType = matchTypes inputVariance (i1, i2) // variance changes for the argument type *only*
-        let resType = matchTypes variance (o1, o2)
-        argType, resType
-
-    and commonOpType variance ((i1, o1), s1: CallableInformation) ((i2, o2), s2: CallableInformation) =
-        let argType, resType = matchInAndOutputType variance (i1, o1) (i2, o2)
-
-        let characteristics =
-            match variance with
-            | Covariant -> CallableInformation.Common [ s1; s2 ]
-            | Contravariant -> // no information can ever be inferred in this case, since contravariance only occurs within the type signatures of passed callables
-                CallableInformation.New
-                    (Union(s1.Characteristics, s2.Characteristics) |> ResolvedCharacteristics.New,
-                     InferredCallableInformation.NoInformation)
-            | Invariant when s1.Characteristics.AreInvalid
-                             || s2.Characteristics.AreInvalid
-                             || s1.Characteristics.GetProperties().SetEquals(s2.Characteristics.GetProperties()) ->
-                let characteristics = if s1.Characteristics.AreInvalid then s2.Characteristics else s1.Characteristics
-
-                let inferred =
-                    InferredCallableInformation.Common [ s1.InferredInformation
-                                                         s2.InferredInformation ]
-
-                CallableInformation.New(characteristics, inferred)
-            | Invariant ->
-                raiseError mismatchErr (true, true) |> ignore
-
-                CallableInformation.New
-                    (ResolvedCharacteristics.New InvalidSetExpr, InferredCallableInformation.NoInformation)
-
-        QsTypeKind.Operation((argType, resType), characteristics) |> ResolvedType.New
-
-    and matchTypes variance (t1: ResolvedType, t2: ResolvedType) =
-        match t1.Resolution, t2.Resolution with
-        | _ when t1.isMissing || t2.isMissing ->
-            raiseError (ErrorCode.ExpressionOfUnknownType, []) (t1.isMissing, t2.isMissing)
-        | QsTypeKind.ArrayType b1, QsTypeKind.ArrayType b2 when b1.isMissing || b2.isMissing ->
-            if b1.isMissing then t2 else t1
-        | QsTypeKind.ArrayType b1, QsTypeKind.ArrayType b2 ->
-            matchTypes Invariant (b1, b2) |> ArrayType |> ResolvedType.New
-        | QsTypeKind.TupleType ts1, QsTypeKind.TupleType ts2 when ts1.Length = ts2.Length ->
-            (Seq.zip ts1 ts2 |> Seq.map (matchTypes variance)).ToImmutableArray()
-            |> TupleType
-            |> ResolvedType.New
-        | QsTypeKind.UserDefinedType udt1, QsTypeKind.UserDefinedType udt2 when udt1 = udt2 -> t1
-        | QsTypeKind.Operation ((i1, o1), l1), QsTypeKind.Operation ((i2, o2), l2) ->
-            commonOpType variance ((i1, o1), l1) ((i2, o2), l2)
-        | QsTypeKind.Function (i1, o1), QsTypeKind.Function (i2, o2) ->
-            matchInAndOutputType variance (i1, o1) (i2, o2) |> QsTypeKind.Function |> ResolvedType.New
-        | QsTypeKind.TypeParameter tp1, QsTypeKind.TypeParameter tp2 when tp1 = tp2 && tp1.Origin = parent -> t1
-        | QsTypeKind.TypeParameter tp1, QsTypeKind.TypeParameter tp2 when tp1 = tp2 ->
-            raiseError (ErrorCode.InvalidUseOfTypeParameterizedObject, []) (true, true)
-        | QsTypeKind.TypeParameter tp, QsTypeKind.InvalidType when tp.Origin = parent -> t1
-        | QsTypeKind.InvalidType, QsTypeKind.TypeParameter tp when tp.Origin = parent -> t2
-        | QsTypeKind.TypeParameter _, _ ->
-            raiseError (ErrorCode.ConstrainsTypeParameter, [ t1 |> toString ]) (true, false)
-        | _, QsTypeKind.TypeParameter _ ->
-            raiseError (ErrorCode.ConstrainsTypeParameter, [ t2 |> toString ]) (false, true)
-        | _ when t1.isInvalid || t2.isInvalid -> if t1.isInvalid then t2 else t1
-        | _ when t1 = t2 -> t1
-        | _ -> raiseError mismatchErr (true, true)
-
-    matchTypes Covariant (lhsType, rhsType)
 
 /// Calls the given addWarning function with a suitable warning code and the given range
 /// if the given expression contains an operation call.
