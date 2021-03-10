0d10b358467c73c76c3945d343d5c1e848e6fe2b
Stefan J. Wernli <swernli@microsoft.com>
2021-03-10 15:38:53 -0800

    Update and renames for CR feedback

--- src/QsCompiler/LlvmBindings/Instructions/Predicates.cs
+++ src/QsCompiler/LlvmBindings/Instructions/Predicates.cs
@@ -116,64 +116,6 @@ namespace Ubiquity.NET.Llvm.Instructions
         BadIcmpPredicate = LastIcmpPredicate + 1
     }
 
-    /*
-    TODO: extensions for predicate:
-      static bool isFPPredicate(Predicate P) {
-        return P >= FIRST_FCMP_PREDICATE && P <= LAST_FCMP_PREDICATE;
-      }
-
-      static bool isIntPredicate(Predicate P) {
-        return P >= FIRST_ICMP_PREDICATE && P <= LAST_ICMP_PREDICATE;
-      }
-
-      /// For example, EQ->EQ, SLE->SGE, ULT->UGT,
-      ///              OEQ->OEQ, ULE->UGE, OLT->OGT, etc.
-      /// @returns the predicate that would be the result of exchanging the two
-      /// operands of the CmpInst instruction without changing the result
-      /// produced.
-      /// @brief Return the predicate as if the operands were swapped
-      static Predicate getSwappedPredicate(Predicate pred);
-
-      /// For example, EQ -> NE, UGT -> ULE, SLT -> SGE,
-      ///              OEQ -> UNE, UGT -> OLE, OLT -> UGE, etc.
-      /// @returns the inverse predicate for predicate provided in \p pred.
-      /// @brief Return the inverse of a given predicate
-      static Predicate getInversePredicate(Predicate pred);
-
-      /// For example, ULT->SLT, ULE->SLE, UGT->SGT, UGE->SGE, SLT->Failed assert
-      /// @returns the signed version of the unsigned predicate pred.
-      /// @brief return the signed version of a predicate
-      static Predicate getSignedPredicate(Predicate pred);
-
-      /// @returns true if the predicate is unsigned, false otherwise.
-      /// @brief Determine if the predicate is an unsigned operation.
-      static bool isUnsigned(Predicate predicate);
-
-      /// @returns true if the predicate is signed, false otherwise.
-      /// @brief Determine if the predicate is an signed operation.
-      static bool isSigned(Predicate predicate);
-
-      /// @brief Determine if the predicate is an ordered operation.
-      static bool isOrdered(Predicate predicate);
-
-      /// @brief Determine if the predicate is an unordered operation.
-      static bool isUnordered(Predicate predicate);
-
-      /// Determine if the predicate is true when comparing a value with itself.
-      static bool isTrueWhenEqual(Predicate predicate);
-
-      /// Determine if the predicate is false when comparing a value with itself.
-      static bool isFalseWhenEqual(Predicate predicate);
-
-      /// Determine if Pred1 implies Pred2 is true when two compares have matching
-      /// operands.
-      static bool isImpliedTrueByMatchingCmp(Predicate Pred1, Predicate Pred2);
-
-      /// Determine if Pred1 implies Pred2 is false when two compares have matching
-      /// operands.
-      static bool isImpliedFalseByMatchingCmp(Predicate Pred1, Predicate Pred2);
-    */
-
     /// <summary>Predicate enumeration for integer comparison</summary>
     public enum IntPredicate
     {
--- src/QsCompiler/LlvmBindings/README.md
+++ src/QsCompiler/LlvmBindings/README.md
@@ -1,5 +1,5 @@
 
-# Llvm.NET Standalone port
+# LlvmBindings Standalone port
 
 This is a port of a small subset of Llvm.NET (originally from [UbiquityDotNET/Llvm.NET](https://github.com/UbiquityDotNET/Llvm.NET)). This port uses [LLVMSharp](https://github.com/microsoft/LLVMSharp) as the backend for wrapped calls into the LLVM-C API instead of Ubiquity.NET.LibLlvm native wrapper. Much of the functionality of Llvm.NET has been stripped out to accomodate this. This project is not intended as a standalone library or for use anywhere outside of the QIR generation component of the Q# compiler.
 
