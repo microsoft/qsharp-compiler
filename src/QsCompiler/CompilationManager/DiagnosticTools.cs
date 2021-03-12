3ffd626b249f836369054cc1aa9e5d2f8aadfe0b
Chris Granade <chgranad@microsoft.com>
2021-03-12 12:34:33 -0800

    Fix #885 (#886)
    * [WIP] Started updating VS LSP client.
    * A few more nullability fixes.
    * Use explicit 0:0 range instead of relying on constructor.
    * Propagate nullability metadata to F# callers.
    * Avoid calling unsafe constructors.
    * Avoid deprecated Assert.Equals.
    * Set DocumentRangeFormattingProvider to false.
    * Fix to AssertCapability.
    * Assertions should work more reliabily when capabilities are null.
    * Disable nullable reference checking when copying unsound types.
    * Check if Range is null, since nullability metadata can be violated.
    
    Co-authored-by: Ricardo Espinoza <ricardoe@microsoft.com>

--- src/QsCompiler/CompilationManager/DiagnosticTools.cs
+++ src/QsCompiler/CompilationManager/DiagnosticTools.cs
@@ -37,28 +37,35 @@ namespace Microsoft.Quantum.QsCompiler.CompilationBuilder
         [return: NotNullIfNotNull("message")]
         public static Diagnostic? Copy(this Diagnostic message)
         {
-            Lsp.Position? CopyPosition(Lsp.Position? position) =>
-                position is null ? null : new Lsp.Position(position.Line, position.Character);
+            Lsp.Position CopyPosition(Lsp.Position position) =>
+                new Lsp.Position(position.Line, position.Character);
 
-            Lsp.Range? CopyRange(Lsp.Range? range) =>
-                range is null
-                    ? null
-                    : new Lsp.Range
-                    {
-                        Start = CopyPosition(range.Start),
-                        End = CopyPosition(range.End)
-                    };
+            Lsp.Range CopyRange(Lsp.Range range) =>
+                new Lsp.Range
+                {
+                    Start = CopyPosition(range.Start),
+                    End = CopyPosition(range.End)
+                };
 
+            // NB: The nullability metadata on Diagnostic.Range is incorrect,
+            //     such that some Diagnostic values may have nullable ranges.
+            //     We cannot assign that to a new Diagnostic without
+            //     contradicting nullability metadata, however, so we need to
+            //     explicitly disable nullable references for the following
+            //     statement. Once the upstream bug in the LSP client package
+            //     is fixed, we can remove the nullable disable here.
+            #nullable disable
             return message is null
                 ? null
                 : new Diagnostic
                 {
-                    Range = CopyRange(message.Range),
+                    Range = message.Range == null ? null : CopyRange(message.Range),
                     Severity = message.Severity,
                     Code = message.Code,
                     Source = message.Source,
                     Message = message.Message
                 };
+            #nullable restore
         }
 
         /// <summary>
@@ -70,12 +77,17 @@ namespace Microsoft.Quantum.QsCompiler.CompilationBuilder
         public static Diagnostic WithLineNumOffset(this Diagnostic diagnostic, int offset)
         {
             var copy = diagnostic.Copy();
-            copy.Range.Start.Line += offset;
-            copy.Range.End.Line += offset;
-            if (copy.Range.Start.Line < 0 || copy.Range.End.Line < 0)
+            // NB: Despite the nullability metadata, Range may be null here.
+            //     We thus need to guard accordingly.
+            if (copy.Range != null)
             {
-                throw new ArgumentOutOfRangeException(
-                    nameof(offset), "Translated diagnostic has negative line numbers.");
+                copy.Range.Start.Line += offset;
+                copy.Range.End.Line += offset;
+                if (copy.Range.Start.Line < 0 || copy.Range.End.Line < 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(offset), "Translated diagnostic has negative line numbers.");
+                }
             }
             return copy;
         }
--- src/QsCompiler/Compiler/LoadedStep.cs
+++ src/QsCompiler/Compiler/LoadedStep.cs
@@ -123,7 +123,9 @@ namespace Microsoft.Quantum.QsCompiler
                 Severity = severity,
                 Message = $"{stageAnnotation}{diagnostic.Message}",
                 Source = diagnostic.Source,
-                Range = diagnostic.Source is null ? null : diagnostic.Range?.ToLsp()
+                Range = diagnostic.Source is null || diagnostic.Range is null
+                        ? new VisualStudio.LanguageServer.Protocol.Range()
+                        : diagnostic.Range.ToLsp()
             };
         }
 
