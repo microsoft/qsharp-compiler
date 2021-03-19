56da823602fa9c50f267f2fc70c46d06c846538c
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2021-03-18 18:50:01 -0700

    Created the base for the C++ entry-point driver generator.

---
+++ src/QsCompiler/Compiler/Templates/QirDriverCppEx.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Quantum.QsCompiler.BondSchemas.EntryPoint;
+
+namespace Microsoft.Quantum.QsCompiler.Templates
+{
+    public partial class QirDriverCpp
+    {
+        private EntryPointOperation entryPointOperation;
+
+        public QirDriverCpp(EntryPointOperation entryPoint)
+        {
+            this.entryPointOperation = entryPoint;
+        }
+    }
+}
