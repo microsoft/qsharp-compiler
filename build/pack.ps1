29d8fb5425d325ab838b970e25dac6cfca53d55f
Bettina Heim <beheim@microsoft.com>
2021-03-24 16:23:00 -0700

    wip

--- build/pack.ps1
+++ build/pack.ps1
@@ -90,6 +90,7 @@ Publish-One '../src/QuantumSdk/Tools/BuildConfiguration/BuildConfiguration.cspro
 Publish-One '../src/QuantumSdk/Tools/DefaultEntryPoint/DefaultEntryPoint.csproj'
 
 Pack-One '../src/QsCompiler/Compiler/Compiler.csproj' '-IncludeReferencedProjects'
+Pack-One '../src/QsCompiler/QirGeneration/QirGeneration.csproj'
 Pack-Dotnet '../src/Documentation/DocumentationGenerator/DocumentationGenerator.csproj'
 Pack-One '../src/ProjectTemplates/Microsoft.Quantum.ProjectTemplates.nuspec'
 Pack-One '../src/QuantumSdk/QuantumSdk.nuspec'
