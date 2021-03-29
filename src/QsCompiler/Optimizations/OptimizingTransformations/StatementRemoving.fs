8f5215a1d751e3817d612cf06c4c9895339514c7
Sarah Marshall <sarah.marshall@microsoft.com>
2021-03-29 15:56:00 -0700

    Formatting

--- src/QsCompiler/Optimizations/OptimizingTransformations/StatementRemoving.fs
+++ src/QsCompiler/Optimizations/OptimizingTransformations/StatementRemoving.fs
@@ -83,10 +83,7 @@ and private VariableRemovalStatements(parent: StatementRemoval, removeFunctions)
                 let lhs = List.map fst myList |> ImmutableArray.CreateRange |> VariableNameTuple
 
                 let rhs =
-                    List.map snd myList
-                    |> ImmutableArray.CreateRange
-                    |> QubitTupleAllocation
-                    |> ResolvedInitializer.New
+                    List.map snd myList |> ImmutableArray.CreateRange |> QubitTupleAllocation |> ResolvedInitializer.New
 
                 let newBody = QsScope.New(s.Body.Statements.InsertRange(0, newStatements), s.Body.KnownSymbols)
                 QsQubitScope.New s.Kind ((lhs, rhs), newBody) |> QsQubitScope |> Seq.singleton
