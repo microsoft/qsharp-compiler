64c781d7460721389ac47551e1b5dff6b633811a
Kevin Hartman <kevin@hart.mn>
2021-03-31 15:02:00 -0400

    Documentation comments for FunctionContext.
    Also include an additional precondition for one of its methods.

--- src/QsCompiler/QirGeneration/FunctionContext.cs
+++ src/QsCompiler/QirGeneration/FunctionContext.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +10,10 @@ using Ubiquity.NET.Llvm.Values;
 
 namespace Microsoft.Quantum.QsCompiler.QIR
 {
+    /// <summary>
+    /// Encapsulates basic block code generation for a function
+    /// currently being built by the QIR generator.
+    /// </summary>
     internal class FunctionContext
     {
         private readonly IrFunction function;
@@ -14,14 +21,31 @@ namespace Microsoft.Quantum.QsCompiler.QIR
         private InstructionBuilder currentBuilder;
         private InstructionBuilder? activelyEmittingTo = null;
 
+        /// <summary>
+        /// Gets the underlying function.
+        /// </summary>
         internal IrFunction Function => this.function;
 
+        /// <summary>
+        /// Gets the current block to which instructions are being emitted.
+        /// </summary>
         internal BasicBlock CurrentBlock => this.currentBuilder.InsertBlock;
 
+        /// <summary>
+        /// Indicates whether the current block is terminated.
+        /// </summary>
         internal bool IsCurrentBlockTerminated => this.CurrentBlock.Terminator != null;
 
+        /// <summary>
+        /// Indicates whether the current block contains instuctions.
+        /// </summary>
         internal bool IsCurrentBlockEmpty => !this.CurrentBlock.Instructions.Any();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FunctionContext"/> class.
+        /// </summary>
+        /// <param name="function">The current function that the QIR generator is processing.</param>
+        /// <param name="uniqueLocalName">A function used to generate unique local names within the function.</param>
         internal FunctionContext(IrFunction function, Func<string, string> uniqueLocalName)
         {
             this.function = function;
@@ -29,6 +53,7 @@ namespace Microsoft.Quantum.QsCompiler.QIR
             this.currentBuilder = new InstructionBuilder(this.function.AppendBasicBlock("entry"));
         }
 
+        /// <inheritdoc cref="Emit{T}(Func{InstructionBuilder, T})"/>
         internal void Emit(Action<InstructionBuilder> action)
         {
             this.Emit(builder =>
@@ -38,6 +63,28 @@ namespace Microsoft.Quantum.QsCompiler.QIR
             });
         }
 
+        /// <summary>
+        /// Provides brokered access to the current block's <see cref="InstructionBuilder"/>,
+        /// ensuring that emitted code does not result in an invalid block.
+        /// </summary>
+        /// <remarks>
+        /// If the current block ends with a <see cref="Terminator"/>, a new (unreachable) block is
+        /// prepared, and <paramref name="func"/> is invoked with its builder. If instructions
+        /// are emitted, the unreachable block becomes the current block. Otherwise, the empty
+        /// unreachable block is removed.
+        /// <para/>
+        /// Note that nested calls to <see cref="Emit"/> are valid. In this case, validation is only
+        /// performed when the root call to <paramref name="func"/> returns.
+        /// I.e. Inner calls to <see cref="Emit"/> invoke their <paramref name="func"/> with
+        /// the same builder being used by that of the most outter.
+        /// </remarks>
+        /// <typeparam name="T">The return type of <paramref name="func"/>.</typeparam>
+        /// <param name="func">The function.</param>
+        /// <returns>The return value of <paramref name="func"/>.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// The call to <paramref name="func"/> emitted an instruction sequence containing
+        /// a <see cref="Terminator"/> that is not the last instruction.
+        /// </exception>
         internal T Emit<T>(Func<InstructionBuilder, T> func)
         {
             if (this.activelyEmittingTo != null)
@@ -97,11 +144,10 @@ namespace Microsoft.Quantum.QsCompiler.QIR
         }
 
         /// <summary>
-        /// Creates a new basic block and adds it to the current function immediately after the current block.
+        /// Creates a new basic block and adds it to the function immediately after the current block.
         /// </summary>
         /// <param name="name">The base name for the new block; a counter will be appended to ensure uniqueness</param>
         /// <returns>The new block.</returns>
-        /// <exception cref="InvalidOperationException">The current function is set to null.</exception>
         internal BasicBlock AddBlockAfterCurrent(string name)
         {
             var flag = false;
@@ -126,6 +172,11 @@ namespace Microsoft.Quantum.QsCompiler.QIR
                 : this.function.InsertBasicBlock(continueName, next);
         }
 
+        /// <summary>
+        /// Creates a new basic block and adds it to the end of the function.
+        /// </summary>
+        /// <param name="name">The base name for the new block; a counter will be appended to ensure uniqueness</param>
+        /// <returns>The new block.</returns>
         internal BasicBlock AppendBlock(string name)
         {
             return this.Function.AppendBasicBlock(this.uniqueBlockName(name));
@@ -135,24 +186,36 @@ namespace Microsoft.Quantum.QsCompiler.QIR
         /// Makes the given basic block current, creates a new builder for it, and makes that builder current.
         /// This method does not check to make sure that the block isn't already current.
         /// </summary>
-        /// <remarks>
-        /// Note that if the current block has no instructions, it is pruned from the current function.
-        /// </remarks>
-        /// <param name="b">The block to make current</param>
-        internal void SetCurrentBlock(BasicBlock b)
+        /// <param name="block">The block to make current</param>
+        /// <exception cref="InvalidOperationException">Function must contain block.</exception>
+        /// <exception cref="InvalidOperationException">Call not valid from inside <see cref="Emit"/>.</exception>
+        internal void SetCurrentBlock(BasicBlock block)
         {
             this.AssertNotEmitting(nameof(this.SetCurrentBlock));
 
-            // TODO: validate that b is inside this function.
-            this.currentBuilder = new InstructionBuilder(b);
+            if (!this.Function.BasicBlocks.Contains(block))
+            {
+                throw new InvalidOperationException($"Function must contain block.");
+            }
+
+            this.currentBuilder = new InstructionBuilder(block);
         }
 
-        internal bool RemoveBlock(BasicBlock b)
+        /// <summary>
+        /// Removes <paramref name="block"/> from the function.
+        /// </summary>
+        /// <param name="block">The block to remove.</param>
+        /// <returns>True if the block existed and was removed, false otherwise.</returns>
+        internal bool RemoveBlock(BasicBlock block)
         {
-            this.AssertNotEmitting(nameof(this.RemoveBlock));
-            return this.function.BasicBlocks.Remove(b);
+            return this.function.BasicBlocks.Remove(block);
         }
 
+        /// <summary>
+        /// Raises an exception if called inside a call to <see cref="Emit"/>.
+        /// </summary>
+        /// <param name="invalidOp">The name of the invalid function called.</param>
+        /// <exception cref="InvalidOperationException">Call not valid from inside <see cref="Emit"/>.</exception>
         private void AssertNotEmitting(string invalidOp)
         {
             if (this.activelyEmittingTo != null)
