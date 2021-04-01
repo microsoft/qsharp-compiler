8bfbdee9579a66d48374eeff0b843adeb2edbf89
Andrey Tretyak <andreyt@microsoft.com>
2021-04-01 16:48:29 +0100

    Unify headers for LlvmBindings project

--- src/QsCompiler/LlvmBindings/IOperandCollection.cs
+++ src/QsCompiler/LlvmBindings/IOperandCollection.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="IFixedShapeCollection.cs" company="Ubiquity.NET Contributors">
+// <copyright file="IOperandCollection.cs" company="Ubiquity.NET Contributors">
 // Copyright (c) Ubiquity.NET Contributors. All rights reserved.
 // Portions Copyright (c) Microsoft Corporation
 // </copyright>
