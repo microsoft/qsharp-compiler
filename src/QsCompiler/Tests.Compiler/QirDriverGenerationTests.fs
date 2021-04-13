7a5c26a43708c0a14eb80bc3417405b0a77ebc39
Alexander Chocron <alchocro@microsoft.com>
2021-04-13 10:34:53 -0700

    Fix unit tests for CI

--- src/QsCompiler/Tests.Compiler/QirDriverGenerationTests.fs
+++ src/QsCompiler/Tests.Compiler/QirDriverGenerationTests.fs
@@ -27,37 +27,37 @@ type QirDriverGenerationTests(output: ITestOutputHelper) =
             argList.Add(arg)
         argList.Item(0).Name <- "int-value"
         argList.Item(0).Type <- DataType.IntegerType
-        argList.Item(0).Values.Item(0).Integer <- 1L
+        argList.Item(0).Values.Item(0).Integer <- new System.Nullable<int64>(1L)
         argList.Item(1).Name <- "integer-array"
         argList.Item(1).Type <- DataType.ArrayType
-        argList.Item(1).ArrayType <- DataType.IntegerType
+        argList.Item(1).ArrayType <- new System.Nullable<DataType>(DataType.IntegerType)
         let array1 = new ArrayValue()
         array1.Integer <- [| 1L; 2L; 3L; 4L; 5L |] |> ResizeArray
         argList.Item(1).Values.Item(0).Array <- array1
         argList.Item(2).Name <- "double-value"
         argList.Item(2).Type <- DataType.DoubleType
-        argList.Item(2).Values.Item(0).Double <- 0.5
+        argList.Item(2).Values.Item(0).Double <- new System.Nullable<float>(0.5)
         argList.Item(3).Name <- "double-array"
         argList.Item(3).Type <- DataType.ArrayType
-        argList.Item(3).ArrayType <- DataType.DoubleType
+        argList.Item(3).ArrayType <- new System.Nullable<DataType>(DataType.DoubleType)
         let array3 = new ArrayValue()
         array3.Double <- [| 0.1; 0.2; 0.3; 0.4; 0.5 |] |> ResizeArray
         argList.Item(3).Values.Item(0).Array <- array3
         argList.Item(4).Name <- "bool-value"
         argList.Item(4).Type <- DataType.BoolType
-        argList.Item(4).Values.Item(0).Bool <- true
+        argList.Item(4).Values.Item(0).Bool <- new System.Nullable<bool>(true)
         argList.Item(5).Name <- "bool-array"
         argList.Item(5).Type <- DataType.ArrayType
-        argList.Item(5).ArrayType <- DataType.BoolType
+        argList.Item(5).ArrayType <- new System.Nullable<DataType>(DataType.BoolType)
         let array5 = new ArrayValue()
         array5.Bool <- [| true; true; false |] |> ResizeArray
         argList.Item(5).Values.Item(0).Array <- array5
         argList.Item(6).Name <- "pauli-value"
         argList.Item(6).Type <- DataType.PauliType
-        argList.Item(6).Values.Item(0).Pauli <- PauliValue.PauliI
+        argList.Item(6).Values.Item(0).Pauli <- new System.Nullable<PauliValue>(PauliValue.PauliI)
         argList.Item(7).Name <- "pauli-array"
         argList.Item(7).Type <- DataType.ArrayType
-        argList.Item(7).ArrayType <- DataType.PauliType
+        argList.Item(7).ArrayType <- new System.Nullable<DataType>(DataType.PauliType)
         let array7 = new ArrayValue()
         array7.Pauli <- [| PauliValue.PauliX; PauliValue.PauliI; PauliValue.PauliY; PauliValue.PauliZ |] |> ResizeArray
         argList.Item(7).Values.Item(0).Array <- array7
@@ -70,7 +70,7 @@ type QirDriverGenerationTests(output: ITestOutputHelper) =
         argList.Item(8).Values.Item(0).Range <- range8
         argList.Item(9).Name <- "range-array"
         argList.Item(9).Type <- DataType.ArrayType
-        argList.Item(9).ArrayType <- DataType.RangeType
+        argList.Item(9).ArrayType <- new System.Nullable<DataType>(DataType.RangeType)
         let range9_0 = new RangeValue()
         range9_0.Start <- 1L
         range9_0.Step <- 2L
@@ -88,10 +88,10 @@ type QirDriverGenerationTests(output: ITestOutputHelper) =
         argList.Item(9).Values.Item(0).Array <- array9
         argList.Item(10).Name <- "result-value"
         argList.Item(10).Type <- DataType.ResultType
-        argList.Item(10).Values.Item(0).Result <- ResultValue.One
+        argList.Item(10).Values.Item(0).Result <- new System.Nullable<ResultValue>(ResultValue.One)
         argList.Item(11).Name <- "result-array"
         argList.Item(11).Type <- DataType.ArrayType
-        argList.Item(11).ArrayType <- DataType.ResultType
+        argList.Item(11).ArrayType <- new System.Nullable<DataType>(DataType.ResultType)
         let array11 = new ArrayValue()
         array11.Result <- [| ResultValue.One; ResultValue.Zero; ResultValue.One; ResultValue.One |] |> ResizeArray
         argList.Item(11).Values.Item(0).Array <- array11
@@ -100,7 +100,7 @@ type QirDriverGenerationTests(output: ITestOutputHelper) =
         argList.Item(12).Values.Item(0).String <- "a sample string"
         argList.Item(13).Name <- "string-array"
         argList.Item(13).Type <- DataType.ArrayType
-        argList.Item(13).ArrayType <- DataType.StringType
+        argList.Item(13).ArrayType <- new System.Nullable<DataType>(DataType.StringType)
         let array13 = new ArrayValue()
         array13.String <- [| "String A"; "String B"; "StringC" |] |> ResizeArray
         argList.Item(13).Values.Item(0).Array <- array13
