3b3d82e5d8428cf037fc567c3068a9a691fbf994
Bettina Heim <beheim@microsoft.com>
2021-04-20 00:13:25 -0700

    local setup that works for executing qir compiled using the source code version

---
+++ examples/QIR/Development/Main.cpp
@@ -0,0 +1,15 @@
+﻿#include "QirContext.hpp"
+#include "QirRuntime.hpp"
+#include "SimFactory.hpp"
+
+using namespace Microsoft::Quantum;
+using namespace std;
+
+extern "C" void Microsoft__Quantum__Qir__Emission__RunExample(); // NOLINT
+int main(int argc, char* argv[]){
+    unique_ptr<IRuntimeDriver> sim = CreateFullstateSimulator();
+    QirContextScope qirctx(sim.get(), false /*trackAllocatedObjects*/);
+    Microsoft__Quantum__Qir__Emission__RunExample();
+    return 0;
+}
+
