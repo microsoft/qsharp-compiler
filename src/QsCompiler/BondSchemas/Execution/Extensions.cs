d277b38e2ca76896308f433859eaa094c1e35b64
Alexander Chocron <alchocro@microsoft.com>
2021-04-23 10:24:17 -0700

    Move bond schemas to common namespace

---
+++ src/QsCompiler/BondSchemas/Execution/Extensions.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Quantum.QsCompiler.BondSchemas.Execution
+{
+    /// <summary>
+    /// This class provides extension methods for objects in the Microsoft.Quantum.QsCompiler.BondSchemas.Execution namespace.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Determine whether the values of two object instances are equal.
+        /// </summary>
+        public static bool ValueEquals(this Argument @this, Argument other)
+        {
+            return @this.Name.Equals(other.Name) &&
+                    @this.Type == other.Type &&
+                    @this.Bool == other.Bool &&
+                    @this.Double == other.Double &&
+                    @this.Integer == other.Integer &&
+                    @this.Pauli == other.Pauli &&
+                    @this.Result == other.Result &&
+                    AreNullablesEqual(@this.String, other.String, string.Equals) &&
+                    AreNullablesEqual(@this.Range, other.Range, ValueEquals) &&
+                    AreNullablesEqual(@this.Array, other.Array, (a, b) => AreCollectionsEqual(a, b, ValueEquals));
+        }
+
+        /// <summary>
+        /// Determine whether the values of two object instances are equal.
+        /// </summary>
+        public static bool ValueEquals(this QirExecutionWrapper @this, QirExecutionWrapper other)
+        {
+            return ValueEquals(@this.QirBytecode, other.QirBytecode) &&
+                   AreNullablesEqual(@this.EntryPoints, other.EntryPoints, (a, b) => AreCollectionsEqual(a, b, ValueEquals));
+        }
+
+        public static bool ValueEquals(this RangeValue rangeValueA, RangeValue rangeValueB)
+        {
+            if (rangeValueA.Start != rangeValueB.Start)
+            {
+                return false;
+            }
+            else if (rangeValueA.Step != rangeValueB.Step)
+            {
+                return false;
+            }
+            else if (rangeValueA.End != rangeValueB.End)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool AreCollectionsEqual<T>(ICollection<T> collectionA, ICollection<T> collectionB, Func<T, T, bool> equalityFunction)
+        {
+            if (collectionA.Count != collectionB.Count)
+            {
+                return false;
+            }
+
+            using (var enumeratorA = collectionA.GetEnumerator())
+            using (var enumeratorB = collectionB.GetEnumerator())
+            {
+                while (enumeratorA.MoveNext() && enumeratorB.MoveNext())
+                {
+                    if (!equalityFunction(enumeratorA.Current, enumeratorB.Current))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static bool AreNullablesEqual<T>(T? itemA, T? itemB, Func<T, T, bool> equalityFunction)
+            where T : class
+        {
+            if ((itemA == null) && (itemB != null))
+            {
+                return false;
+            }
+
+            if ((itemA != null) && (itemB == null))
+            {
+                return false;
+            }
+
+            if ((itemA != null) && (itemB != null))
+            {
+                return equalityFunction(itemA, itemB);
+            }
+
+            return true;
+        }
+
+        private static bool ValueEquals(this ArraySegment<byte> @this, ArraySegment<byte> other)
+        {
+            return @this.Array.Skip(@this.Offset).Take(@this.Count).SequenceEqual(other.Array.Skip(other.Offset).Take(other.Count));
+        }
+
+        private static bool ValueEquals(this EntryPointOperation @this, EntryPointOperation other)
+        {
+            return AreNullablesEqual(@this.Name, other.Name, string.Equals) &&
+                   AreNullablesEqual(@this.Arguments, other.Arguments, (a, b) => AreCollectionsEqual(a, b, ValueEquals));
+        }
+    }
+}
---
+++ src/QsCompiler/BondSchemas/Execution/Protocols.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.IO;
+using Bond;
+using Bond.IO.Unsafe;
+using Bond.Protocols;
+
+namespace Microsoft.Quantum.QsCompiler.BondSchemas.Execution
+{
+    /// <summary>
+    /// This class provides methods for serialization/deserialization of objects in the Microsoft.Quantum.QsCompiler.BondSchemas.Execution namespace.
+    /// </summary>
+    public static class Protocols
+    {
+        /// <summary>
+        /// Deserializes an Argument object from its JSON representation.
+        /// </summary>
+        internal static Argument DeserializeArgumentFromJson(Stream stream)
+        {
+            var reader = new SimpleJsonReader(stream);
+            var deserializer = new Deserializer<SimpleJsonReader>(typeof(Argument));
+            return deserializer.Deserialize<Argument>(reader);
+        }
+
+        /// <summary>
+        /// Serializes an Argument object to its JSON representation.
+        /// </summary>
+        internal static void SerializeArgumentToJson(Argument entryPoint, Stream stream)
+        {
+            var writer = new SimpleJsonWriter(stream);
+            var serializer = new Serializer<SimpleJsonWriter>(typeof(Argument));
+            serializer.Serialize(entryPoint, writer);
+            writer.Flush();
+            stream.Flush();
+            stream.Position = 0;
+        }
+
+        /// <summary>
+        /// Deserializes a QirExecutionWrapper object from its fast binary representation.
+        /// </summary>
+        public static QirExecutionWrapper DeserializeQirExecutionWrapperFromFastBinary(Stream stream)
+        {
+            var inputStream = new InputStream(stream);
+            var reader = new FastBinaryReader<InputStream>(inputStream);
+            var deserializer = new Deserializer<FastBinaryReader<InputStream>>(typeof(QirExecutionWrapper));
+            var qirExecutionWrapper = deserializer.Deserialize<QirExecutionWrapper>(reader);
+            return qirExecutionWrapper;
+        }
+
+        /// <summary>
+        /// Serializes a QirExecutionWrapper object to its fast binary representation.
+        /// </summary>
+        public static void SerializeQirExecutionWrapperToFastBinary(QirExecutionWrapper qirExecutionWrapper, Stream stream)
+        {
+            var outputBuffer = new OutputBuffer();
+            var writer = new FastBinaryWriter<OutputBuffer>(outputBuffer);
+            var serializer = new Serializer<FastBinaryWriter<OutputBuffer>>(typeof(QirExecutionWrapper));
+            serializer.Serialize(qirExecutionWrapper, writer);
+            stream.Write(outputBuffer.Data);
+            stream.Flush();
+            stream.Position = 0;
+        }
+    }
+}
