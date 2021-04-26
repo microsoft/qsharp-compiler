5368a28ff0bdc79ced4111fc714f0a0b63ee6138
Stefan J. Wernli <swernli@microsoft.com>
2021-04-26 00:30:58 -0700

    More QIR JIT experimentation
    NOTE: this requires updates to the QIR Runtime API that are not part of main, so won't work using the existing API

--- examples/QIR/Development/Program.qs
+++ examples/QIR/Development/Program.qs
@@ -1,8 +1,51 @@
 ﻿namespace Microsoft.Quantum.Qir.Emission {
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Arrays;
+    open Microsoft.Quantum.Preparation;
+    open Microsoft.Quantum.Measurement;
+
+    @EntryPoint()
+    operation Sum(nums : Int[]) : Int {
+        mutable sum = 0;
+        for n in nums {
+            set sum = sum + n;
+        }
+        return sum;
+    }
 
     @EntryPoint()
     operation Add(x : Int, y : Int) : Int {
         return x + y;
+    }
+
+    @EntryPoint()
+    operation SampleTeleport(input : Bool) : Result {
+        // Use two qubits to form the bell pair.
+        use bellPair = Qubit[2];
+        H(bellPair[0]);
+        CNOT(bellPair[0], bellPair[1]);
+
+        // Encode the boolean input into a third qubit.
+        use qubit = Qubit();
+        if (input) {
+            X(qubit);
+        }
+
+        // Teleport the input through the bell pair, using
+        // branching based on measurement for the corrections.
+        CNOT(qubit, bellPair[0]);
+        H(qubit);
+        if (M(bellPair[0]) == One) {
+            X(bellPair[1]);
+        }
+        if (MResetZ(qubit) == One) {
+            Z(bellPair[1]);
+        }
+
+        // Return the measurement of the teleported value.
+        let mres = MResetZ(bellPair[1]);
+        ResetAll(bellPair);
+        return mres;
     }
 
 }
--- examples/QIR/JIT/Program.cs
+++ examples/QIR/JIT/Program.cs
@@ -8,26 +8,54 @@ namespace JIT
 {
     class Program
     {
-        [UnmanagedFunctionPointer(System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        delegate Int64 BinaryInt64Operation(Int64 op1, Int64 op2);
+        [DllImport("Microsoft.Quantum.Qir.QSharp.Core", ExactSpelling = true, CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        public static extern IntPtr CreateFullstateSimulator();
 
-        const string FUNCTION_NAME = "Microsoft__Quantum__Qir__Emission__Add__body";
-        const string BITCODE_PATH = "../Development/obj/qsharp/Development.bc";
+        [DllImport("Microsoft.Quantum.Qir.Runtime", ExactSpelling = true, CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        public static extern void InitializeQirContext(IntPtr driver);
 
-        static void Main(string[] args)
+        [StructLayout(LayoutKind.Sequential)]
+        public unsafe struct InteropArray
         {
+            public long Length;
+            public void* Array;
+        }
+
+        [UnmanagedFunctionPointer(System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        delegate long BinaryAddOperation(long op1, long op2);
+
+        [UnmanagedFunctionPointer(System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        unsafe delegate long BinarySumArrayOperation(InteropArray* input);
+
+        [UnmanagedFunctionPointer(System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        delegate void TeleportOperation(char input);
+
+        const string ADD_FUNCTION_NAME = "Microsoft__Quantum__Qir__Emission__Add__body";
+        const string SUM_FUNCTION_NAME = "Microsoft__Quantum__Qir__Emission__Sum__Interop";
+        const string TELEPORT_FUNCTION_NAME = "Microsoft__Quantum__Qir__Emission__SampleTeleport";
+
+        const string BITCODE_PATH = "C:\\Users\\swern\\Programming\\qsharp-compiler\\examples\\QIR\\Development\\obj\\qsharp\\Development.bc";
+
+        static void Main()
+        {
+            var qsharpFoundationLibrary = NativeLibrary.Load("Microsoft.Quantum.Qir.QSharp.Foundation", typeof(InteropArray).Assembly, null);
+            var qsharpCoreLibrary = NativeLibrary.Load("Microsoft.Quantum.Qir.QSharp.Core", typeof(InteropArray).Assembly, null);
+
+            // To get this line to work, I had to change the CreateFullstateSimulator API to use raw pointers instead of shared pointers,
+            // and I had to update both calls to be "extern 'C'" otherwise name mangling makes then impossible to call here.
+            // This should be an important consideration as we look at how to update our ABI for compatibility across langauges.
+            InitializeQirContext(CreateFullstateSimulator());
+
             var context = new Context();
             var module = BitcodeModule.LoadFrom(BITCODE_PATH, context);
-            string verifyMessage;
-            if (!module.Verify(out verifyMessage))
+            if (!module.Verify(out string verifyMessage))
             {
                 throw new ExternalException(String.Format("Failed to verify module: {0}", verifyMessage));
             }
 
-            IrFunction funcDef;
-            if (!module.TryGetFunction(FUNCTION_NAME, out funcDef))
+            if (!module.TryGetFunction(ADD_FUNCTION_NAME, out IrFunction funcDef))
             {
-                throw new ExternalException(String.Format("Failed to find function '{0}'", FUNCTION_NAME));
+                throw new ExternalException(String.Format("Failed to find function '{0}'", ADD_FUNCTION_NAME));
             }
 
             LLVM.InitializeNativeTarget();
@@ -35,10 +63,39 @@ namespace JIT
             LLVM.InitializeNativeAsmPrinter();
 
             var engine = module.ModuleHandle.CreateMCJITCompiler();
-            var function = engine.GetPointerToGlobal<BinaryInt64Operation>(funcDef.ValueHandle);
+            var function = engine.GetPointerToGlobal<BinaryAddOperation>(funcDef.ValueHandle);
             var result = function(2, 3);
 
-            System.Console.WriteLine(result);
+            Console.WriteLine(result);
+
+            if (!module.TryGetFunction(SUM_FUNCTION_NAME, out IrFunction funcDef2))
+            {
+                throw new ExternalException(string.Format("Failed to find function '{0}'", SUM_FUNCTION_NAME));
+            }
+
+            var function2 = engine.GetPointerToGlobal<BinarySumArrayOperation>(funcDef2.ValueHandle);
+            var innerArray = new long[]{1, 2, 3, 4, 5};
+            unsafe
+            {
+                fixed (long* rawArray = &innerArray[0])
+                {
+                    var array = new InteropArray {
+                        Length = innerArray.Length,
+                        Array = rawArray
+                    };
+                    result = function2(&array);
+                }
+            }
+
+            Console.WriteLine(result);
+
+            if (!module.TryGetFunction(TELEPORT_FUNCTION_NAME, out IrFunction funcDef3))
+            {
+                throw new ExternalException(string.Format("Failed to find function '{0}'", TELEPORT_FUNCTION_NAME));
+            }
+
+            var function3 = engine.GetPointerToGlobal<TeleportOperation>(funcDef3.ValueHandle);
+            function3((char)1);
         }
     }
 }
