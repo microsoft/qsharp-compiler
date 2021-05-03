eb62110e2bebccd56e5f86794b1369e053fd3ed2
Scott Carda <sccarda@microsoft.com>
2021-05-03 14:52:04 -0700

    removed unused using

--- src/QsCompiler/QirGeneration/NameGeneration.cs
+++ src/QsCompiler/QirGeneration/NameGeneration.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Quantum.QsCompiler.SyntaxTree;
