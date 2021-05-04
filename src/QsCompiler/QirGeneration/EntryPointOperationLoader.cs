2f3f5105cea4effd5ea910e198c92f9e551e2fbc
Scott Carda <sccarda@microsoft.com>
2021-05-04 16:27:28 -0700

    Fills out the Parameters field of the entry point operation objects.

--- src/QsCompiler/QirGeneration/EntryPointOperationLoader.cs
+++ src/QsCompiler/QirGeneration/EntryPointOperationLoader.cs
@@ -23,10 +23,74 @@ namespace Microsoft.Quantum.QsCompiler.QIR
 
         private static IList<EntryPointOperation> GenerateEntryPointOperations(QsCompilation compilation)
         {
+            var globals = compilation.Namespaces.GlobalCallableResolutions();
+
             return compilation.EntryPoints.Select(ep => new EntryPointOperation()
             {
-                Name = NameGeneration.InteropFriendlyWrapperName(ep)
+                Name = NameGeneration.InteropFriendlyWrapperName(ep),
+                Parameters = GetParams(globals[ep])
             }).ToList();
+        }
+
+        private static List<Parameter> GetParams(QsCallable callable)
+        {
+            return SyntaxGenerator.ExtractItems(callable.ArgumentTuple)
+                .Where(sym => !sym.Type.Resolution.IsUnitType)
+                .Select(sym => MakeParameter(sym))
+                .ToList();
+        }
+
+        private static Parameter MakeParameter(LocalVariableDeclaration<QsLocalSymbol> parameter)
+        {
+            return new Parameter()
+            {
+                Name = parameter.VariableName is QsLocalSymbol.ValidName name
+                    ? name.Item
+                    : throw new ArgumentException("Encountered invalid name for parameter."),
+                Type = MapResolvedTypeToDataType(parameter.Type)
+            };
+        }
+
+        private static DataType MapResolvedTypeToDataType(ResolvedType rt)
+        {
+            var res = rt.Resolution;
+
+            if (res.IsBool)
+            {
+                return DataType.BoolType;
+            }
+            else if (res.IsInt)
+            {
+                return DataType.IntegerType;
+            }
+            else if (res.IsDouble)
+            {
+                return DataType.DoubleType;
+            }
+            else if (res.IsPauli)
+            {
+                return DataType.PauliType;
+            }
+            else if (res.IsRange)
+            {
+                return DataType.RangeType;
+            }
+            else if (res.IsResult)
+            {
+                return DataType.ResultType;
+            }
+            else if (res.IsString)
+            {
+                return DataType.StringType;
+            }
+            else if (res.IsArrayType)
+            {
+                return DataType.ArrayType;
+            }
+            else
+            {
+                throw new NotImplementedException("invalid type for entry point parameter");
+            }
         }
     }
 }
