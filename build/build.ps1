9fe6022a3d3abfea91820664f4e110fbfac6ad0b
Bettina Heim <beheim@microsoft.com>
2021-05-18 22:44:19 -0700

    install clang

--- build/build.ps1
+++ build/build.ps1
@@ -7,6 +7,9 @@ $ErrorActionPreference = 'Stop'
 $all_ok = $True
 Write-Host "Assembly version: $Env:ASSEMBLY_VERSION"
 
+Write-Host "Installing LLVM version 11.1.0 ..."
+choco install llvm --version=11.1.0
+
 ##
 # Q# compiler and Sdk tools
 ##
