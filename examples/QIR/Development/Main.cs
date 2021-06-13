dc46fd78c9d1c202895b6a5f00ebe3d18c210391
Bettina Heim <beheim@microsoft.com>
2021-06-13 11:17:39 -0700

    undoing changes to development project

---
+++ examples/QIR/Development/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Microsoft.Quantum.Sdk.Tools
+{
+    public static class DefaultEntryPoint
+    {
+        private static void Main(string[] args) =>
+            Console.WriteLine(
+                "Full support for executing projects compiled to QIR is not yet integrated into the Sdk. " +
+                "For more information about the feature, see https://github.com/microsoft/qsharp-compiler/tree/main/src/QsCompiler/QirGeneration. " +
+                "The generated QIR can be executed by manually linking the QIR runtime. " +
+                "See https://github.com/microsoft/qsharp-runtime/tree/main/src/Qir/Runtime for further instructions.");
+    }
+}
