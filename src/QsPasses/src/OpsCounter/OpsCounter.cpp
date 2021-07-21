1a5e95f8ecc78960a1886f785f0a2060ecc2d332
Troels F. Roennow <troels@wonop.com>
2021-07-21 17:05:21 +0200

    Refactoring pass

--- src/QsPasses/src/OpsCounter/OpsCounter.cpp
+++ src/QsPasses/src/OpsCounter/OpsCounter.cpp
@@ -7,65 +7,23 @@
 
 using namespace llvm;
 
-namespace
-{
-
-void Visitor(Function& f)
-{
-    errs() << "(operation-counter) " << f.getName() << "\n";
-    errs() << "(operation-counter)   number of arguments: " << f.arg_size() << "\n";
-}
-
-struct OpsCounterPass : PassInfoMixin<OpsCounterPass>
-{
-    static auto run(Function& f, FunctionAnalysisManager& /*unused*/) -> PreservedAnalyses // NOLINT
-    {
-        Visitor(f);
-
-        return PreservedAnalyses::all();
-    }
-};
-
-class CLegacyOpsCounterPass : public FunctionPass
-{
-  public:
-    static char ID;
-    CLegacyOpsCounterPass()
-        : FunctionPass(ID)
-    {
-    }
-
-    auto runOnFunction(Function& f) -> bool override
-    {
-        Visitor(f);
-        return false;
-    }
-};
-} // namespace
-
 llvm::PassPluginLibraryInfo GetOpsCounterPluginInfo()
 {
-    return {LLVM_PLUGIN_API_VERSION, "OpsCounter", LLVM_VERSION_STRING, [](PassBuilder& pb) {
-                pb.registerPipelineParsingCallback(
-                    [](StringRef name, FunctionPassManager& fpm, ArrayRef<PassBuilder::PipelineElement> /*unused*/) {
-                        if (name == "operation-counter")
-                        {
-                            fpm.addPass(OpsCounterPass());
-                            return true;
-                        }
-                        return false;
-                    });
-            }};
+  return {
+      LLVM_PLUGIN_API_VERSION, "OpsCounter", LLVM_VERSION_STRING, [](PassBuilder &pb) {
+        pb.registerPipelineParsingCallback([](StringRef name, FunctionPassManager &fpm,
+                                              ArrayRef<PassBuilder::PipelineElement> /*unused*/) {
+          if (name == "operation-counter")
+          {
+            fpm.addPass(COpsCounterPrinter(llvm::errs()));
+            return true;
+          }
+          return false;
+        });
+      }};
 }
 
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo()
 {
-    return GetOpsCounterPluginInfo();
+  return GetOpsCounterPluginInfo();
 }
-
-char                                       CLegacyOpsCounterPass::ID = 0;
-static RegisterPass<CLegacyOpsCounterPass> LegacyOpsCounterRegistration(
-    "legacy-operation-counter",
-    "Gate Counter Pass",
-    true,
-    false);
--- src/QsPasses/src/OpsCounter/OpsCounter.hpp
+++ src/QsPasses/src/OpsCounter/OpsCounter.hpp
@@ -4,4 +4,34 @@
 
 #include "Llvm.hpp"
 
+class COpsCounterPrinter : public llvm::PassInfoMixin<COpsCounterPrinter>
+{
+public:
+  explicit COpsCounterPrinter(llvm::raw_ostream &out_stream)
+    : out_stream_(out_stream)
+  {}
+
+  //  llvm::PreservedAnalyses run(llvm::Function &function, llvm::FunctionAnalysisManager &fam);
+  auto run(llvm::Function &f, llvm::FunctionAnalysisManager & /*unused*/)
+      -> llvm::PreservedAnalyses  // NOLINT
+  {
+    out_stream_ << "(operation-counter) " << f.getName() << "\n";
+    out_stream_ << "(operation-counter)   number of arguments: " << f.arg_size() << "\n";
+
+    return llvm::PreservedAnalyses::all();
+  }
+  /*
+  TODO(TFR): Documentation suggests that there such be a isRequired, however, comes out as
+  unused after compilation
+  */
+
+  static bool isRequired()
+  {
+    return true;
+  }
+
+private:
+  llvm::raw_ostream &out_stream_;
+};
+
 auto GetOpsCounterPluginInfo() -> llvm::PassPluginLibraryInfo;
