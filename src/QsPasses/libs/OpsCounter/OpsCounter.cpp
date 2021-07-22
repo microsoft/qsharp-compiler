fdb465a88911c0aed8975ccbc6a5a9f45bd12d2c
Troels F. Roennow <troels@wonop.com>
2021-07-22 15:20:57 +0200

    Adding namespaces to passes

--- src/QsPasses/libs/OpsCounter/OpsCounter.cpp
+++ src/QsPasses/libs/OpsCounter/OpsCounter.cpp
@@ -9,75 +9,83 @@
 #include <iostream>
 using namespace llvm;
 
-COpsCounterAnalytics::Result COpsCounterAnalytics::run(
-    llvm::Function& function,
-    llvm::FunctionAnalysisManager& /*unused*/)
+namespace Microsoft
 {
-    COpsCounterAnalytics::Result opcode_map;
-    for (auto& basic_block : function)
+namespace Quantum
+{
+    COpsCounterAnalytics::Result COpsCounterAnalytics::run(
+        llvm::Function& function,
+        llvm::FunctionAnalysisManager& /*unused*/)
     {
-        for (auto& instruction : basic_block)
+        COpsCounterAnalytics::Result opcode_map;
+        for (auto& basic_block : function)
         {
-            if (instruction.isDebugOrPseudoInst())
+            for (auto& instruction : basic_block)
             {
-                continue;
-            }
-            auto name = instruction.getOpcodeName();
+                if (instruction.isDebugOrPseudoInst())
+                {
+                    continue;
+                }
+                auto name = instruction.getOpcodeName();
 
-            if (opcode_map.find(name) == opcode_map.end())
-            {
-                opcode_map[instruction.getOpcodeName()] = 1;
-            }
-            else
-            {
-                opcode_map[instruction.getOpcodeName()]++;
+                if (opcode_map.find(name) == opcode_map.end())
+                {
+                    opcode_map[instruction.getOpcodeName()] = 1;
+                }
+                else
+                {
+                    opcode_map[instruction.getOpcodeName()]++;
+                }
             }
         }
+
+        return opcode_map;
     }
 
-    return opcode_map;
-}
-
-COpsCounterPrinter::COpsCounterPrinter(llvm::raw_ostream& out_stream)
-    : out_stream_(out_stream)
-{
-}
-
-llvm::PreservedAnalyses COpsCounterPrinter::run(llvm::Function& function, llvm::FunctionAnalysisManager& fam)
-{
-    auto& opcode_map = fam.getResult<COpsCounterAnalytics>(function);
-
-    out_stream_ << "Stats for '" << function.getName() << "'\n";
-    out_stream_ << "===========================\n";
-
-    constexpr auto str1 = "Opcode";
-    constexpr auto str2 = "# Used";
-    out_stream_ << llvm::format("%-15s %-8s\n", str1, str2);
-    out_stream_ << "---------------------------"
-                << "\n";
-
-    for (auto const& instruction : opcode_map)
+    COpsCounterPrinter::COpsCounterPrinter(llvm::raw_ostream& out_stream)
+        : out_stream_(out_stream)
     {
-        out_stream_ << llvm::format("%-15s %-8lu\n", instruction.first().str().c_str(), instruction.second);
     }
-    out_stream_ << "---------------------------"
-                << "\n\n";
 
-    return llvm::PreservedAnalyses::all();
-}
+    llvm::PreservedAnalyses COpsCounterPrinter::run(llvm::Function& function, llvm::FunctionAnalysisManager& fam)
+    {
+        auto& opcode_map = fam.getResult<COpsCounterAnalytics>(function);
 
-bool COpsCounterPrinter::isRequired()
-{
-    return true;
-}
+        out_stream_ << "Stats for '" << function.getName() << "'\n";
+        out_stream_ << "===========================\n";
 
-llvm::AnalysisKey COpsCounterAnalytics::Key;
+        constexpr auto str1 = "Opcode";
+        constexpr auto str2 = "# Used";
+        out_stream_ << llvm::format("%-15s %-8s\n", str1, str2);
+        out_stream_ << "---------------------------"
+                    << "\n";
+
+        for (auto const& instruction : opcode_map)
+        {
+            out_stream_ << llvm::format("%-15s %-8lu\n", instruction.first().str().c_str(), instruction.second);
+        }
+        out_stream_ << "---------------------------"
+                    << "\n\n";
+
+        return llvm::PreservedAnalyses::all();
+    }
+
+    bool COpsCounterPrinter::isRequired()
+    {
+        return true;
+    }
+
+    llvm::AnalysisKey COpsCounterAnalytics::Key;
+} // namespace Quantum
+} // namespace Microsoft
 
 // Interface to plugin
 namespace
 {
 llvm::PassPluginLibraryInfo GetOpsCounterPluginInfo()
 {
+    using namespace Microsoft::Quantum;
+
     return {
         LLVM_PLUGIN_API_VERSION, "OpsCounter", LLVM_VERSION_STRING,
         [](PassBuilder& pb)
--- src/QsPasses/libs/OpsCounter/OpsCounter.hpp
+++ src/QsPasses/libs/OpsCounter/OpsCounter.hpp
@@ -4,57 +4,65 @@
 
 #include "Llvm.hpp"
 
-class COpsCounterAnalytics : public llvm::AnalysisInfoMixin<COpsCounterAnalytics>
+namespace Microsoft
 {
-  public:
-    using Result = llvm::StringMap<unsigned>;
-
-    /// Constructors and destructors
-    /// @{
-    COpsCounterAnalytics()                            = default;
-    COpsCounterAnalytics(COpsCounterAnalytics const&) = delete;
-    COpsCounterAnalytics(COpsCounterAnalytics&&)      = default;
-    ~COpsCounterAnalytics()                           = default;
-    /// @}
-
-    /// Operators
-    /// @{
-    COpsCounterAnalytics& operator=(COpsCounterAnalytics const&) = delete;
-    COpsCounterAnalytics& operator=(COpsCounterAnalytics&&) = delete;
-    /// @}
-
-    /// Functions required by LLVM
-    /// @{
-    Result run(llvm::Function& function, llvm::FunctionAnalysisManager& /*unused*/);
-    /// @}
-  private:
-    static llvm::AnalysisKey Key;
-    friend struct llvm::AnalysisInfoMixin<COpsCounterAnalytics>;
-};
-
-class COpsCounterPrinter : public llvm::PassInfoMixin<COpsCounterPrinter>
+namespace Quantum
 {
-  public:
-    /// Constructors and destructors
-    /// @{
-    explicit COpsCounterPrinter(llvm::raw_ostream& out_stream);
-    COpsCounterPrinter()                          = delete;
-    COpsCounterPrinter(COpsCounterPrinter const&) = delete;
-    COpsCounterPrinter(COpsCounterPrinter&&)      = default;
-    ~COpsCounterPrinter()                         = default;
-    /// @}
 
-    /// Operators
-    /// @{
-    COpsCounterPrinter& operator=(COpsCounterPrinter const&) = delete;
-    COpsCounterPrinter& operator=(COpsCounterPrinter&&) = delete;
-    /// @}
+    class COpsCounterAnalytics : public llvm::AnalysisInfoMixin<COpsCounterAnalytics>
+    {
+      public:
+        using Result = llvm::StringMap<unsigned>;
 
-    /// Functions required by LLVM
-    /// @{
-    llvm::PreservedAnalyses run(llvm::Function& function, llvm::FunctionAnalysisManager& fam);
-    static bool             isRequired();
-    /// @}
-  private:
-    llvm::raw_ostream& out_stream_;
-};
+        /// Constructors and destructors
+        /// @{
+        COpsCounterAnalytics()                            = default;
+        COpsCounterAnalytics(COpsCounterAnalytics const&) = delete;
+        COpsCounterAnalytics(COpsCounterAnalytics&&)      = default;
+        ~COpsCounterAnalytics()                           = default;
+        /// @}
+
+        /// Operators
+        /// @{
+        COpsCounterAnalytics& operator=(COpsCounterAnalytics const&) = delete;
+        COpsCounterAnalytics& operator=(COpsCounterAnalytics&&) = delete;
+        /// @}
+
+        /// Functions required by LLVM
+        /// @{
+        Result run(llvm::Function& function, llvm::FunctionAnalysisManager& /*unused*/);
+        /// @}
+      private:
+        static llvm::AnalysisKey Key;
+        friend struct llvm::AnalysisInfoMixin<COpsCounterAnalytics>;
+    };
+
+    class COpsCounterPrinter : public llvm::PassInfoMixin<COpsCounterPrinter>
+    {
+      public:
+        /// Constructors and destructors
+        /// @{
+        explicit COpsCounterPrinter(llvm::raw_ostream& out_stream);
+        COpsCounterPrinter()                          = delete;
+        COpsCounterPrinter(COpsCounterPrinter const&) = delete;
+        COpsCounterPrinter(COpsCounterPrinter&&)      = default;
+        ~COpsCounterPrinter()                         = default;
+        /// @}
+
+        /// Operators
+        /// @{
+        COpsCounterPrinter& operator=(COpsCounterPrinter const&) = delete;
+        COpsCounterPrinter& operator=(COpsCounterPrinter&&) = delete;
+        /// @}
+
+        /// Functions required by LLVM
+        /// @{
+        llvm::PreservedAnalyses run(llvm::Function& function, llvm::FunctionAnalysisManager& fam);
+        static bool             isRequired();
+        /// @}
+      private:
+        llvm::raw_ostream& out_stream_;
+    };
+
+} // namespace Quantum
+} // namespace Microsoft
