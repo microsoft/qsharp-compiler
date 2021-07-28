1866d17434397dbc901c8b180b46496d177ffde1
Troels F. Roennow <troels@wonop.com>
2021-07-28 16:47:26 +0200

    Adding expansion pass to allow allocating arrays statically

--- src/Passes/libs/ExpandStaticAllocation/ExpandStaticAllocation.hpp
+++ src/Passes/libs/ExpandStaticAllocation/ExpandStaticAllocation.hpp
@@ -3,44 +3,49 @@
 // Licensed under the MIT License.
 
 #include "Llvm.hpp"
+
 #include "QubitAllocationAnalysis/QubitAllocationAnalysis.hpp"
 
 #include <unordered_map>
 
-namespace microsoft {
-namespace quantum {
-
-class ExpandStaticAllocationPass : public llvm::PassInfoMixin<ExpandStaticAllocationPass>
+namespace microsoft
 {
-public:
-  using QubitAllocationResult = QubitAllocationAnalysisAnalytics::Result;
-  using ConstantArguments     = std::unordered_map<std::string, llvm::ConstantInt *>;
+namespace quantum
+{
 
-  /// Constructors and destructors
-  /// @{
-  ExpandStaticAllocationPass()                                   = default;
-  ExpandStaticAllocationPass(ExpandStaticAllocationPass const &) = default;
-  ExpandStaticAllocationPass(ExpandStaticAllocationPass &&)      = default;
-  ~ExpandStaticAllocationPass()                                  = default;
-  /// @}
+    class ExpandStaticAllocationPass : public llvm::PassInfoMixin<ExpandStaticAllocationPass>
+    {
+      public:
+        using QubitAllocationResult = QubitAllocationAnalysisAnalytics::Result;
+        using ConstantArguments     = std::unordered_map<std::string, llvm::ConstantInt*>;
 
-  /// Operators
-  /// @{
-  ExpandStaticAllocationPass &operator=(ExpandStaticAllocationPass const &) = default;
-  ExpandStaticAllocationPass &operator=(ExpandStaticAllocationPass &&) = default;
-  /// @}
+        /// Constructors and destructors
+        /// @{
+        ExpandStaticAllocationPass()                                  = default;
+        ExpandStaticAllocationPass(ExpandStaticAllocationPass const&) = default;
+        ExpandStaticAllocationPass(ExpandStaticAllocationPass&&)      = default;
+        ~ExpandStaticAllocationPass()                                 = default;
+        /// @}
 
-  /// Functions required by LLVM
-  /// @{
-  llvm::PreservedAnalyses run(llvm::Function &function, llvm::FunctionAnalysisManager &fam);
-  static bool             isRequired();
-  /// @}
+        /// Operators
+        /// @{
+        ExpandStaticAllocationPass& operator=(ExpandStaticAllocationPass const&) = default;
+        ExpandStaticAllocationPass& operator=(ExpandStaticAllocationPass&&) = default;
+        /// @}
 
-  /// @{
-  llvm::Function *ExpandFunctionCall(QubitAllocationResult const &depenency_graph,
-                                     llvm::Function &callee, ConstantArguments const &const_args);
-  /// @}
-};
+        /// Functions required by LLVM
+        /// @{
+        llvm::PreservedAnalyses run(llvm::Function& function, llvm::FunctionAnalysisManager& fam);
+        static bool             isRequired();
+        /// @}
 
-}  // namespace quantum
-}  // namespace microsoft
+        /// @{
+        llvm::Function* expandFunctionCall(
+            QubitAllocationResult const& depenency_graph,
+            llvm::Function&              callee,
+            ConstantArguments const&     const_args);
+        /// @}
+    };
+
+} // namespace quantum
+} // namespace microsoft
