7c8acd6dcea89c7df5a16a6dd75b6b62fca06c13
Troels F. Roennow <troels@wonop.com>
2021-08-02 08:02:30 +0200

    Creating development script

---
+++ build/development.ps1
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+$ErrorActionPreference = 'Stop'
+
+python --version
+
+throw "Preventing other steps from running."
