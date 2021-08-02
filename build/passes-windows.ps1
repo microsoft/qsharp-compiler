008c9d409aca4a1d265c27a8b189bd7deb529760
Troels F. Roennow <troels@wonop.com>
2021-08-02 11:08:12 +0200

    Refactoring names and updating windows

---
+++ build/passes-windows.ps1
@@ -0,0 +1,25 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+$ErrorActionPreference = 'Stop'
+
+python --version
+pip --version
+
+# Installing requirements for CI
+cd ../src/Passes/
+pip install -r requirements.txt
+
+# Installing Clang, CMake and LLVM
+if (!(Get-Command clang -ErrorAction SilentlyContinue)) {
+    choco install llvm --version=11.1.0
+}
+if (!(Get-Command ninja -ErrorAction SilentlyContinue)) {
+    choco install ninja
+}
+if (!(Get-Command cmake -ErrorAction SilentlyContinue)) {
+    choco install cmake
+}
+
+# Running CI
+python manage runci
