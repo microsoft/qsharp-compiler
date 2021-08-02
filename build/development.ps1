330da4bff937979e8b5e50f29faf876e0662c98a
Troels F. Roennow <troels@wonop.com>
2021-08-02 08:21:51 +0200

    Udpating development script

--- build/development.ps1
+++ build/development.ps1
@@ -4,5 +4,15 @@
 $ErrorActionPreference = 'Stop'
 
 python --version
+pip --version
+virtualenv --version
+
+# Installing requirements for CI
+cd src/Passes
+pip install -r requirements.txt
+
+# Running CI
+python manage runci
+
 
 throw "Preventing other steps from running."
