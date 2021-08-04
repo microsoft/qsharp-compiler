be473a6f1898b66708f697f3baa8b7f8995e25d7
Troels F. Roennow <troels@wonop.com>
2021-08-04 14:57:27 +0200

    Disbaling Windows CI

--- build/passes-windows.ps1
+++ build/passes-windows.ps1
@@ -18,23 +18,9 @@ if (!(Get-Command clang -ErrorAction SilentlyContinue)) {
     & (Join-Path $env:CONDA scripts conda.exe) install -c conda-forge llvmdev=11.1.0
 }
 
-# if (!(Get-Command cmake -ErrorAction SilentlyContinue)) {
-#     choco install cmake
-# }
-
 refreshenv
 $env:Path += ";C:\Miniconda\Library\LLVM\bin\"
 $env:LLVM = "C:\Miniconda\Library\LLVM\"
-
-
-dir "C:\Program Files\LLVM\"
-clang.exe --version
-
-cmake.exe --version
-
-clang-format.exe --version
-
-clang-tidy.exe --version
 
 cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat`" && set > %temp%\vcvars.txt"
 
