bb2a40e42ee7d194d1178cdcbaf1bf568490d086
Troels F. Roennow <troels@wonop.com>
2021-08-10 09:01:14 +0200

    Fixing style

--- src/Passes/libs/OpsCounter/LibOpsCounter.cpp
+++ src/Passes/libs/OpsCounter/LibOpsCounter.cpp
@@ -16,27 +16,30 @@ llvm::PassPluginLibraryInfo getOpsCounterPluginInfo()
     using namespace microsoft::quantum;
     using namespace llvm;
 
-    return {LLVM_PLUGIN_API_VERSION, "OpsCounter", LLVM_VERSION_STRING, [](PassBuilder& pb) {
-                // Registering the printer
-                pb.registerPipelineParsingCallback(
-                    [](StringRef name, FunctionPassManager& fpm, ArrayRef<PassBuilder::PipelineElement> /*unused*/) {
-                        if (name == "print<operation-counter>")
-                        {
-                            fpm.addPass(OpsCounterPrinter(llvm::errs()));
-                            return true;
-                        }
-                        return false;
-                    });
-
-                pb.registerVectorizerStartEPCallback(
-                    [](llvm::FunctionPassManager& fpm, llvm::PassBuilder::OptimizationLevel /*level*/) {
+    return {
+        LLVM_PLUGIN_API_VERSION, "OpsCounter", LLVM_VERSION_STRING,
+        [](PassBuilder& pb)
+        {
+            // Registering the printer
+            pb.registerPipelineParsingCallback(
+                [](StringRef name, FunctionPassManager& fpm, ArrayRef<PassBuilder::PipelineElement> /*unused*/)
+                {
+                    if (name == "print<operation-counter>")
+                    {
                         fpm.addPass(OpsCounterPrinter(llvm::errs()));
-                    });
+                        return true;
+                    }
+                    return false;
+                });
 
-                // Registering the analysis module
-                pb.registerAnalysisRegistrationCallback(
-                    [](FunctionAnalysisManager& fam) { fam.registerPass([] { return OpsCounterAnalytics(); }); });
-            }};
+            pb.registerVectorizerStartEPCallback(
+                [](llvm::FunctionPassManager& fpm, llvm::PassBuilder::OptimizationLevel /*level*/)
+                { fpm.addPass(OpsCounterPrinter(llvm::errs())); });
+
+            // Registering the analysis module
+            pb.registerAnalysisRegistrationCallback([](FunctionAnalysisManager& fam)
+                                                    { fam.registerPass([] { return OpsCounterAnalytics(); }); });
+        }};
 }
 
 } // namespace
