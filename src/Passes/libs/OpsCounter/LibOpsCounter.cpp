16d4cd33f8e2deca38a4598212407076a571a5ad
Troels F. Roennow <troels@wonop.com>
2021-08-10 09:13:32 +0200

    Fixing style

--- src/Passes/libs/OpsCounter/LibOpsCounter.cpp
+++ src/Passes/libs/OpsCounter/LibOpsCounter.cpp
@@ -16,30 +16,27 @@ llvm::PassPluginLibraryInfo getOpsCounterPluginInfo()
     using namespace microsoft::quantum;
     using namespace llvm;
 
-    return {
-        LLVM_PLUGIN_API_VERSION, "OpsCounter", LLVM_VERSION_STRING,
-        [](PassBuilder& pb)
-        {
-            // Registering the printer
-            pb.registerPipelineParsingCallback(
-                [](StringRef name, FunctionPassManager& fpm, ArrayRef<PassBuilder::PipelineElement> /*unused*/)
-                {
-                    if (name == "print<operation-counter>")
-                    {
+    return {LLVM_PLUGIN_API_VERSION, "OpsCounter", LLVM_VERSION_STRING, [](PassBuilder& pb) {
+                // Registering the printer
+                pb.registerPipelineParsingCallback(
+                    [](StringRef name, FunctionPassManager& fpm, ArrayRef<PassBuilder::PipelineElement> /*unused*/) {
+                        if (name == "print<operation-counter>")
+                        {
+                            fpm.addPass(OpsCounterPrinter(llvm::errs()));
+                            return true;
+                        }
+                        return false;
+                    });
+
+                pb.registerVectorizerStartEPCallback(
+                    [](llvm::FunctionPassManager& fpm, llvm::PassBuilder::OptimizationLevel /*level*/) {
                         fpm.addPass(OpsCounterPrinter(llvm::errs()));
-                        return true;
-                    }
-                    return false;
-                });
+                    });
 
-            pb.registerVectorizerStartEPCallback(
-                [](llvm::FunctionPassManager& fpm, llvm::PassBuilder::OptimizationLevel /*level*/)
-                { fpm.addPass(OpsCounterPrinter(llvm::errs())); });
-
-            // Registering the analysis module
-            pb.registerAnalysisRegistrationCallback([](FunctionAnalysisManager& fam)
-                                                    { fam.registerPass([] { return OpsCounterAnalytics(); }); });
-        }};
+                // Registering the analysis module
+                pb.registerAnalysisRegistrationCallback(
+                    [](FunctionAnalysisManager& fam) { fam.registerPass([] { return OpsCounterAnalytics(); }); });
+            }};
 }
 
 } // namespace
