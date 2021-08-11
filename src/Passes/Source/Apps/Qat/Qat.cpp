85e76b63ce8549fd91dfd6d4e57deb482b51f7da
Troels F. Roennow <troels@wonop.com>
2021-08-11 19:40:20 +0200

    Creating prototype QAT

--- src/Passes/Source/Apps/Qat/Qat.cpp
+++ src/Passes/Source/Apps/Qat/Qat.cpp
@@ -1,6 +1,10 @@
 #include "Llvm/Llvm.hpp"
+#include "Passes/TransformationRule/TransformationRule.hpp"
+#include "Rules/Factory.hpp"
 
 using namespace llvm;
+using namespace microsoft::quantum;
+
 int main(int /*argc*/, char **argv)
 {
   LLVMContext  context;
@@ -8,18 +12,51 @@ int main(int /*argc*/, char **argv)
   auto         module = parseIRFile(argv[1], error, context);
   if (module)
   {
-    ModulePassManager   MPM;
-    FunctionPassManager FPM;
 
-    // InstSimplifyPass is a function pass
+    llvm::PassBuilder             passBuilder;
+    llvm::LoopAnalysisManager     loopAnalysisManager(true);  // true is just to output debug info
+    llvm::FunctionAnalysisManager functionAnalysisManager(true);
+    llvm::CGSCCAnalysisManager    cGSCCAnalysisManager(true);
+    llvm::ModuleAnalysisManager   moduleAnalysisManager(true);
 
-    FPM.addPass(LoopSimplifyPass());
-    //    FPM.addPass(LoopUnrollPass());
+    passBuilder.registerModuleAnalyses(moduleAnalysisManager);
+    passBuilder.registerCGSCCAnalyses(cGSCCAnalysisManager);
+    passBuilder.registerFunctionAnalyses(functionAnalysisManager);
+    passBuilder.registerLoopAnalyses(loopAnalysisManager);
+    // This is the important line:
+    passBuilder.crossRegisterProxies(loopAnalysisManager, functionAnalysisManager,
+                                     cGSCCAnalysisManager, moduleAnalysisManager);
 
-    MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
+    auto functionPassManager = passBuilder.buildFunctionSimplificationPipeline(
+        llvm::PassBuilder::OptimizationLevel::O1, llvm::PassBuilder::ThinLTOPhase::None, true);
 
-    // MPM.run(*module);
-    //    m->print(llvm)
+    RuleSet rule_set;
+
+    // Defining the mapping
+    auto factory = RuleFactory(rule_set);
+
+    factory.useStaticQuantumArrayAllocation();
+    factory.useStaticQuantumAllocation();
+    factory.useStaticResultAllocation();
+
+    factory.optimiseBranchQuatumOne();
+    //  factory.optimiseBranchQuatumZero();
+
+    factory.disableReferenceCounting();
+    factory.disableAliasCounting();
+    factory.disableStringSupport();
+
+    functionPassManager.addPass(TransformationRulePass(std::move(rule_set)));
+
+    llvm::ModulePassManager modulePassManager =
+        passBuilder.buildPerModuleDefaultPipeline(llvm::PassBuilder::OptimizationLevel::O1);
+
+    // https://llvm.org/docs/NewPassManager.html
+    // modulePassManager.addPass(createModuleToCGSCCPassAdaptor(...));
+    // InlinerPass()
+
+    modulePassManager.run(*module, moduleAnalysisManager);
+
     llvm::errs() << *module << "\n";
   }
 
