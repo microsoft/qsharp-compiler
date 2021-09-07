c2436074e586e79e7dca6ee7bc0b82145252938a
Troels F. Roennow <troels@wonop.com>
2021-09-07 11:06:20 +0200

    More refactoring

--- src/Passes/Source/ProfilePass/Configuration.hpp
+++ src/Passes/Source/ProfilePass/Configuration.hpp
@@ -4,64 +4,69 @@
 
 #include "Commandline/ConfigurationManager.hpp"
 
-namespace microsoft
+namespace microsoft {
+namespace quantum {
+
+struct ProfilePassConfiguration
 {
-namespace quantum
-{
+  void setup(ConfigurationManager &config)
+  {
+    config.setSectionName("Pass configuration",
+                          "Configuration of the pass and its corresponding optimisations.");
+    config.addParameter(delete_dead_code, "delete-dead-code", "Deleted dead code.");
+    config.addParameter(clone_functions, "clone-functions",
+                        "Clone functions to ensure correct qubit allocation.");
 
-    struct ProfilePassConfiguration
-    {
-        void setup(ConfigurationManager& config)
-        {
-            config.setSectionName(
-                "Pass configuration", "Configuration of the pass and its corresponding optimisations.");
-            config.addParameter(delete_dead_code, "delete-dead-code", "Deleted dead code.");
-            config.addParameter(
-                clone_functions, "clone-functions", "Clone functions to ensure correct qubit allocation.");
+    config.addParameter(max_recursion, "max-recursion", "max-recursion");
+    config.addParameter(reuse_qubits, "reuse-qubits", "reuse-qubits");
 
-            config.addParameter(max_recursion, "max-recursion", "max-recursion");
-            config.addParameter(reuse_qubits, "reuse-qubits", "reuse-qubits");
+    config.addParameter(entry_point_attr, "entry-point-attr",
+                        "Specifies the attribute indicating the entry point.");
 
-            // Not implemented yet
-            config.addParameter(group_measurements, "group-measurements", "NOT IMPLEMENTED - group-measurements");
-            config.addParameter(one_shot_measurement, "one-shot-measurement", "NOT IMPLEMENTED - one-shot-measurement");
-        }
+    // Not implemented yet
+    config.addParameter(group_measurements, "group-measurements",
+                        "NOT IMPLEMENTED - group-measurements");
+    config.addParameter(one_shot_measurement, "one-shot-measurement",
+                        "NOT IMPLEMENTED - one-shot-measurement");
+  }
 
-        static ProfilePassConfiguration disable()
-        {
-            ProfilePassConfiguration ret;
-            ret.delete_dead_code     = false;
-            ret.clone_functions      = false;
-            ret.apply_rules_to_all   = true;
-            ret.max_recursion        = 512;
-            ret.reuse_qubits         = false;
-            ret.group_measurements   = false;
-            ret.one_shot_measurement = false;
-            return ret;
-        }
+  static ProfilePassConfiguration disable()
+  {
+    ProfilePassConfiguration ret;
+    ret.delete_dead_code              = false;
+    ret.clone_functions               = false;
+    ret.transform_execution_path_only = false;
+    ret.max_recursion                 = 512;
+    ret.reuse_qubits                  = false;
+    ret.group_measurements            = false;
+    ret.one_shot_measurement          = false;
+    return ret;
+  }
 
-        /// @{
-        bool delete_dead_code{true};
-        bool clone_functions{true};
-        bool apply_rules_to_all{false}; // TODO: Rename to "follow_execution_path"
-        /// @}
+  /// @{
+  bool delete_dead_code{true};
+  bool clone_functions{true};
+  bool transform_execution_path_only{true};
+  /// @}
 
-        /// Const-expression
-        /// @{
-        uint64_t max_recursion{512};
-        /// @}
+  /// Const-expression
+  /// @{
+  uint64_t max_recursion{512};
+  /// @}
 
-        /// Allocation options
-        /// @{
-        bool reuse_qubits{true}; // NOT IMPLEMENTED
-        /// @}
+  /// Allocation options
+  /// @{
+  bool reuse_qubits{true};  // NOT IMPLEMENTED
+  /// @}
 
-        /// Measurement
-        /// @{
-        bool group_measurements{false};  // NOT IMPLEMENTED
-        bool one_shot_measurement{true}; // NOT IMPLEMENTED
-                                         /// @}
-    };
+  /// Measurement
+  /// @{
+  bool group_measurements{false};   // NOT IMPLEMENTED
+  bool one_shot_measurement{true};  // NOT IMPLEMENTED
+                                    /// @}
 
-} // namespace quantum
-} // namespace microsoft
+  std::string entry_point_attr{"EntryPoint"};
+};
+
+}  // namespace quantum
+}  // namespace microsoft
--- src/Passes/Source/TestTools/IrManipulationTestHelper.hpp
+++ src/Passes/Source/TestTools/IrManipulationTestHelper.hpp
@@ -3,141 +3,139 @@
 // Licensed under the MIT License.
 
 #include "Generators/IProfileGenerator.hpp"
-
 #include "Llvm/Llvm.hpp"
 
 #include <unordered_set>
 #include <vector>
 
-namespace microsoft
+namespace microsoft {
+namespace quantum {
+
+class IrManipulationTestHelper
 {
-namespace quantum
-{
+public:
+  using String            = std::string;
+  using LLVMContext       = llvm::LLVMContext;
+  using SMDiagnostic      = llvm::SMDiagnostic;
+  using Module            = llvm::Module;
+  using ModulePtr         = std::unique_ptr<Module>;
+  using Strings           = std::vector<String>;
+  using OptimizationLevel = llvm::PassBuilder::OptimizationLevel;
+  using ProfilePtr        = std::shared_ptr<IProfileGenerator>;
 
-    class IrManipulationTestHelper
-    {
-      public:
-        using String            = std::string;
-        using LLVMContext       = llvm::LLVMContext;
-        using SMDiagnostic      = llvm::SMDiagnostic;
-        using Module            = llvm::Module;
-        using ModulePtr         = std::unique_ptr<Module>;
-        using Strings           = std::vector<String>;
-        using OptimizationLevel = llvm::PassBuilder::OptimizationLevel;
-        using ProfilePtr        = std::shared_ptr<IProfileGenerator>;
+  /// IrManipulationTestHelper is default constructible with no ability to move
+  /// or copy.
+  /// @{
+  IrManipulationTestHelper();
+  IrManipulationTestHelper(IrManipulationTestHelper const &) = delete;
+  IrManipulationTestHelper &operator=(IrManipulationTestHelper const &) = delete;
+  IrManipulationTestHelper(IrManipulationTestHelper &&)                 = delete;
+  IrManipulationTestHelper &operator=(IrManipulationTestHelper &&) = delete;
+  /// @}
 
-        /// IrManipulationTestHelper is default constructible with no ability to move
-        /// or copy.
-        /// @{
-        IrManipulationTestHelper();
-        IrManipulationTestHelper(IrManipulationTestHelper const&) = delete;
-        IrManipulationTestHelper& operator=(IrManipulationTestHelper const&) = delete;
-        IrManipulationTestHelper(IrManipulationTestHelper&&)                 = delete;
-        IrManipulationTestHelper& operator=(IrManipulationTestHelper&&) = delete;
-        /// @}
+  /// Output functions
+  /// @{
 
-        /// Output functions
-        /// @{
+  /// Generates a string for the IR currently held in the module.
+  String toString() const;
 
-        /// Generates a string for the IR currently held in the module.
-        String toString() const;
+  /// Generates a list of instructions for the main function in the module.
+  Strings toBodyInstructions() const;
+  /// @}
 
-        /// Generates a list of instructions for the main function in the module.
-        Strings toBodyInstructions() const;
-        /// @}
+  /// Test functions
+  /// @{
 
-        /// Test functions
-        /// @{
+  /// Tests whether the main body contains a sequence of instructions. This function
+  /// ignores instructions inbetween the instruction set given.
+  bool hasInstructionSequence(Strings const &instructions);
 
-        /// Tests whether the main body contains a sequence of instructions. This function
-        /// ignores instructions inbetween the instruction set given.
-        bool hasInstructionSequence(Strings const& instructions);
+  /// Applies a profile to the module to allow which transforms the IR. This
+  /// allow us to write small profiles to test a single piece of transformation.
+  void applyProfile(ProfilePtr const &       profile,
+                    OptimizationLevel const &optimisation_level = OptimizationLevel::O0,
+                    bool                     debug              = false);
+  /// @}
 
-        /// Applies a profile to the module to allow which transforms the IR. This
-        /// allow us to write small profiles to test a single piece of transformation.
-        void applyProfile(
-            ProfilePtr const&        profile,
-            OptimizationLevel const& optimisation_level = OptimizationLevel::O0,
-            bool                     debug              = false);
-        /// @}
+  /// Declaration of partial or full IR
+  /// @{
 
-        /// Declaration of partial or full IR
-        /// @{
+  /// Declares a opaque type. Only the name of the type should be supplied to
+  /// this function. Example usage
+  ///
+  /// ```
+  /// irmanip.declareOpaque("Qubit");
+  /// ```
+  void declareOpaque(String const &name);
 
-        /// Declares a opaque type. Only the name of the type should be supplied to
-        /// this function. Example usage
-        ///
-        /// ```
-        /// irmanip.declareOpaque("Qubit");
-        /// ```
-        void declareOpaque(String const& name);
+  /// Declares a function. The full signature should be supplied to
+  /// as the first argument. Example usage
+  ///
+  /// ```
+  /// irmanip.declareOpaque("%Result* @__quantum__rt__result_get_zero()");
+  /// ```
+  void declareFunction(String const &declaration);
 
-        /// Declares a function. The full signature should be supplied to
-        /// as the first argument. Example usage
-        ///
-        /// ```
-        /// irmanip.declareOpaque("%Result* @__quantum__rt__result_get_zero()");
-        /// ```
-        void declareFunction(String const& declaration);
+  /// Creates an LLVM module given a function body. This function makes use
+  /// of the inputs from IrManipulationTestHelper::declareOpaque and
+  /// IrManipulationTestHelper::declareFunction to construct the full
+  /// IR. Example usage:
+  ///
+  /// ```
+  /// irmanip.fromBodyString(R"script(
+  /// %leftMessage = call %Qubit* @__quantum__rt__qubit_allocate()
+  /// call void @__quantum__qis__h__body(%Qubit* %leftMessage)
+  /// )script");
+  /// ```
+  ///
+  /// Returns false if the IR is invalid.
+  bool fromBodyString(String const &body);
 
-        /// Creates an LLVM module given a function body. This function makes use
-        /// of the inputs from IrManipulationTestHelper::declareOpaque and
-        /// IrManipulationTestHelper::declareFunction to construct the full
-        /// IR. Example usage:
-        ///
-        /// ```
-        /// irmanip.fromBodyString(R"script(
-        /// %leftMessage = call %Qubit* @__quantum__rt__qubit_allocate()
-        /// call void @__quantum__qis__h__body(%Qubit* %leftMessage)
-        /// )script");
-        /// ```
-        ///
-        /// Returns false if the IR is invalid.
-        bool fromBodyString(String const& body);
+  /// Generates a script given the body of the main function.
+  String generateScript(String const &body) const;
 
-        /// Generates a script given the body of the main function.
-        String generateScript(String const& body) const;
+  /// Creates an LLVM module given from a fully specified IR. This function
+  /// ignores all inputs from IrManipulationTestHelper::declareOpaque and
+  /// IrManipulationTestHelper::declareFunction.
+  ///
+  /// Returns false if the IR is invalid.
+  bool fromString(String const &data);
 
-        /// Creates an LLVM module given from a fully specified IR. This function
-        /// ignores all inputs from IrManipulationTestHelper::declareOpaque and
-        /// IrManipulationTestHelper::declareFunction.
-        ///
-        /// Returns false if the IR is invalid.
-        bool fromString(String const& data);
+  /// Gets an error message if the compilation failed.
+  String getErrorMessage() const;
 
-        /// Gets an error message if the compilation failed.
-        String getErrorMessage() const;
+  /// @}
 
-        /// @}
+  bool isModuleBroken();
 
-        /// Acccess member functions
-        /// @{
-        llvm::PassBuilder&             passBuilder();
-        llvm::LoopAnalysisManager&     loopAnalysisManager();
-        llvm::FunctionAnalysisManager& functionAnalysisManager();
-        llvm::CGSCCAnalysisManager&    gsccAnalysisManager();
-        llvm::ModuleAnalysisManager&   moduleAnalysisManager();
-        ModulePtr&                     module();
-        /// @}
-      private:
-        std::unordered_set<std::string> opaque_declarations_{};
-        std::unordered_set<std::string> function_declarations_{};
+  /// Acccess member functions
+  /// @{
+  llvm::PassBuilder &            passBuilder();
+  llvm::LoopAnalysisManager &    loopAnalysisManager();
+  llvm::FunctionAnalysisManager &functionAnalysisManager();
+  llvm::CGSCCAnalysisManager &   gsccAnalysisManager();
+  llvm::ModuleAnalysisManager &  moduleAnalysisManager();
+  ModulePtr &                    module();
+  /// @}
+private:
+  std::unordered_set<std::string> opaque_declarations_{};
+  std::unordered_set<std::string> function_declarations_{};
 
-        /// @{
-        SMDiagnostic error_;
-        LLVMContext  context_;
-        ModulePtr    module_;
-        /// @}
+  /// @{
+  SMDiagnostic error_;
+  LLVMContext  context_;
+  ModulePtr    module_;
+  /// @}
 
-        /// Objects used to run a set of passes
-        /// @{
-        llvm::PassBuilder             pass_builder_;
-        llvm::LoopAnalysisManager     loop_analysis_manager_;
-        llvm::FunctionAnalysisManager function_analysis_manager_;
-        llvm::CGSCCAnalysisManager    gscc_analysis_manager_;
-        llvm::ModuleAnalysisManager   module_analysis_manager_;
-        /// @}
-    };
+  /// Objects used to run a set of passes
+  /// @{
+  llvm::PassBuilder             pass_builder_;
+  llvm::LoopAnalysisManager     loop_analysis_manager_;
+  llvm::FunctionAnalysisManager function_analysis_manager_;
+  llvm::CGSCCAnalysisManager    gscc_analysis_manager_;
+  llvm::ModuleAnalysisManager   module_analysis_manager_;
+  /// @}
+};
 
-} // namespace quantum
-} // namespace microsoft
+}  // namespace quantum
+}  // namespace microsoft
