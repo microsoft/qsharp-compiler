5eb7bcda8950487bd30ebe7ba1616032104832fd
Troels F. Roennow <troels@wonop.com>
2021-09-15 15:40:08 +0200

    Updating style

--- src/Passes/Source/Rules/RuleSet.hpp
+++ src/Passes/Source/Rules/RuleSet.hpp
@@ -3,69 +3,72 @@
 // Licensed under the MIT License.
 
 #include "AllocationManager/IAllocationManager.hpp"
-#include "Llvm/Llvm.hpp"
 #include "Rules/OperandPrototype.hpp"
 #include "Rules/ReplacementRule.hpp"
+
+#include "Llvm/Llvm.hpp"
 
 #include <memory>
 #include <vector>
 
-namespace microsoft {
-namespace quantum {
-
-/// RuleSet contains a set of replacement rules and the corresponding logic
-/// to apply the rules. The class allows one to apply the rules by which
-/// each rule is tested one-by-one until a successful attempt at performing
-/// a replace has happened, or the list was exhausted.
-class RuleSet
+namespace microsoft
 {
-public:
-  using ReplacementRulePtr   = std::shared_ptr<ReplacementRule>;
-  using Rules                = std::vector<ReplacementRulePtr>;
-  using Replacements         = ReplacementRule::Replacements;
-  using Captures             = IOperandPrototype::Captures;
-  using Instruction          = llvm::Instruction;
-  using Value                = llvm::Value;
-  using Builder              = ReplacementRule::Builder;
-  using AllocationManagerPtr = IAllocationManager::AllocationManagerPtr;
+namespace quantum
+{
 
-  // Constructors
-  //
-  RuleSet()                = default;
-  RuleSet(RuleSet const &) = default;
-  RuleSet(RuleSet &&)      = default;
-  ~RuleSet()               = default;
+    /// RuleSet contains a set of replacement rules and the corresponding logic
+    /// to apply the rules. The class allows one to apply the rules by which
+    /// each rule is tested one-by-one until a successful attempt at performing
+    /// a replace has happened, or the list was exhausted.
+    class RuleSet
+    {
+      public:
+        using ReplacementRulePtr   = std::shared_ptr<ReplacementRule>;
+        using Rules                = std::vector<ReplacementRulePtr>;
+        using Replacements         = ReplacementRule::Replacements;
+        using Captures             = IOperandPrototype::Captures;
+        using Instruction          = llvm::Instruction;
+        using Value                = llvm::Value;
+        using Builder              = ReplacementRule::Builder;
+        using AllocationManagerPtr = IAllocationManager::AllocationManagerPtr;
 
-  // Operators
-  //
+        // Constructors
+        //
+        RuleSet()               = default;
+        RuleSet(RuleSet const&) = default;
+        RuleSet(RuleSet&&)      = default;
+        ~RuleSet()              = default;
 
-  RuleSet &operator=(RuleSet const &) = default;
-  RuleSet &operator=(RuleSet &&) = default;
+        // Operators
+        //
 
-  // Operating rule sets
-  //
+        RuleSet& operator=(RuleSet const&) = default;
+        RuleSet& operator=(RuleSet&&) = default;
 
-  /// Matches patterns and runs the replacement routines if a match
-  /// is found. The function returns true if a pattern is matched and
-  /// and the replacement was a success. In all other cases, it returns
-  /// false.
-  bool matchAndReplace(Instruction *value, Replacements &replacements);
+        // Operating rule sets
+        //
 
-  // Set up and configuration
-  //
+        /// Matches patterns and runs the replacement routines if a match
+        /// is found. The function returns true if a pattern is matched and
+        /// and the replacement was a success. In all other cases, it returns
+        /// false.
+        bool matchAndReplace(Instruction* value, Replacements& replacements);
 
-  /// Adds a new replacement rule to the set.
-  void addRule(ReplacementRulePtr const &rule);
+        // Set up and configuration
+        //
 
-  /// Clears the rule set for all rules.
-  void clear();
+        /// Adds a new replacement rule to the set.
+        void addRule(ReplacementRulePtr const& rule);
 
-  /// Returns the size of the rule set.
-  uint64_t size() const;
+        /// Clears the rule set for all rules.
+        void clear();
 
-private:
-  Rules rules_;  ///< Rules that describes QIR mappings
-};
+        /// Returns the size of the rule set.
+        uint64_t size() const;
 
-}  // namespace quantum
-}  // namespace microsoft
+      private:
+        Rules rules_; ///< Rules that describes QIR mappings
+    };
+
+} // namespace quantum
+} // namespace microsoft
