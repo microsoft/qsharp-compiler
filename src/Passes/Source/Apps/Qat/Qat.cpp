15ace35e093fe33ee64c1d4fd448c2dc03da44ac
Troels F. Roennow <troels@wonop.com>
2021-09-15 14:50:06 +0200

    Updating style

--- src/Passes/Source/Apps/Qat/Qat.cpp
+++ src/Passes/Source/Apps/Qat/Qat.cpp
@@ -47,10 +47,11 @@
 #include "Commandline/ParameterParser.hpp"
 #include "Generators/DefaultProfileGenerator.hpp"
 #include "Generators/LlvmPassesConfig.hpp"
-#include "Llvm/Llvm.hpp"
 #include "Profile/Profile.hpp"
 #include "RuleTransformationPass/Configuration.hpp"
 #include "Rules/FactoryConfig.hpp"
+
+#include "Llvm/Llvm.hpp"
 
 #include <iomanip>
 #include <iostream>
@@ -59,121 +60,121 @@
 using namespace llvm;
 using namespace microsoft::quantum;
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-  try
-  {
-    // Default generator. A future version of QAT may allow the generator to be selected
-    // through the command line, but it is hard coded for now.
-    auto generator = std::make_shared<DefaultProfileGenerator>();
-
-    // Configuration and command line parsing
-    //
-
-    ConfigurationManager &configuration_manager = generator->configurationManager();
-    configuration_manager.addConfig<QatConfig>();
-    configuration_manager.addConfig<FactoryConfiguration>();
-
-    ParameterParser parser;
-    configuration_manager.setupArguments(parser);
-    parser.parseArgs(argc, argv);
-    configuration_manager.configure(parser);
-
-    // Getting the main configuration
-    auto const &config = configuration_manager.get<QatConfig>();
-
-    // In case we debug, we also print the settings to allow provide a full
-    // picture of what is going. This step deliberately comes before validating
-    // the input to allow dumping the configuration if something goes wrong.
-    if (config.dumpConfig())
+    try
     {
-      configuration_manager.printConfiguration();
+        // Default generator. A future version of QAT may allow the generator to be selected
+        // through the command line, but it is hard coded for now.
+        auto generator = std::make_shared<DefaultProfileGenerator>();
+
+        // Configuration and command line parsing
+        //
+
+        ConfigurationManager& configuration_manager = generator->configurationManager();
+        configuration_manager.addConfig<QatConfig>();
+        configuration_manager.addConfig<FactoryConfiguration>();
+
+        ParameterParser parser;
+        configuration_manager.setupArguments(parser);
+        parser.parseArgs(argc, argv);
+        configuration_manager.configure(parser);
+
+        // Getting the main configuration
+        auto const& config = configuration_manager.get<QatConfig>();
+
+        // In case we debug, we also print the settings to allow provide a full
+        // picture of what is going. This step deliberately comes before validating
+        // the input to allow dumping the configuration if something goes wrong.
+        if (config.dumpConfig())
+        {
+            configuration_manager.printConfiguration();
+        }
+
+        // Checking that we have sufficient information to proceed. If not we print
+        // usage instructions and the corresponding description of how to use the tool.
+        if (parser.arguments().empty())
+        {
+            std::cerr << "Usage: " << argv[0] << " [options] filename" << std::endl;
+            configuration_manager.printHelp();
+            std::cerr << "\n";
+            exit(-1);
+        }
+
+        // Loading IR from file.
+        //
+
+        LLVMContext  context;
+        SMDiagnostic error;
+        auto         module = parseIRFile(parser.getArg(0), error, context);
+
+        if (!module)
+        {
+            std::cerr << "Invalid IR." << std::endl;
+            exit(-1);
+        }
+
+        // Getting the optimisation level
+        //
+        auto optimisation_level = llvm::PassBuilder::OptimizationLevel::O0;
+
+        // Setting the optimisation level
+        if (config.opt1())
+        {
+            optimisation_level = llvm::PassBuilder::OptimizationLevel::O1;
+        }
+
+        if (config.opt2())
+        {
+            optimisation_level = llvm::PassBuilder::OptimizationLevel::O2;
+        }
+
+        if (config.opt3())
+        {
+            optimisation_level = llvm::PassBuilder::OptimizationLevel::O3;
+        }
+
+        // Profile manipulation
+        //
+
+        // Creating the profile that will be used for generation and validation
+        auto profile = generator->newProfile(optimisation_level, config.debug());
+
+        if (config.generate())
+        {
+            profile.apply(*module);
+        }
+
+        // We deliberately emit LLVM prior to verification and validation
+        // to allow output the IR for debugging purposes.
+        if (config.emitLlvm())
+        {
+            llvm::outs() << *module << "\n";
+        }
+
+        if (config.verifyModule())
+        {
+            if (!profile.verify(*module))
+            {
+                std::cerr << "IR is broken." << std::endl;
+                exit(-1);
+            }
+        }
+
+        if (config.validate())
+        {
+            if (!profile.validate(*module))
+            {
+                std::cerr << "QIR is not compliant with profile." << std::endl;
+                exit(-1);
+            }
+        }
     }
-
-    // Checking that we have sufficient information to proceed. If not we print
-    // usage instructions and the corresponding description of how to use the tool.
-    if (parser.arguments().empty())
+    catch (std::exception const& e)
     {
-      std::cerr << "Usage: " << argv[0] << " [options] filename" << std::endl;
-      configuration_manager.printHelp();
-      std::cerr << "\n";
-      exit(-1);
-    }
-
-    // Loading IR from file.
-    //
-
-    LLVMContext  context;
-    SMDiagnostic error;
-    auto         module = parseIRFile(parser.getArg(0), error, context);
-
-    if (!module)
-    {
-      std::cerr << "Invalid IR." << std::endl;
-      exit(-1);
-    }
-
-    // Getting the optimisation level
-    //
-    auto optimisation_level = llvm::PassBuilder::OptimizationLevel::O0;
-
-    // Setting the optimisation level
-    if (config.opt1())
-    {
-      optimisation_level = llvm::PassBuilder::OptimizationLevel::O1;
-    }
-
-    if (config.opt2())
-    {
-      optimisation_level = llvm::PassBuilder::OptimizationLevel::O2;
-    }
-
-    if (config.opt3())
-    {
-      optimisation_level = llvm::PassBuilder::OptimizationLevel::O3;
-    }
-
-    // Profile manipulation
-    //
-
-    // Creating the profile that will be used for generation and validation
-    auto profile = generator->newProfile(optimisation_level, config.debug());
-
-    if (config.generate())
-    {
-      profile.apply(*module);
-    }
-
-    // We deliberately emit LLVM prior to verification and validation
-    // to allow output the IR for debugging purposes.
-    if (config.emitLlvm())
-    {
-      llvm::outs() << *module << "\n";
-    }
-
-    if (config.verifyModule())
-    {
-      if (!profile.verify(*module))
-      {
-        std::cerr << "IR is broken." << std::endl;
+        std::cerr << "An error occurred: " << e.what() << std::endl;
         exit(-1);
-      }
     }
 
-    if (config.validate())
-    {
-      if (!profile.validate(*module))
-      {
-        std::cerr << "QIR is not compliant with profile." << std::endl;
-        exit(-1);
-      }
-    }
-  }
-  catch (std::exception const &e)
-  {
-    std::cerr << "An error occurred: " << e.what() << std::endl;
-    exit(-1);
-  }
-
-  return 0;
+    return 0;
 }
--- src/Passes/Source/Generators/IProfileGenerator.hpp
+++ src/Passes/Source/Generators/IProfileGenerator.hpp
@@ -3,119 +3,121 @@
 // Licensed under the MIT License.
 
 #include "Commandline/ConfigurationManager.hpp"
-#include "Llvm/Llvm.hpp"
 #include "Profile/Profile.hpp"
 
-namespace microsoft {
-namespace quantum {
+#include "Llvm/Llvm.hpp"
 
-class IProfileGenerator
+namespace microsoft
 {
-public:
-  // LLVM types
-  //
-  using PassBuilder             = llvm::PassBuilder;
-  using OptimizationLevel       = PassBuilder::OptimizationLevel;
-  using FunctionAnalysisManager = llvm::FunctionAnalysisManager;
-
-  // Types used for defining a component
-  //
-  using String = std::string;
-
-  /// Setup function that uses a configuration type R to
-  /// configure the profile and/or generator.
-  template <typename R>
-  using SetupFunction = std::function<void(R const &, IProfileGenerator *, Profile &)>;
-
-  /// Wrapper function type for invoking the profile setup function
-  using SetupFunctionWrapper = std::function<void(IProfileGenerator *, Profile &)>;
-
-  /// List of components to be configured.
-  using Components = std::vector<std::pair<String, SetupFunctionWrapper>>;
-
-  // Construction, moves and copies
-  //
-
-  IProfileGenerator()                          = default;
-  ~IProfileGenerator()                         = default;
-  IProfileGenerator(IProfileGenerator const &) = delete;
-  IProfileGenerator(IProfileGenerator &&)      = delete;
-  IProfileGenerator &operator=(IProfileGenerator const &) = delete;
-  IProfileGenerator &operator=(IProfileGenerator &&) = delete;
-
-  // Profile generation interface
-  //
-
-  /// Reference to configuration manager. This property allows to access and modify configurations
-  /// of the generator. This property is intended for managing the configuration.
-  ConfigurationManager &configurationManager();
-
-  /// Constant reference to the configuration manager. This property allows read access to the
-  /// configuration manager and is intended for profile generation.
-  ConfigurationManager const &configurationManager() const;
-
-  /// Creates a new profile based on the registered components, optimisation level and debug
-  /// requirements. The returned profile can be applied to an IR to transform it in accordance with
-  /// the configurations given.
-  Profile newProfile(OptimizationLevel const &optimisation_level, bool debug);
-
-  // Defining the generator
-  //
-
-  /// Registers a new profile component with a given configuration R. The profile component is given
-  /// a name and a setup function which is responsible for configuring the profile in accordance
-  /// with the configuration.
-  template <typename R>
-  void registerProfileComponent(String const &name, SetupFunction<R> setup);
-
-  // Support properties for generators
-  //
-
-  /// Returns the module pass manager.
-  llvm::ModulePassManager &modulePassManager();
-
-  /// Returns the pass builder.
-  llvm::PassBuilder &passBuilder();
-
-  /// Returns the optimisation level.
-  OptimizationLevel optimisationLevel() const;
-
-  /// Flag indicating whether we are operating in debug mode or not.
-  bool debug() const;
-
-protected:
-  llvm::ModulePassManager createGenerationModulePass(Profile &                profile,
-                                                     OptimizationLevel const &optimisation_level,
-                                                     bool                     debug);
-
-  llvm::ModulePassManager createValidationModulePass(PassBuilder &            pass_builder,
-                                                     OptimizationLevel const &optimisation_level,
-                                                     bool                     debug);
-
-  void addFunctionAnalyses(FunctionAnalysisManager &fam);
-
-private:
-  ConfigurationManager configuration_manager_;
-  Components           components_;
-
-  llvm::ModulePassManager *module_pass_manager_{nullptr};
-  llvm::PassBuilder *      pass_builder_{nullptr};
-  OptimizationLevel        optimisation_level_{OptimizationLevel::O0};
-  bool                     debug_{false};
-};
-
-template <typename R>
-void IProfileGenerator::registerProfileComponent(String const &name, SetupFunction<R> setup)
+namespace quantum
 {
-  configuration_manager_.addConfig<R>();
 
-  auto setup_wrapper = [setup](IProfileGenerator *ptr, Profile &profile) {
-    auto &config = ptr->configuration_manager_.get<R>();
-    setup(config, ptr, profile);
-  };
+    class IProfileGenerator
+    {
+      public:
+        // LLVM types
+        //
+        using PassBuilder             = llvm::PassBuilder;
+        using OptimizationLevel       = PassBuilder::OptimizationLevel;
+        using FunctionAnalysisManager = llvm::FunctionAnalysisManager;
 
-  components_.push_back({name, std::move(setup_wrapper)});
-}
+        // Types used for defining a component
+        //
+        using String = std::string;
 
-}  // namespace quantum
-}  // namespace microsoft
+        /// Setup function that uses a configuration type R to
+        /// configure the profile and/or generator.
+        template <typename R> using SetupFunction = std::function<void(R const&, IProfileGenerator*, Profile&)>;
+
+        /// Wrapper function type for invoking the profile setup function
+        using SetupFunctionWrapper = std::function<void(IProfileGenerator*, Profile&)>;
+
+        /// List of components to be configured.
+        using Components = std::vector<std::pair<String, SetupFunctionWrapper>>;
+
+        // Construction, moves and copies
+        //
+
+        IProfileGenerator()                         = default;
+        ~IProfileGenerator()                        = default;
+        IProfileGenerator(IProfileGenerator const&) = delete;
+        IProfileGenerator(IProfileGenerator&&)      = delete;
+        IProfileGenerator& operator=(IProfileGenerator const&) = delete;
+        IProfileGenerator& operator=(IProfileGenerator&&) = delete;
+
+        // Profile generation interface
+        //
+
+        /// Reference to configuration manager. This property allows to access and modify configurations
+        /// of the generator. This property is intended for managing the configuration.
+        ConfigurationManager& configurationManager();
+
+        /// Constant reference to the configuration manager. This property allows read access to the
+        /// configuration manager and is intended for profile generation.
+        ConfigurationManager const& configurationManager() const;
+
+        /// Creates a new profile based on the registered components, optimisation level and debug
+        /// requirements. The returned profile can be applied to an IR to transform it in accordance with
+        /// the configurations given.
+        Profile newProfile(OptimizationLevel const& optimisation_level, bool debug);
+
+        // Defining the generator
+        //
+
+        /// Registers a new profile component with a given configuration R. The profile component is given
+        /// a name and a setup function which is responsible for configuring the profile in accordance
+        /// with the configuration.
+        template <typename R> void registerProfileComponent(String const& name, SetupFunction<R> setup);
+
+        // Support properties for generators
+        //
+
+        /// Returns the module pass manager.
+        llvm::ModulePassManager& modulePassManager();
+
+        /// Returns the pass builder.
+        llvm::PassBuilder& passBuilder();
+
+        /// Returns the optimisation level.
+        OptimizationLevel optimisationLevel() const;
+
+        /// Flag indicating whether we are operating in debug mode or not.
+        bool debug() const;
+
+      protected:
+        llvm::ModulePassManager createGenerationModulePass(
+            Profile&                 profile,
+            OptimizationLevel const& optimisation_level,
+            bool                     debug);
+
+        llvm::ModulePassManager createValidationModulePass(
+            PassBuilder&             pass_builder,
+            OptimizationLevel const& optimisation_level,
+            bool                     debug);
+
+        void addFunctionAnalyses(FunctionAnalysisManager& fam);
+
+      private:
+        ConfigurationManager configuration_manager_;
+        Components           components_;
+
+        llvm::ModulePassManager* module_pass_manager_{nullptr};
+        llvm::PassBuilder*       pass_builder_{nullptr};
+        OptimizationLevel        optimisation_level_{OptimizationLevel::O0};
+        bool                     debug_{false};
+    };
+
+    template <typename R> void IProfileGenerator::registerProfileComponent(String const& name, SetupFunction<R> setup)
+    {
+        configuration_manager_.addConfig<R>();
+
+        auto setup_wrapper = [setup](IProfileGenerator* ptr, Profile& profile) {
+            auto& config = ptr->configuration_manager_.get<R>();
+            setup(config, ptr, profile);
+        };
+
+        components_.push_back({name, std::move(setup_wrapper)});
+    }
+
+} // namespace quantum
+} // namespace microsoft
