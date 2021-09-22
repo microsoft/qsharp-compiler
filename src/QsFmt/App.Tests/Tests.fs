2694a17a85edc5d9716fb1891bb02effbcb54f0c
Scott Carda <sccarda@microsoft.com>
2021-09-22 13:35:33 -0700

    Fixed help text test.

--- src/QsFmt/App.Tests/Tests.fs
+++ src/QsFmt/App.Tests/Tests.fs
@@ -79,6 +79,7 @@ SUBCOMMANDS:
 
 OPTIONS:
 
+    --backup, -b          Create backup files of input files.
     --recurse, -r         Process the input folder recursively.
     --help                Display this list of options.
 "
