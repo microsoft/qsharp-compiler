f953c40b539da5b2d6946c5ea27e538097d1e690
Stefan J. Wernli <swernli@microsoft.com>
2021-10-04 01:17:11 -0700

    Swernl/llvm passes merge main (#1181)
    * QIR generation bug fixes (#1153)
    
    * Allow ProjectReference as Target Package (#1166)
    
    Previously only a `PackageReference` would be recognized as a target package. This allows `ProjectReference` to be used as a target package, and still enforces that only one such reference exists.
    
    * Make signature for slicing compiliant with spec (#1167)
    
    * update packages for component governance (#1168)
    
    * Q# Command Line Process Multiple Files and Directories (#1156)
    
    * Force use of LLVM 11 in build pipeline (#1177)
    
    * Formatter - Output to Files (#1165)
    
    * Only re-indent if closing brace occurs in indentation region (#1174)
    
    * Only re-indent if closing brace occurs in indentation region
    
    * Use char.IsWhiteSpace
    
    * Blank line
    
    Co-authored-by: bettinaheim <34236215+bettinaheim@users.noreply.github.com>
    Co-authored-by: Angela Burton <anjbur@users.noreply.github.com>
    Co-authored-by: Scott Carda <55811729+ScottCarda-MS@users.noreply.github.com>
    Co-authored-by: Sarah Marshall <33814365+samarsha@users.noreply.github.com>

---
+++ src/QsFmt/Formatter.Tests/Tests.fs
@@ -0,0 +1,192 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+module Microsoft.Quantum.QsFmt.Formatter.Tests.Tests
+
+open System
+open System.IO
+open Microsoft.Quantum.QsFmt.Formatter
+open Xunit
+
+/// <summary>
+/// Ensures that the new line characters will conform to the standard of the environment's new line character.
+/// </summary>
+let standardizeNewLines (s: string) =
+    s.Replace("\r", "").Replace("\n", Environment.NewLine)
+
+let private run input expectedOutput expectedWarnings =
+    let previousError = Console.Error
+    use error = new StringWriter()
+    let expectedOutput = expectedOutput |> standardizeNewLines |> Ok
+    let expectedWarnings = expectedWarnings |> standardizeNewLines
+
+    try
+        Console.SetError error
+        let output = Formatter.update "" input |> Result.map standardizeNewLines
+        let warnings = error.ToString() |> standardizeNewLines
+        Assert.Equal(expectedOutput, output)
+        Assert.Equal(expectedWarnings, warnings)
+    finally
+        Console.SetError previousError
+
+[<Fact>]
+let ``Array Syntax Qubit Types`` () =
+    let input =
+        """namespace Foo {
+    operation Bar() : Unit {
+        let t1 = new Int[3];
+        let t2 = new Qubit[3];
+        let t3 = new Qubit[][3];
+        let t4 = new (Int, (Double, Qubit), (String), Unit)[3];
+        let t5 = new Double[3];
+    }
+}"""
+
+    let expectedOutput =
+        """namespace Foo {
+    operation Bar() : Unit {
+        let t1 = [0, size = 3];
+        let t2 = new Qubit[3];
+        let t3 = new Qubit[][3];
+        let t4 = new (Int, (Double, Qubit), (String), Unit)[3];
+        let t5 = [0.0, size = 3];
+    }
+}"""
+
+    let expectedWarnings =
+        """Warning: Unable to updated deprecated new array syntax in input from line 4, character 18 to line 4, character 30.
+Warning: Unable to updated deprecated new array syntax in input from line 5, character 18 to line 5, character 32.
+Warning: Unable to updated deprecated new array syntax in input from line 6, character 18 to line 6, character 63.
+"""
+
+    run input expectedOutput expectedWarnings
+
+[<Fact>]
+let ``Array Syntax UDT Types`` () =
+    let input =
+        """namespace Foo {
+    newtype MyType = (First: Int, Second: Double);
+    operation Bar() : Unit {
+        let t1 = new Int[3];
+        let t2 = new MyType[3];
+        let t3 = new MyType[][3];
+        let t4 = new (Int, (Double, MyType), (String), Unit)[3];
+        let t5 = new Double[3];
+    }
+}"""
+
+    let expectedOutput =
+        """namespace Foo {
+    newtype MyType = (First: Int, Second: Double);
+    operation Bar() : Unit {
+        let t1 = [0, size = 3];
+        let t2 = new MyType[3];
+        let t3 = new MyType[][3];
+        let t4 = new (Int, (Double, MyType), (String), Unit)[3];
+        let t5 = [0.0, size = 3];
+    }
+}"""
+
+    let expectedWarnings =
+        """Warning: Unable to updated deprecated new array syntax in input from line 5, character 18 to line 5, character 31.
+Warning: Unable to updated deprecated new array syntax in input from line 6, character 18 to line 6, character 33.
+Warning: Unable to updated deprecated new array syntax in input from line 7, character 18 to line 7, character 64.
+"""
+
+    run input expectedOutput expectedWarnings
+
+[<Fact>]
+let ``Array Syntax Type Parameter Types`` () =
+    let input =
+        """namespace Foo {
+    operation Bar<'T>() : Unit {
+        let t1 = new Int[3];
+        let t2 = new 'T[3];
+        let t3 = new 'T[][3];
+        let t4 = new (Int, (Double, 'T), (String), Unit)[3];
+        let t5 = new Double[3];
+    }
+}"""
+
+    let expectedOutput =
+        """namespace Foo {
+    operation Bar<'T>() : Unit {
+        let t1 = [0, size = 3];
+        let t2 = new 'T[3];
+        let t3 = new 'T[][3];
+        let t4 = new (Int, (Double, 'T), (String), Unit)[3];
+        let t5 = [0.0, size = 3];
+    }
+}"""
+
+    let expectedWarnings =
+        """Warning: Unable to updated deprecated new array syntax in input from line 4, character 18 to line 4, character 27.
+Warning: Unable to updated deprecated new array syntax in input from line 5, character 18 to line 5, character 29.
+Warning: Unable to updated deprecated new array syntax in input from line 6, character 18 to line 6, character 60.
+"""
+
+    run input expectedOutput expectedWarnings
+
+[<Fact>]
+let ``Array Syntax Function Types`` () =
+    let input =
+        """namespace Foo {
+    operation Bar() : Unit {
+        let t1 = new Int[3];
+        let t2 = new (Int -> Unit)[3];
+        let t3 = new (Int -> Unit)[][3];
+        let t4 = new (Int, (Double, (Int -> Unit)), (String), Unit)[3];
+        let t5 = new Double[3];
+    }
+}"""
+
+    let expectedOutput =
+        """namespace Foo {
+    operation Bar() : Unit {
+        let t1 = [0, size = 3];
+        let t2 = new (Int -> Unit)[3];
+        let t3 = new (Int -> Unit)[][3];
+        let t4 = new (Int, (Double, (Int -> Unit)), (String), Unit)[3];
+        let t5 = [0.0, size = 3];
+    }
+}"""
+
+    let expectedWarnings =
+        """Warning: Unable to updated deprecated new array syntax in input from line 4, character 18 to line 4, character 38.
+Warning: Unable to updated deprecated new array syntax in input from line 5, character 18 to line 5, character 40.
+Warning: Unable to updated deprecated new array syntax in input from line 6, character 18 to line 6, character 71.
+"""
+
+    run input expectedOutput expectedWarnings
+
+[<Fact>]
+let ``Array Syntax Operation Types`` () =
+    let input =
+        """namespace Foo {
+    operation Bar() : Unit {
+        let t1 = new Int[3];
+        let t2 = new (Int => Unit)[3];
+        let t3 = new (Int => Unit)[][3];
+        let t4 = new (Int, (Double, (Int => Unit)), (String), Unit)[3];
+        let t5 = new Double[3];
+    }
+}"""
+
+    let expectedOutput =
+        """namespace Foo {
+    operation Bar() : Unit {
+        let t1 = [0, size = 3];
+        let t2 = new (Int => Unit)[3];
+        let t3 = new (Int => Unit)[][3];
+        let t4 = new (Int, (Double, (Int => Unit)), (String), Unit)[3];
+        let t5 = [0.0, size = 3];
+    }
+}"""
+
+    let expectedWarnings =
+        """Warning: Unable to updated deprecated new array syntax in input from line 4, character 18 to line 4, character 38.
+Warning: Unable to updated deprecated new array syntax in input from line 5, character 18 to line 5, character 40.
+Warning: Unable to updated deprecated new array syntax in input from line 6, character 18 to line 6, character 71.
+"""
+
+    run input expectedOutput expectedWarnings
