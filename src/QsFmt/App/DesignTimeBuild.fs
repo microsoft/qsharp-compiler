f8158d1a87a88c1f9b69f7348385b45be5e17053
Scott Carda <sccarda@microsoft.com>
2021-10-06 13:44:29 -0700

    WIP: Fixed issue with relative paths to inputs, got version from project files.

--- src/QsFmt/App/DesignTimeBuild.fs
+++ src/QsFmt/App/DesignTimeBuild.fs
@@ -20,34 +20,43 @@ let globalProperties =
 /// Extracts the EvaluatedInclude for all items of the given type in the given project instance,
 /// and returns the combined path of the project directory and the evaluated include.
 /// </summary>
-let getItemsByType (project : ProjectInstance) (itemType : string) = 
+let getItemsByType (project: ProjectInstance) (itemType: string) =
     project.Items
-    |> Seq.where (fun item -> item.ItemType.Equals(itemType, StringComparison.OrdinalIgnoreCase) && item.EvaluatedInclude <> null)
-    |> Seq.map (fun item -> Path.Combine(project.Directory, item.EvaluatedInclude)) 
+    |> Seq.where
+        (fun item ->
+            item.ItemType.Equals(itemType, StringComparison.OrdinalIgnoreCase) && item.EvaluatedInclude <> null)
+    |> Seq.map (fun item -> Path.Combine(project.Directory, item.EvaluatedInclude))
 
-let getSourceFiles (projectFile : string) =
+let getSourceFiles (projectFile: string) =
 
     // todo: check project file exists?
 
     let mutable project = null
+
     try
         // Unloading the project unloads the project but *doesn't* clear the cache to be resilient to inconsistent states.
         // Hence we actually need to unload all projects, which does make sure the cache is cleared and changes on disk are reflected.
         // See e.g. https://github.com/Microsoft/msbuild/issues/795
-        if ProjectCollection.GlobalProjectCollection = null then 
-            ProjectCollection.GlobalProjectCollection.UnloadAllProjects(); // needed due to the caching behavior of MS build
+        if ProjectCollection.GlobalProjectCollection = null then
+            ProjectCollection.GlobalProjectCollection.UnloadAllProjects() // needed due to the caching behavior of MS build
+
         let properties = globalProperties // TODO: we may need to revise that
-        project <- new Project(projectFile, properties, ToolLocationHelper.CurrentToolsVersion);
+        project <- new Project(projectFile, properties, ToolLocationHelper.CurrentToolsVersion)
         let instance = project.CreateProjectInstance()
 
         // do we want to check if the project is a Q# project and if not fail?
         //let isQsProject = instance.Targets.ContainsKey "QSharpCompile"
 
         // get all the source files in the project
-        let sourceFiles = getItemsByType instance "QSharpCompile"
-        sourceFiles
-        // TODO: we also need to find out the version number of the Sdk
+        let sourceFiles = getItemsByType instance "QSharpCompile" |> Seq.toList
+
+        let version =
+            instance.Properties
+            |> Seq.tryFind (fun x -> x.Name = "QuantumSdkVersion")
+            |> Option.map (fun x -> x.EvaluatedValue)
+
+        (sourceFiles, version)
 
     finally
-        if project <> null && ProjectCollection.GlobalProjectCollection <> null then 
+        if project <> null && ProjectCollection.GlobalProjectCollection <> null then
             ProjectCollection.GlobalProjectCollection.UnloadProject project
--- src/QsFmt/App/Program.fs
+++ src/QsFmt/App/Program.fs
@@ -9,6 +9,7 @@ open System
 open System.IO
 open Microsoft.Build.Locator
 open System.Runtime.Loader
+open Microsoft.Quantum.QsFmt.App.DesignTimeBuild
 
 /// A command-line argument.
 [<HelpDescription "Display this list of options.">]
@@ -17,6 +18,7 @@ type Argument =
     | [<MainCommand; Unique; Last>] Inputs of string list
     | [<InheritAttribute; Unique; AltCommandLine("-b")>] Backup
     | [<InheritAttribute; Unique; AltCommandLine("-r")>] Recurse
+    | [<InheritAttribute; Unique>] Project of string
     | [<SubCommand; CliPrefix(CliPrefix.None)>] Update
     | [<SubCommand; CliPrefix(CliPrefix.None)>] Format
 
@@ -26,6 +28,7 @@ type Argument =
             | Inputs _ -> "Files or folders to format or \"-\" to read from standard input."
             | Backup -> "Create backup files of input files."
             | Recurse -> "Process the input folder recursively."
+            | Project _ -> "The project file for the project to process."
             | Update _ -> "Update depreciated syntax in the input files."
             | Format _ -> "Format the source code in input files."
 
@@ -76,12 +79,12 @@ let run arguments inputs =
                         else
                             if arguments.BackupFlag then File.Copy(input, (input + "~"), true)
                             File.ReadAllText input
-                    
+
                     let command =
                         match arguments.CommandKind with
                         | Update -> Formatter.update input
                         | Format -> Formatter.format
-                    
+
                     match command source with
                     | Ok result ->
                         if input = "-" then printf "%s" result else File.WriteAllText(input, result)
@@ -111,21 +114,26 @@ let main args =
     // We need to set the current directory to the same directory of
     // the LanguageServer executable so that it will pick the global.json file
     // and force the MSBuildLocator to use .NET Core SDK 3.1
-    Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+    let cwd = Directory.GetCurrentDirectory()
+    Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory)
     // In the case where we actually instantiate a server, we need to "configure" the design time build.
     // This needs to be done before any MsBuild packages are loaded.
     try
-        let vsi = MSBuildLocator.RegisterDefaults();
+        try
+            let vsi = MSBuildLocator.RegisterDefaults()
 
-        // We're using the installed version of the binaries to avoid a dependency between
-        // the .NET Core SDK version and NuGet. This is a workaround due to the issue below:
-        // https://github.com/microsoft/MSBuildLocator/issues/86
-        AssemblyLoadContext.Default.add_Resolving(new Func<_, _, _>(fun assemblyLoadContext assemblyName ->
-            let path = Path.Combine(vsi.MSBuildPath, sprintf "%s.dll" assemblyName.Name);
-            if File.Exists(path) then
-                assemblyLoadContext.LoadFromAssemblyPath path;
-            else null))
-    with | _ ->
+            // We're using the installed version of the binaries to avoid a dependency between
+            // the .NET Core SDK version and NuGet. This is a workaround due to the issue below:
+            // https://github.com/microsoft/MSBuildLocator/issues/86
+            AssemblyLoadContext.Default.add_Resolving (
+                new Func<_, _, _>(fun assemblyLoadContext assemblyName ->
+                    let path = Path.Combine(vsi.MSBuildPath, sprintf "%s.dll" assemblyName.Name)
+                    if File.Exists(path) then assemblyLoadContext.LoadFromAssemblyPath path else null)
+            )
+        finally
+            Directory.SetCurrentDirectory(cwd)
+    with
+    | _ ->
         // TODO: give some meaningful warning?
         ()
 
@@ -133,6 +141,11 @@ let main args =
 
     try
         let results = parser.Parse args
+
+        let inputs, qsharp_version =
+            match results.TryGetResult Project with
+            | Some p -> getSourceFiles p
+            | None -> results.GetResult Inputs, None
 
         let args =
             {
@@ -144,7 +157,7 @@ let main args =
                     | _ -> failwith "unrecognized command used"
                 RecurseFlag = results.Contains Recurse
                 BackupFlag = results.Contains Backup
-                Inputs = results.GetResult Inputs
+                Inputs = inputs
             }
 
         args.Inputs |> run args
