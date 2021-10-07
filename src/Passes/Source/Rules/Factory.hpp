85abed30060a0dccc6a60c81f1e838cb950b3edb
Troels F. Roennow <troels@wonop.com>
2021-10-07 07:48:29 +0200

    Fixing style

--- src/Passes/Source/Rules/Factory.hpp
+++ src/Passes/Source/Rules/Factory.hpp
@@ -143,10 +143,10 @@ namespace quantum
         /// Replaces branching of quantum results compared to zero. This method is not implemented yet.
         void optimiseBranchQuantumZero();
 
-        /// Replaces select based branching of quantum results compared to one. 
+        /// Replaces select based branching of quantum results compared to one.
         void optimiseSelectQuantumOne();
 
-        /// Replaces select based branching of quantum results compared to zero. 
+        /// Replaces select based branching of quantum results compared to zero.
         void optimiseSelectQuantumZero();
 
         // Disabling by feature
