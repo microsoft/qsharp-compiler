6fa630423f59284b872d1d0189e9e3c418498a56
Ryan Moreno (HE/HIM) <t-ryanmoreno@microsoft.com>
2021-10-27 08:15:00 -0500

    Adjust copyright header format

--- src/QsCompiler/LlvmBindings/LLVMSharpExtensions/LLVMContextRefExtensions.cs
+++ src/QsCompiler/LlvmBindings/LLVMSharpExtensions/LLVMContextRefExtensions.cs
@@ -1,11 +1,7 @@
-// -----------------------------------------------------------------------
 // <copyright file="LLVMContextRefExtensions.cs" company="Ubiquity.NET Contributors">
 // Copyright (c) Ubiquity.NET Contributors. All rights reserved.
 // Portions Copyright (c) Microsoft Corporation
 // </copyright>
-// -----------------------------------------------------------------------
-
-using System;
 
 namespace LLVMSharp.Interop
 {
