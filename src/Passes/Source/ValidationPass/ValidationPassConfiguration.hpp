82c11d35346103b79c3f689ce83056c518b4d654
Troels F. Roennow <troels@wonop.com>
2021-11-08 16:03:04 +0100

    Styling

--- src/Passes/Source/ValidationPass/ValidationPassConfiguration.hpp
+++ src/Passes/Source/ValidationPass/ValidationPassConfiguration.hpp
@@ -5,89 +5,91 @@
 #include "Commandline/ConfigurationManager.hpp"
 #include "QatTypes/QatTypes.hpp"
 
-namespace microsoft {
-namespace quantum {
-
-class ValidationPassConfiguration
+namespace microsoft
 {
-public:
-  using Set = std::unordered_set<std::string>;
-  // Setup and construction
-  //
+namespace quantum
+{
 
-  /// Setup function that adds the configuration flags to the ConfigurationManager. See the
-  /// ConfigurationManager documentation for more details on how the setup process is implemented.
-  void setup(ConfigurationManager &config)
-  {
-    config.setSectionName("Validation configuration", "");
-    config.addParameter(allow_internal_calls_, "allow-internal-calls",
-                        "Whether or not internal calls are allowed.");
-  }
-
-  static ValidationPassConfiguration fromProfileName(String const &name)
-  {
-    auto ret = ValidationPassConfiguration();
-    if (name == "generic")
+    class ValidationPassConfiguration
     {
-      ret.allow_internal_calls_     = true;
-      ret.whitelist_external_calls_ = false;
-      ret.whitelist_opcodes_        = false;
-    }
-    else if (name == "base")
-    {
-      ret.allow_internal_calls_     = false;
-      ret.whitelist_external_calls_ = true;
-      ret.whitelist_opcodes_        = true;
-      ret.opcodes_                  = Set{"br", "call", "unreachable", "ret", "phi", "select"};
-      ret.external_calls_ = Set{"__quantum__qis__mz__body",    "__quantum__qis__read_result__body",
-                                "__quantum__qis__reset__body", "__quantum__qis__z__body",
-                                "__quantum__qis__s__adj",      "__quantum__qis__dumpregister__body",
-                                "__quantum__qis__y__body",     "__quantum__qis__x__body",
-                                "__quantum__qis__t__body",     "__quantum__qis__cz__body",
-                                "__quantum__qis__s__body",     "__quantum__qis__h__body",
-                                "__quantum__qis__cnot__body",  "__quantum__qis__sqrt__body",
-                                "__quantum__qis__crz__body",   "__quantum__qis__rz__body"};
-    }
-    else
-    {
-      throw std::runtime_error("Invalid profile " + name);
-    }
-    return ret;
-  }
+      public:
+        using Set = std::unordered_set<std::string>;
+        // Setup and construction
+        //
 
-  Set const &allowedOpcodes() const
-  {
-    return opcodes_;
-  }
+        /// Setup function that adds the configuration flags to the ConfigurationManager. See the
+        /// ConfigurationManager documentation for more details on how the setup process is implemented.
+        void setup(ConfigurationManager& config)
+        {
+            config.setSectionName("Validation configuration", "");
+            config.addParameter(
+                allow_internal_calls_, "allow-internal-calls", "Whether or not internal calls are allowed.");
+        }
 
-  Set const &allowedExternalCallNames() const
-  {
-    return external_calls_;
-  }
+        static ValidationPassConfiguration fromProfileName(String const& name)
+        {
+            auto ret = ValidationPassConfiguration();
+            if (name == "generic")
+            {
+                ret.allow_internal_calls_     = true;
+                ret.whitelist_external_calls_ = false;
+                ret.whitelist_opcodes_        = false;
+            }
+            else if (name == "base")
+            {
+                ret.allow_internal_calls_     = false;
+                ret.whitelist_external_calls_ = true;
+                ret.whitelist_opcodes_        = true;
+                ret.opcodes_                  = Set{"br", "call", "unreachable", "ret", "phi", "select"};
+                ret.external_calls_           = Set{"__quantum__qis__mz__body",    "__quantum__qis__read_result__body",
+                                          "__quantum__qis__reset__body", "__quantum__qis__z__body",
+                                          "__quantum__qis__s__adj",      "__quantum__qis__dumpregister__body",
+                                          "__quantum__qis__y__body",     "__quantum__qis__x__body",
+                                          "__quantum__qis__t__body",     "__quantum__qis__cz__body",
+                                          "__quantum__qis__s__body",     "__quantum__qis__h__body",
+                                          "__quantum__qis__cnot__body",  "__quantum__qis__sqrt__body",
+                                          "__quantum__qis__crz__body",   "__quantum__qis__rz__body"};
+            }
+            else
+            {
+                throw std::runtime_error("Invalid profile " + name);
+            }
+            return ret;
+        }
 
-  bool allowInternalCalls() const
-  {
-    return allow_internal_calls_;
-  }
+        Set const& allowedOpcodes() const
+        {
+            return opcodes_;
+        }
 
-  bool whitelistOpcodes() const
-  {
-    return whitelist_opcodes_;
-  }
+        Set const& allowedExternalCallNames() const
+        {
+            return external_calls_;
+        }
 
-  bool whitelistExternalCalls() const
-  {
-    return whitelist_external_calls_;
-  }
+        bool allowInternalCalls() const
+        {
+            return allow_internal_calls_;
+        }
 
-private:
-  Set opcodes_{};
-  Set external_calls_{};
+        bool whitelistOpcodes() const
+        {
+            return whitelist_opcodes_;
+        }
 
-  bool whitelist_opcodes_{true};
-  bool whitelist_external_calls_{true};
-  bool allow_internal_calls_{false};
-};
+        bool whitelistExternalCalls() const
+        {
+            return whitelist_external_calls_;
+        }
 
-}  // namespace quantum
-}  // namespace microsoft
+      private:
+        Set opcodes_{};
+        Set external_calls_{};
+
+        bool whitelist_opcodes_{true};
+        bool whitelist_external_calls_{true};
+        bool allow_internal_calls_{false};
+    };
+
+} // namespace quantum
+} // namespace microsoft
