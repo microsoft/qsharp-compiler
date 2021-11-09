8c645948eea7eb31e2a380261e0400f989406815
Troels F. Roennow <troels@wonop.com>
2021-11-09 09:56:37 +0100

    Updating logger

--- src/Passes/Source/Logging/CommentLogger.hpp
+++ src/Passes/Source/Logging/CommentLogger.hpp
@@ -9,40 +9,10 @@
 namespace microsoft {
 namespace quantum {
 
-/// Concrete ILogger implementation that collects all messages and their corresponding location in a
-/// list that can be traversed later on.
-class LogCollection : public ILogger
+/// Concrete ILogger implementation that prints all messages as IR comments to llvm::errs().
+class CommentLogger : public ILogger
 {
 public:
-  /// Class that holds the location of where the incident happened.
-  struct Location
-  {
-    String   name{};
-    uint64_t row{0};
-    uint64_t col{0};
-  };
-
-  /// Enum description what type of information we are conveying.
-  enum class Type
-  {
-    Debug,
-    Info,
-    Warning,
-    Error,
-    InternalError,
-  };
-
-  /// Struct to hold a message together with its type and location
-  struct Message
-  {
-    Type     type;
-    Location location;
-    String   message;
-  };
-
-  /// List of messages defined as alias.
-  using Messages = std::vector<Message>;
-
   // Interface implementation
   //
 
@@ -65,8 +35,9 @@ public:
   void setLocation(String const &name, uint64_t row, uint64_t col) override;
 
 private:
-  Location current_location_{};  ///< Holds current location.
-  Messages messages_;            ///< All messages emitted.
+  String   location_name_{""};
+  uint64_t location_row_{0};
+  uint64_t location_col_{0};
 };
 
 }  // namespace quantum
--- src/Passes/Source/ValidationPass/ValidationPassConfiguration.hpp
+++ src/Passes/Source/ValidationPass/ValidationPassConfiguration.hpp
@@ -5,91 +5,97 @@
 #include "Commandline/ConfigurationManager.hpp"
 #include "QatTypes/QatTypes.hpp"
 
-namespace microsoft
-{
-namespace quantum
-{
+namespace microsoft {
+namespace quantum {
 
-    class ValidationPassConfiguration
+class ValidationPassConfiguration
+{
+public:
+  using Set = std::unordered_set<std::string>;
+  // Setup and construction
+  //
+
+  /// Setup function that adds the configuration flags to the ConfigurationManager. See the
+  /// ConfigurationManager documentation for more details on how the setup process is implemented.
+  void setup(ConfigurationManager &config)
+  {
+    config.setSectionName("Validation configuration", "");
+    config.addParameter(allow_internal_calls_, "allow-internal-calls",
+                        "Whether or not internal calls are allowed.");
+    config.addParameter(allow_internal_calls_, "save-validation-report",
+                        "Saves the validation report to specified filename.");
+  }
+
+  static ValidationPassConfiguration fromProfileName(String const &name)
+  {
+    auto ret = ValidationPassConfiguration();
+    if (name == "generic")
     {
-      public:
-        using Set = std::unordered_set<std::string>;
-        // Setup and construction
-        //
+      ret.allow_internal_calls_     = true;
+      ret.whitelist_external_calls_ = false;
+      ret.whitelist_opcodes_        = false;
+    }
+    else if (name == "base")
+    {
+      ret.allow_internal_calls_     = false;
+      ret.whitelist_external_calls_ = true;
+      ret.whitelist_opcodes_        = true;
+      ret.opcodes_                  = Set{"br", "call", "unreachable", "ret", "phi", "select"};
+      ret.external_calls_ = Set{"__quantum__qis__mz__body",    "__quantum__qis__read_result__body",
+                                "__quantum__qis__reset__body", "__quantum__qis__z__body",
+                                "__quantum__qis__s__adj",      "__quantum__qis__dumpregister__body",
+                                "__quantum__qis__y__body",     "__quantum__qis__x__body",
+                                "__quantum__qis__t__body",     "__quantum__qis__cz__body",
+                                "__quantum__qis__s__body",     "__quantum__qis__h__body",
+                                "__quantum__qis__cnot__body",  "__quantum__qis__sqrt__body",
+                                "__quantum__qis__crz__body",   "__quantum__qis__rz__body"};
+    }
+    else
+    {
+      throw std::runtime_error("Invalid profile " + name);
+    }
+    return ret;
+  }
 
-        /// Setup function that adds the configuration flags to the ConfigurationManager. See the
-        /// ConfigurationManager documentation for more details on how the setup process is implemented.
-        void setup(ConfigurationManager& config)
-        {
-            config.setSectionName("Validation configuration", "");
-            config.addParameter(
-                allow_internal_calls_, "allow-internal-calls", "Whether or not internal calls are allowed.");
-        }
+  Set const &allowedOpcodes() const
+  {
+    return opcodes_;
+  }
 
-        static ValidationPassConfiguration fromProfileName(String const& name)
-        {
-            auto ret = ValidationPassConfiguration();
-            if (name == "generic")
-            {
-                ret.allow_internal_calls_     = true;
-                ret.whitelist_external_calls_ = false;
-                ret.whitelist_opcodes_        = false;
-            }
-            else if (name == "base")
-            {
-                ret.allow_internal_calls_     = false;
-                ret.whitelist_external_calls_ = true;
-                ret.whitelist_opcodes_        = true;
-                ret.opcodes_                  = Set{"br", "call", "unreachable", "ret", "phi", "select"};
-                ret.external_calls_           = Set{"__quantum__qis__mz__body",    "__quantum__qis__read_result__body",
-                                          "__quantum__qis__reset__body", "__quantum__qis__z__body",
-                                          "__quantum__qis__s__adj",      "__quantum__qis__dumpregister__body",
-                                          "__quantum__qis__y__body",     "__quantum__qis__x__body",
-                                          "__quantum__qis__t__body",     "__quantum__qis__cz__body",
-                                          "__quantum__qis__s__body",     "__quantum__qis__h__body",
-                                          "__quantum__qis__cnot__body",  "__quantum__qis__sqrt__body",
-                                          "__quantum__qis__crz__body",   "__quantum__qis__rz__body"};
-            }
-            else
-            {
-                throw std::runtime_error("Invalid profile " + name);
-            }
-            return ret;
-        }
+  Set const &allowedExternalCallNames() const
+  {
+    return external_calls_;
+  }
 
-        Set const& allowedOpcodes() const
-        {
-            return opcodes_;
-        }
+  bool allowInternalCalls() const
+  {
+    return allow_internal_calls_;
+  }
 
-        Set const& allowedExternalCallNames() const
-        {
-            return external_calls_;
-        }
+  bool whitelistOpcodes() const
+  {
+    return whitelist_opcodes_;
+  }
 
-        bool allowInternalCalls() const
-        {
-            return allow_internal_calls_;
-        }
+  bool whitelistExternalCalls() const
+  {
+    return whitelist_external_calls_;
+  }
 
-        bool whitelistOpcodes() const
-        {
-            return whitelist_opcodes_;
-        }
+  String const &saveReportTo() const
+  {
+    return save_report_to_;
+  }
 
-        bool whitelistExternalCalls() const
-        {
-            return whitelist_external_calls_;
-        }
+private:
+  Set    opcodes_{};
+  Set    external_calls_{};
+  String save_report_to_{""};
 
-      private:
-        Set opcodes_{};
-        Set external_calls_{};
+  bool whitelist_opcodes_{true};
+  bool whitelist_external_calls_{true};
+  bool allow_internal_calls_{false};
+};
 
-        bool whitelist_opcodes_{true};
-        bool whitelist_external_calls_{true};
-        bool allow_internal_calls_{false};
-    };
-
-} // namespace quantum
-} // namespace microsoft
+}  // namespace quantum
+}  // namespace microsoft
