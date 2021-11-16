0c4ab4af8c891c9ab1e149501f8fbe0c7c9f9aa4
Stefan J. Wernli <swernli@microsoft.com>
2021-11-16 11:33:59 -0800

    Add aggressive inline pass to QAT (#1260)
    * Add aggressive inline pass to QAT
    
    This adds an aggresive inline pass to QAT that will ensure functions remaining at the end of transformations get inlined even beyond the default limit of 250 lines.
    
    * Fix formatting, use INT_MAX
    
    * Remove extranous newlines

--- src/Passes/qir/qat/Apps/Qat/Qat.cpp
+++ src/Passes/qir/qat/Apps/Qat/Qat.cpp
@@ -59,6 +59,7 @@
 
 #include <dlfcn.h>
 
+#include <climits>
 #include <iomanip>
 #include <iostream>
 #include <unordered_map>
@@ -226,6 +227,9 @@ int main(int argc, char** argv)
                     llvm::ModulePassManager pipeline3 =
                         pass_builder.buildModuleOptimizationPipeline(opt, ptr->isDebugMode());
                     mpm.addPass(std::move(pipeline3));
+
+                    llvm::ModuleInlinerWrapperPass pipeline4 = ModuleInlinerWrapperPass(getInlineParams(INT_MAX));
+                    mpm.addPass(std::move(pipeline4));
                 }
             });
 
