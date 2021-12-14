d4a8ef1d97bdb4e99cc1a3e6fb85fe45c7e776cf
Scott Carda <sccarda@microsoft.com>
2021-12-13 17:46:05 -0800

    Added as much as I could without lambda data structs having the parameter information I need.

--- src/QsCompiler/Transformations/LiftLambdas.cs
+++ src/QsCompiler/Transformations/LiftLambdas.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using Microsoft.Quantum.QsCompiler.DataTypes;
 using Microsoft.Quantum.QsCompiler.SyntaxTokens;
 using Microsoft.Quantum.QsCompiler.SyntaxTree;
 using Microsoft.Quantum.QsCompiler.Transformations.Core;
@@ -63,7 +64,49 @@ namespace Microsoft.Quantum.QsCompiler.Transformations.LiftLambdas
                 /// <inheritdoc/>
                 public override ExpressionKind OnLambda(Lambda<TypedExpression> lambda)
                 {
-                    return base.OnLambda(lambda);
+                    // ToDo: process inner components of lambda
+
+                    var lambdaBody = lambda.Body;
+                    var returnStatment = new QsStatement(
+                        QsStatementKind.NewQsReturnStatement(lambdaBody),
+                        LocalDeclarations.Empty,
+                        QsNullable<QsLocation>.Null,
+                        QsComments.Empty);
+
+                    // ToDo: local declarations should include all local variables in scope as well as lambda parameters.
+                    var generatedContent = new QsScope(new[] { returnStatment }.ToImmutableArray(), LocalDeclarations.Empty);
+
+                    // The LiftBody determines which callable kind to generate based on the callable kind of the parent callable.
+                    // We need to override that behavior with the lambda kind. So we just change the callable in the shared state
+                    // to have the callable kind that we want.
+                    var originalCallable = this.SharedState.CurrentCallable!.Callable;
+                    var modifiedCallabe = new QsCallable(
+                        lambda.Kind.IsFunction ? QsCallableKind.Function : QsCallableKind.Operation,
+                        originalCallable.FullName,
+                        originalCallable.Attributes,
+                        originalCallable.Access,
+                        originalCallable.Source,
+                        originalCallable.Location,
+                        originalCallable.Signature,
+                        originalCallable.ArgumentTuple,
+                        originalCallable.Specializations,
+                        originalCallable.Documentation,
+                        originalCallable.Comments);
+
+                    this.SharedState.CurrentCallable = new ContentLifting.LiftContent.CallableDetails(modifiedCallabe);
+                    if (this.SharedState.LiftBody(generatedContent, true, out var call, out var callable))
+                    {
+                        // ToDo: ensure the lambda parameters are present and last in the parameter list for the generated callable,
+                        // and not present in the call expression.
+
+                        this.SharedState.GeneratedCallables!.Add(callable);
+
+                        return call.Expression; // ToDo: this seems wrong, should just have to return the out from the LiftBody
+                    }
+                    else
+                    {
+                        return base.OnLambda(lambda);
+                    }
                 }
             }
         }
