94c2244c351f06c449ed4e87ea951535661df0b8
Bettina Heim <beheim@microsoft.com>
2022-06-21 18:35:41 -0700

    undoing accidentally committed change in the language server

--- src/QsCompiler/LanguageServer/LanguageServer.cs
+++ src/QsCompiler/LanguageServer/LanguageServer.cs
@@ -176,26 +176,17 @@ namespace Microsoft.Quantum.QsLanguageServer
         internal Task SendTelemetryAsync(
                 string eventName,
                 Dictionary<string, string?> properties,
-                Dictionary<string, int> measurements)
-        {
-            var propsStr = properties.Select(kv => $"{kv.Key}: {kv.Value}");
-            var measStr = measurements.Select(kv => $"{kv.Key}: {kv.Value}");
-            var propsMsg = $"properties: \n{string.Join("\n", propsStr)}";
-            var measMsg = $"measurements: \n{string.Join("\n", measStr)}";
-            var msg = $"Event: {eventName}\n{propsMsg}\n{measMsg}";
-            this.LogToWindow(msg, MessageType.Info);
-
-#if TELEMETRY
+                Dictionary<string, int> measurements) =>
+            #if TELEMETRY
             this.NotifyClientAsync(Methods.TelemetryEventName, new Dictionary<string, object>
             {
                 ["event"] = eventName,
                 ["properties"] = properties,
                 ["measurements"] = measurements,
             });
-#else
-            return Task.CompletedTask;
-#endif
-        }
+            #else
+            Task.CompletedTask;
+            #endif
 
         /// <summary>
         /// to be called when the server encounters an internal error (i.e. a QsCompilerError is raised)
