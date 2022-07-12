9ba21b141951106f125598d3701961b7aa373263
Scott Carda <Scott.Carda@microsoft.com>
2022-07-12 11:18:27 -0700

    Output Recording will run for Microsoft Simulator

--- src/QsCompiler/QirGeneration/CompilationSteps.cs
+++ src/QsCompiler/QirGeneration/CompilationSteps.cs
@@ -46,9 +46,17 @@ namespace Microsoft.Quantum.QsCompiler.QIR
             }
         }
 
-        internal static Generator CreateAndPopulateGenerator(QsCompilation compilation, TargetCapability? targetCapability)
+        internal static Generator CreateAndPopulateGenerator(QsCompilation compilation, IDictionary<string, string?> assemblyConstants)
         {
+            var targetCapability = assemblyConstants.TryGetValue(ReservedKeywords.AssemblyConstants.TargetCapability, out var capability) && !string.IsNullOrWhiteSpace(capability)
+                ? TargetCapability.TryParse(capability) // null if parsing fails
+                : null;
+            var isMicrosoftSimulator =
+                assemblyConstants.TryGetValue(ReservedKeywords.AssemblyConstants.ProcessorArchitecture, out var architecture)
+                && architecture == ReservedKeywords.AssemblyConstants.MicrosoftSimulator;
+
             compilation =
+                isMicrosoftSimulator ||
                 targetCapability == TargetCapabilityModule.BasicExecution ||
                 targetCapability == TargetCapabilityModule.AdaptiveExecution
                 ? AddOutputRecording.Apply(compilation, useRuntimeAPI: true, mainSuffix: NameGeneration.MainSuffix, alwaysCreateWrapper: true)
@@ -77,7 +85,7 @@ namespace Microsoft.Quantum.QsCompiler.QIR
             return transformed;
         }
 
-        private static bool ValidateAndEmit(QsCompilation compilation, TargetCapability? targetCapability, Func<Generator, Action<string>, bool> emit, List<IRewriteStep.Diagnostic>? diagnostics = null)
+        private static bool ValidateAndEmit(QsCompilation compilation, IDictionary<string, string?> assemblyConstants, Func<Generator, Action<string>, bool> emit, List<IRewriteStep.Diagnostic>? diagnostics = null)
         {
             diagnostics ??= new List<IRewriteStep.Diagnostic>();
             var preconditionSatisfied =
@@ -95,7 +103,7 @@ namespace Microsoft.Quantum.QsCompiler.QIR
             if (preconditionSatisfied)
             {
                 compilation = RunTargetInstructionInference(compilation, diagnostics);
-                using var generator = CreateAndPopulateGenerator(compilation, targetCapability);
+                using var generator = CreateAndPopulateGenerator(compilation, assemblyConstants);
                 return emit(generator, GenerateError);
             }
             else
@@ -108,7 +116,7 @@ namespace Microsoft.Quantum.QsCompiler.QIR
         /// Creates a file with the given name that contains the QIR bitcode for the given compilation.
         /// </summary>
         /// <param name="compilation">The Q# compilation to compile to bitcode.</param>
-        /// <param name="targetCapability">The set of features that are supported by the targeted backend. This is relevant for certain kinds of optimizations.</param>
+        /// <param name="assemblyConstants">Dictionary that contains the assembly constants for the project or compilation unit.</param>
         /// <param name="fileName">The name of the file where the bitcode should be written to.</param>
         /// <param name="overwrite">Whether or not to overwrite a file if it already exists.</param>
         /// <param name="diagnostics">A list to store any diagnostics generated in the process.</param>
@@ -116,18 +124,18 @@ namespace Microsoft.Quantum.QsCompiler.QIR
         /// <exception cref="InvalidOperationException">
         /// Thrown if a file with the given name already exists, but <paramref name="overwrite"/> has been set to false.
         /// </exception>
-        public static bool GenerateBitcode(QsCompilation compilation, TargetCapability? targetCapability, string fileName, bool overwrite = true, List<IRewriteStep.Diagnostic>? diagnostics = null)
+        public static bool GenerateBitcode(QsCompilation compilation, IDictionary<string, string?> assemblyConstants, string fileName, bool overwrite = true, List<IRewriteStep.Diagnostic>? diagnostics = null)
         {
             bool Emit(Generator generator, Action<string> onError) =>
                 generator.Emit(fileName, emitBitcode: true, overwrite: overwrite, onError: onError);
-            return ValidateAndEmit(compilation, targetCapability, Emit, diagnostics);
+            return ValidateAndEmit(compilation, assemblyConstants, Emit, diagnostics);
         }
 
         /// <summary>
         /// Creates a file with the given name that contains the human readable QIR for the given compilation.
         /// </summary>
         /// <param name="compilation">The Q# compilation to compile to bitcode.</param>
-        /// <param name="targetCapability">The set of features that are supported by the targeted backend. This is relevant for certain kinds of optimizations.</param>
+        /// <param name="assemblyConstants">Dictionary that contains the assembly constants for the project or compilation unit.</param>
         /// <param name="fileName">The name of the file where the bitcode should be written to.</param>
         /// <param name="overwrite">Whether or not to overwrite a file if it already exists.</param>
         /// <param name="diagnostics">A list to store any diagnostics generated in the process.</param>
@@ -135,27 +143,27 @@ namespace Microsoft.Quantum.QsCompiler.QIR
         /// <exception cref="InvalidOperationException">
         /// Thrown if a file with the given name already exists, but <paramref name="overwrite"/> has been set to false.
         /// </exception>
-        public static bool GenerateLlvmIR(QsCompilation compilation, TargetCapability? targetCapability, string fileName, bool overwrite = true, List<IRewriteStep.Diagnostic>? diagnostics = null)
+        public static bool GenerateLlvmIR(QsCompilation compilation, IDictionary<string, string?> assemblyConstants, string fileName, bool overwrite = true, List<IRewriteStep.Diagnostic>? diagnostics = null)
         {
             bool Emit(Generator generator, Action<string> onError) =>
                 generator.Emit(fileName, emitBitcode: false, overwrite: overwrite, onError: onError);
-            return ValidateAndEmit(compilation, targetCapability, Emit, diagnostics);
+            return ValidateAndEmit(compilation, assemblyConstants, Emit, diagnostics);
         }
 
         /// <summary>
         /// Creates a string containing the human readable QIR for the given compilation.
         /// </summary>
         /// <param name="compilation">The Q# compilation to compile to bitcode.</param>
-        /// <param name="targetCapability">The set of features that are supported by the targeted backend. This is relevant for certain kinds of optimizations.</param>
+        /// <param name="assemblyConstants">Dictionary that contains the assembly constants for the project or compilation unit.</param>
         /// <param name="diagnostics">A list to store any diagnostics generated in the process.</param>
         /// <returns>True if the QIR passes basic LLVM validation.</returns>
-        public static bool GenerateLlvmIR(QsCompilation compilation, TargetCapability? targetCapability, out string? llvmIR, List<IRewriteStep.Diagnostic>? diagnostics = null)
+        public static bool GenerateLlvmIR(QsCompilation compilation, IDictionary<string, string?> assemblyConstants, out string? llvmIR, List<IRewriteStep.Diagnostic>? diagnostics = null)
         {
             string? generatedIR = null;
             bool Emit(Generator generator, Action<string> onError) =>
                 generator.EmitLlvmIR(out generatedIR, onError: onError);
 
-            var succeeded = ValidateAndEmit(compilation, targetCapability, Emit, diagnostics);
+            var succeeded = ValidateAndEmit(compilation, assemblyConstants, Emit, diagnostics);
             llvmIR = generatedIR;
             return succeeded;
         }
