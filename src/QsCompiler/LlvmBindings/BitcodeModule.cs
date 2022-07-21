575f3fcf29c693713140c4d375c6ee9975d80bb0
Scott Carda <Scott.Carda@microsoft.com>
2022-07-21 12:43:35 -0700

    Added in the LoadFrom static functions to the Llvm BitcodeModule type.

--- src/QsCompiler/LlvmBindings/BitcodeModule.cs
+++ src/QsCompiler/LlvmBindings/BitcodeModule.cs
@@ -593,6 +593,64 @@ namespace LlvmBindings
             return FromHandle(this.moduleHandle.Clone())!;
         }
 
+        /// <summary>Load a bit-code module from a given file.</summary>
+        /// <param name="path">path of the file to load.</param>
+        /// <param name="context">Context to use for creating the module.</param>
+        /// <returns>Loaded <see cref="BitcodeModule"/>.</returns>
+        public static BitcodeModule LoadFrom(string path, Context context)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException();
+            }
+
+            var buffer = new MemoryBuffer(path);
+            return LoadFrom(buffer, context);
+        }
+
+        /// <summary>Load bit code from a memory buffer.</summary>
+        /// <param name="buffer">Buffer to load from.</param>
+        /// <param name="context">Context to load the module into.</param>
+        /// <returns>Loaded <see cref="BitcodeModule"/>.</returns>
+        /// <remarks>
+        /// This along with <see cref="WriteToBuffer"/> are useful for "cloning"
+        /// a module from one context to another. This allows creation of multiple
+        /// modules on different threads and contexts and later moving them to a
+        /// single context in order to link them into a single final module for
+        /// optimization.
+        /// </remarks>
+        public static BitcodeModule LoadFrom(MemoryBuffer buffer, Context context)
+        {
+            return TryParseBitcode(buffer.BufferHandle, out LLVMModuleRef modRef, out string message)
+                ? context.GetModuleFor(modRef)
+                : throw new InternalCodeGeneratorException(message);
+        }
+
+        private static unsafe bool TryParseBitcode(LLVMMemoryBufferRef handle, out LLVMModuleRef outModule, out string outMessage)
+        {
+            fixed (LLVMModuleRef* pOutModule = &outModule)
+            {
+                sbyte* pMessage = null;
+                var result = LLVM.ParseBitcodeInContext(
+                    LLVM.ContextCreate(),
+                    handle,
+                    (LLVMOpaqueModule**)pOutModule,
+                    &pMessage);
+
+                if (pMessage == null)
+                {
+                    outMessage = string.Empty;
+                }
+                else
+                {
+                    var span = new ReadOnlySpan<byte>(pMessage, int.MaxValue);
+                    outMessage = span.Slice(0, span.IndexOf((byte)'\0')).AsString();
+                }
+
+                return result == 0;
+            }
+        }
+
         internal static BitcodeModule FromHandle(LLVMModuleRef nativeHandle)
         {
             var contextRef = nativeHandle.Context;
