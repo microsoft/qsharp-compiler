c3e16237f79af4d630085fc7a85897f9ca0da94b
Ricardo Espinoza <ricardoe@microsoft.com>
2022-07-22 10:44:25 -0700

    Force telemetry flag across entire repository

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -49,7 +49,7 @@ if ("$VsCodeVsixVersion".Trim().Length -eq 0) {
 }
 
 
-$Telemetry = "$($Env:ASSEMBLY_CONSTANTS)".Contains("TELEMETRY").ToString().ToLower();
+$Telemetry = "true";
 Write-Host "Enable telemetry: $Telemetry";
 
 Get-ChildItem -Recurse *.v.template `
