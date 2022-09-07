d97c57cf62100243eebe9a130fe10059d4a6e537
Sarah Marshall <sarah.marshall@microsoft.com>
2022-09-07 12:05:16 -0700

    Improve assertion failure message

--- src/QsCompiler/Tests.Compiler/TestUtils/Diagnostics.fs
+++ src/QsCompiler/Tests.Compiler/TestUtils/Diagnostics.fs
@@ -3,17 +3,36 @@
 
 module internal Microsoft.Quantum.QsCompiler.Testing.Diagnostics
 
-open Microsoft.VisualStudio.LanguageServer.Protocol
-open System.Collections.Generic
-open System.Collections.Immutable
-open Xunit
-
+open System
 open Microsoft.Quantum.QsCompiler
 open Microsoft.Quantum.QsCompiler.CompilationBuilder
 open Microsoft.Quantum.QsCompiler.Diagnostics
 open Microsoft.Quantum.QsCompiler.SyntaxTree
+open Microsoft.VisualStudio.LanguageServer.Protocol
+open System.Collections.Generic
+open System.Collections.Immutable
+
+open Microsoft.Quantum.QsCompiler.DataTypes
 
 open type CompilationUnitManager
+
+type private MultiHashSet<'T when 'T: equality>(items) =
+    // Invariant: For all (item, n) in dict, n > 0.
+    let dict = Dictionary<'T, _>()
+
+    do
+        for item in items do
+            dict[item] <- dict.GetValueOrDefault item + 1
+
+    member _.IsEmpty = dict.Count = 0
+
+    member _.Remove item =
+        match dict.GetValueOrDefault item with
+        | 0 -> false
+        | 1 -> dict.Remove item
+        | n ->
+            dict[item] <- n - 1
+            true
 
 let byDeclaration (compilation: Compilation) =
     let syntaxTree =
@@ -65,36 +84,37 @@ let byDeclaration (compilation: Compilation) =
     ]
     |> dict
 
+let private toDiagnosticItem (diagnostic: Diagnostic) =
+    let _, code = Diagnostics.TryGetCode diagnostic.Code.Value.Second
+
+    match diagnostic.Severity.Value with
+    | DiagnosticSeverity.Error -> enum code |> Error
+    | DiagnosticSeverity.Warning -> enum code |> Warning
+    | DiagnosticSeverity.Information -> enum code |> Information
+    | _ -> failwith "Severity not supported."
+
+let private showRange (range: Range option) =
+    match range with
+    | None -> "with any range"
+    | Some r -> $"from [Ln {r.Start.Line}, Col {r.Start.Column}] to [Ln {r.End.Line}, Col {r.End.Column}]"
+
+let private showDiagnostics diagnostics =
+    diagnostics
+    |> Seq.map (fun (diagnostic, range) -> $"- {diagnostic} {showRange range}")
+    |> String.concat Environment.NewLine
+
 let assertMatches expected (actual: Diagnostic seq) =
-    let expectedDict = Dictionary()
-
-    for e in expected do
-        match expectedDict.TryGetValue e with
-        | true, count -> expectedDict[e] <- count + 1
-        | false, _ -> expectedDict[e] <- 1
-
     let actual =
-        actual
-        |> Seq.map (fun d ->
-            let code = Diagnostics.TryGetCode d.Code.Value.Second |> snd
+        Seq.map (fun d -> toDiagnosticItem d, d.Range.ToQSharp()) actual |> ImmutableArray.CreateRange
 
-            let item =
-                match d.Severity.Value with
-                | DiagnosticSeverity.Error -> enum code |> Error
-                | DiagnosticSeverity.Warning -> enum code |> Warning
-                | DiagnosticSeverity.Information -> enum code |> Information
-                | _ -> failwith "Invalid severity."
+    let fail () =
+        let expected' = showDiagnostics expected
+        let actual' = Seq.map (fun (d, r) -> d, Some r) actual |> showDiagnostics
+        failwith $"Diagnostics did not match.\n\nExpected:\n{expected'}\nActual:\n{actual'}"
 
-            item, d.Range.ToQSharp())
+    let remaining = MultiHashSet expected
 
     for item, range in actual do
-        match expectedDict.TryGetValue((item, Some range)) with
-        | true, 1 -> let removed = expectedDict.Remove((item, Some range)) in assert removed
-        | true, count -> expectedDict[(item, Some range)] <- count - 1
-        | false, _ ->
-            match expectedDict.TryGetValue((item, None)) with
-            | true, 1 -> let removed = expectedDict.Remove((item, None)) in assert removed
-            | true, count -> expectedDict[(item, None)] <- count - 1
-            | false, _ -> failwith "Unexpected diagnostic."
+        if not (remaining.Remove((item, Some range)) || remaining.Remove((item, None))) then fail ()
 
-    Assert.Empty expectedDict
+    if not remaining.IsEmpty then fail ()
